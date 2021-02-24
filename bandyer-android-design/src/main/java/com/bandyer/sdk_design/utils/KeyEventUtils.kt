3f4adfcd81bccdb4afe802b7ccda9ee467675091
Federico Marin <federico.marin@bandyer.com>
2021-02-24 09:34:36 +0100

    Add doc to key event util function

--- bandyer-android-design/src/main/java/com/bandyer/sdk_design/utils/KeyEventUtils.kt
+++ bandyer-android-design/src/main/java/com/bandyer/sdk_design/utils/KeyEventUtils.kt
@@ -2,6 +2,11 @@ package com.bandyer.sdk_design.utils
 
 import android.view.KeyEvent
 
+/**
+ * Check if the input keycode represents a confirmation
+ * @receiver KeyEvent the input keycode
+ * @return Boolean true if represents a confirmation, false otherwise
+ */
 fun KeyEvent.isConfirmButton(): Boolean = keyCode == android.view.KeyEvent.KEYCODE_DPAD_CENTER ||
                 keyCode == android.view.KeyEvent.KEYCODE_ENTER ||
                 keyCode == android.view.KeyEvent.KEYCODE_SPACE
