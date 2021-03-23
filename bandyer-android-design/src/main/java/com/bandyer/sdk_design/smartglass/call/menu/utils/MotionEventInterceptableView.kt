cb7fe6100150f45fdff4676734ab740d9fcb0a65
Federico Marin <federico.marin@bandyer.com>
2021-03-23 22:51:34 +0100

    Fix MotionEventInterceptableView generic type declaration and fix documentation

--- bandyer-android-design/src/main/java/com/bandyer/sdk_design/smartglass/call/menu/utils/MotionEventInterceptableView.kt
+++ bandyer-android-design/src/main/java/com/bandyer/sdk_design/smartglass/call/menu/utils/MotionEventInterceptableView.kt
@@ -2,7 +2,6 @@ package com.bandyer.sdk_design.smartglass.call.menu.utils
 
 import android.view.MotionEvent
 import android.view.View
-import android.view.ViewGroup
 import com.bandyer.android_common.FieldProperty
 
 /**
@@ -12,14 +11,14 @@ import com.bandyer.android_common.FieldProperty
 interface MotionEventInterceptableView<T> where T : View
 
 /**
- * Property specifying the motion event interceptor assigned to a MotionEventInterceptableViewGroup instance
+ * Property specifying the motion event interceptor assigned to a MotionEventInterceptableView instance
  */
-var <T> MotionEventInterceptableView<T>.motionEventInterceptor: MotionEventInterceptor? where T : ViewGroup by FieldProperty { null }
+var <T> MotionEventInterceptableView<T>.motionEventInterceptor: MotionEventInterceptor? where T : View by FieldProperty { null }
 
 /**
  * Dispatches input motion events to the motionEventInterceptor if present
- * @receiver MotionEventInterceptableViewGroup<T> the MotionEventInterceptableView that should dispatch motion events to the motion event interceptor
+ * @receiver MotionEventInterceptableView<T> the MotionEventInterceptableView that should dispatch motion events to the motion event interceptor
  * @param event MotionEvent? optional input motion event that occurred on the view
  * @return Unit?
  */
-fun <T> MotionEventInterceptableView<T>.dispatchMotionEventToInterceptor(event: MotionEvent?) where T : ViewGroup = motionEventInterceptor?.onMotionEventIntercepted(event)
+fun <T> MotionEventInterceptableView<T>.dispatchMotionEventToInterceptor(event: MotionEvent?) where T : View = motionEventInterceptor?.onMotionEventIntercepted(event)
