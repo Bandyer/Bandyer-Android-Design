a95a09f1cd4e5c4ab561fabb1efceef2302b536c
Stefano <bpbrusad@gmail.com>
2021-05-20 18:48:55 +0200

    add FileShareActivity

--- demo_app/src/main/java/com/bandyer/demo_sdk_design/FileShareActivity.kt
+++ demo_app/src/main/java/com/bandyer/demo_sdk_design/FileShareActivity.kt
@@ -2,15 +2,24 @@ package com.bandyer.demo_sdk_design
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.util.concurrent.ConcurrentHashMap
+import com.bandyer.communication_center.file_share.file_sharing_center.FileSharingConfig
+import com.bandyer.demo_sdk_design.file_share.FileShareViewModel
+import com.bandyer.demo_sdk_design.file_share.MockFileSharingManager
+import com.bandyer.sdk_design.filesharing.BandyerFileShareDialog
+import com.google.android.material.button.MaterialButton
 
 class FileShareActivity : AppCompatActivity() {
 
 //    private var adapterItems: ConcurrentHashMap<String, CustomAbstractItem<*, *>> = ConcurrentHashMap()
+    private val fsm = MockFileSharingManager.newInstance(FileSharingConfig(""))
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_file_share)
+
+        findViewById<MaterialButton>(R.id.btn_file_share).setOnClickListener {
+            BandyerFileShareDialog().show(this@FileShareActivity)
+        }
     }
 }
