7ec7dbe53eaa5b5cf203a3fb2d9691070b9c3bbf
Stefano <bpbrusad@gmail.com>
2021-05-21 17:18:54 +0200

    fix parseToHHmm exts

--- bandyer-android-design/src/main/java/com/bandyer/sdk_design/extensions/LongExtensions.kt
+++ bandyer-android-design/src/main/java/com/bandyer/sdk_design/extensions/LongExtensions.kt
@@ -1,7 +1,7 @@
 package com.bandyer.sdk_design.extensions
 
 fun Long.parseToHHmm(): String {
-    val min = (this / (1000 * 60) % 60)
-    val hr = (this / (1000 * 60 * 60) % 24)
-    return "$min:$hr"
+    val mm = (this / (1000 * 60) % 60)
+    val hh = (this / (1000 * 60 * 60) % 24)
+    return "$hh:$mm"
 }
