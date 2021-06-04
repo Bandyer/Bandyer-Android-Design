b0070dc8b90ca952c3fa05ddd8ff05607ba1951b
Stefano <bpbrusad@gmail.com>
2021-06-04 09:53:39 +0200

    add itemsData in viewmodel

--- bandyer-android-design/src/main/java/com/bandyer/sdk_design/filesharing/FileShareViewModel.kt
+++ bandyer-android-design/src/main/java/com/bandyer/sdk_design/filesharing/FileShareViewModel.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import java.io.File
+import java.util.concurrent.ConcurrentHashMap
 
 abstract class FileShareViewModel : ViewModel() {
+    val itemsData: ConcurrentHashMap<String, FileShareItemData> = ConcurrentHashMap()
+
     abstract fun upload(uploadId: String?, context: Context, uri: Uri): String
 
     abstract fun cancelUpload(uploadId: String)
