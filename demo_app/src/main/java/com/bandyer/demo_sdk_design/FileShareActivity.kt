30df7ee0301c4d3780047ec61b4b8aa5af6f8414
Stefano <bpbrusad@gmail.com>
2021-06-22 15:59:15 +0200

    import viewBinding to demo app

--- demo_app/src/main/java/com/bandyer/demo_sdk_design/FileShareActivity.kt
+++ demo_app/src/main/java/com/bandyer/demo_sdk_design/FileShareActivity.kt
@@ -2,18 +2,23 @@ package com.bandyer.demo_sdk_design
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bandyer.demo_sdk_design.databinding.ActivityFileShareBinding
 import com.bandyer.sdk_design.filesharing.BandyerFileShareDialog
 
 class FileShareActivity : AppCompatActivity() {
 
     private var fileShareDialog: BandyerFileShareDialog? = null
 
+    private lateinit var binding: ActivityFileShareBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_file_share)
+        binding = ActivityFileShareBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         fileShareDialog = BandyerFileShareDialog()
 
+        initListeners()
     }
 
     override fun onStop() {
@@ -24,5 +29,20 @@ class FileShareActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         fileShareDialog = null
+    }
+
+    private fun initListeners() {
+        binding.btnAddUpload.setOnClickListener { }
+
+        binding.btnAddDownload.setOnClickListener { }
+
+        binding.btnAddDownloadAvailable.setOnClickListener {
+//            viewModel.itemsData[downloadId] = DownloadAvailableData(id = downloadId, sender = "Will Smith", endpoint = "", startTime = Date().time, totalBytes = 0L, fileName = "")
+//            if(fileShareDialog?.isVisible == true) fileShareDialog?.updateRecyclerViewItems(viewModel.itemsData)
+        }
+
+        binding.btnFileShare.setOnClickListener {
+            fileShareDialog?.show(this@FileShareActivity)
+        }
     }
 }
