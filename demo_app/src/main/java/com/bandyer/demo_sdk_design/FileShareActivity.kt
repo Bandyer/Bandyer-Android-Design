dcd1dae1d6ac0553c29f69cd1743587379217a39
Stefano <bpbrusad@gmail.com>
2021-06-22 17:10:27 +0200

    adjust file share item view

--- demo_app/src/main/java/com/bandyer/demo_sdk_design/FileShareActivity.kt
+++ demo_app/src/main/java/com/bandyer/demo_sdk_design/FileShareActivity.kt
@@ -23,11 +23,11 @@ class FileShareActivity : AppCompatActivity() {
         binding = ActivityFileShareBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.itemsData["id_1"] = FileTransfer(FileInfo(uri = "".toUri(), name = "razer.jpg", mimeType = "image/jpg", sender = "Gianluigi", size = 100L), FileTransfer.State.Pending, FileTransfer.Type.DownloadAvailable)
+        viewModel.itemsData["id_1"] = FileTransfer(FileInfo(uri = "".toUri(), name = "razer.jpg", mimeType = "image/jpeg", sender = "Gianluigi", size = 100L), FileTransfer.State.Pending, FileTransfer.Type.DownloadAvailable)
         viewModel.itemsData["id_2"] = FileTransfer(FileInfo(uri = "".toUri(), name = "identity_card.pdf", mimeType = "", sender = "Mario", size = 100L), FileTransfer.State.Success("".toUri()), FileTransfer.Type.Download)
         viewModel.itemsData["id_3"] = FileTransfer(FileInfo(uri = "".toUri(), name = "car.zip", mimeType = "application/zip", sender = "Luigi", size = 1000L), FileTransfer.State.OnProgress(600L), FileTransfer.Type.Download)
         viewModel.itemsData["id_4"] = FileTransfer(FileInfo(uri = "".toUri(), name = "phone.doc", mimeType = "", sender = "Gianni", size = 23000000L), FileTransfer.State.Error(Throwable()), FileTransfer.Type.Upload)
-        viewModel.itemsData["id_5"] = FileTransfer(FileInfo(uri = "".toUri(), name = "address.jpg", mimeType = "image/jpg", sender = "Marco", size = 1000L), FileTransfer.State.Pending, FileTransfer.Type.Upload)
+        viewModel.itemsData["id_5"] = FileTransfer(FileInfo(uri = "".toUri(), name = "address.jpg", mimeType = "image/jpeg", sender = "Marco", size = 1000L), FileTransfer.State.Pending, FileTransfer.Type.Upload)
 
         binding.btnFileShare.setOnClickListener { BandyerFileShareDialog().show(this@FileShareActivity, viewModel) }
     }
