4dba12c93f76fad0d8f4f4c1c88adb78110f889b
Stefano <bpbrusad@gmail.com>
2021-07-21 18:28:35 +0200

    add VolumeFragment

---
+++ bandyer-android-design/src/main/java/com/bandyer/sdk_design/VolumeFragment.kt
@@ -0,0 +1,30 @@
+package com.bandyer.sdk_design
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bandyer.sdk_design.databinding.BandyerFragmentVolumeBinding
+import com.bandyer.sdk_design.new_smartglass.SmartGlassTouchEvent
+import com.bandyer.sdk_design.new_smartglass.SmartGlassTouchEventListener
+
+class VolumeFragment : Fragment(), SmartGlassTouchEventListener {
+
+    private lateinit var binding: BandyerFragmentVolumeBinding
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = BandyerFragmentVolumeBinding.inflate(inflater, container, false)
+        val root = binding.root
+        root.setOnTouchListener { _, event -> binding.slider.onTouchEvent(event) }
+        return root
+    }
+
+    override fun onSmartGlassTouchEvent(event: SmartGlassTouchEvent.Event): Boolean = false
+
+}
