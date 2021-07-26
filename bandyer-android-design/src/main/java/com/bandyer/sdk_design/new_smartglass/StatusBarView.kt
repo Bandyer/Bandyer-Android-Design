8527afc5301a61ad341aeb361db28ad076ece154
Stefano <bpbrusad@gmail.com>
2021-07-26 12:58:52 +0200

    add status bar view and layout

---
+++ bandyer-android-design/src/main/java/com/bandyer/sdk_design/new_smartglass/StatusBarView.kt
@@ -0,0 +1,31 @@
+package com.bandyer.sdk_design.new_smartglass
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.bandyer.sdk_design.databinding.BandyerStatusBarLayoutBinding
+
+class StatusBarView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private var binding: BandyerStatusBarLayoutBinding =
+        BandyerStatusBarLayoutBinding.inflate(LayoutInflater.from(context), this, true)
+
+    fun setTitleText(text: String?) {
+        binding.bandyerTitle.text = text
+    }
+
+    fun setTitleIcon(drawable: Drawable?) {
+        binding.bandyerTitleIcon.setImageDrawable(drawable)
+    }
+
+    fun showMicMutedIcon(value: Boolean) {
+        binding.bandyerMicMutedIcon.visibility = if(value) View.VISIBLE else View.GONE
+    }
+}
