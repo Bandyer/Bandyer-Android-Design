a08e653311f827ae07b89d1449126efdff87ca83
Stefano <bpbrusad@gmail.com>
2021-07-29 18:29:08 +0200

    update contact avatar view

--- bandyer-android-design/src/main/java/com/bandyer/sdk_design/new_smartglass/ContactAvatarView.kt
+++ bandyer-android-design/src/main/java/com/bandyer/sdk_design/new_smartglass/ContactAvatarView.kt
@@ -4,18 +4,11 @@ import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.FrameLayout
-import androidx.annotation.ColorInt
-import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
-import com.bandyer.sdk_design.R
 import com.bandyer.sdk_design.databinding.BandyerContactAvatarLayoutBinding
-import com.bandyer.sdk_design.new_smartglass.utils.parseToColor
-import java.math.BigInteger
-import java.security.MessageDigest
+import com.bandyer.sdk_design.extensions.parseToColor
 import java.util.*
-import kotlin.random.Random
 
 class ContactAvatarView @JvmOverloads constructor(
     context: Context,
@@ -28,20 +21,20 @@ class ContactAvatarView @JvmOverloads constructor(
             LayoutInflater.from(context), this, true
         )
 
-    private var text: String? = null
-    private val defaultAvatar = R.drawable.ic_bandyer_avatar
+    private val defaultAvatar = android.R.color.transparent
     private val defaultBackgroundColor = Color.GRAY
 
     fun setAvatar(@DrawableRes resId: Int?) {
-        binding.bandyerAvatarText.visibility = if (resId == null && text == null) VISIBLE else GONE
         binding.bandyerAvatarImage.setImageResource(resId ?: defaultAvatar)
+        binding.bandyerAvatarText.visibility = if (resId == null) VISIBLE else GONE
     }
-    
+
     fun setText(text: String?) {
-        this.text = text
         binding.bandyerAvatarText.text = text?.toUpperCase(Locale.getDefault())
     }
 
-    fun setBackground(text: String?) = binding.bandyerAvatarImage.setBackgroundColor(text?.parseToColor() ?: defaultBackgroundColor)
+    fun setBackground(text: String?) = binding.bandyerAvatarImage.setBackgroundColor(
+        text?.parseToColor() ?: defaultBackgroundColor
+    )
 }
 
