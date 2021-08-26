1a6e70d2091eb8d20d401a2b354f9fe5648adbde
Stefano <bpbrusad@gmail.com>
2021-08-26 10:42:14 +0200

    refactor the slider, and solved the bug when initializing  it to zero

--- bandyer-android-design/src/main/java/com/bandyer/sdk_design/new_smartglass/BandyerSlider.kt
+++ bandyer-android-design/src/main/java/com/bandyer/sdk_design/new_smartglass/BandyerSlider.kt
@@ -2,6 +2,7 @@ package com.bandyer.sdk_design.new_smartglass
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.MotionEvent
@@ -10,6 +11,7 @@ import android.widget.SeekBar
 import com.bandyer.sdk_design.R
 import com.bandyer.sdk_design.databinding.BandyerSliderLayoutBinding
 
+@SuppressLint("ClickableViewAccessibility")
 class BandyerSlider @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
@@ -36,19 +38,47 @@ class BandyerSlider @JvmOverloads constructor(
 
         setPercentageText(0)
 
-        binding.seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                setPercentageText(progress * 10)
-                onSliderChangeListener?.onProgressChanged(progress)
-            }
+        with(binding.seekbar) {
+            setOnTouchListener { _, _ -> true }
+            max = MAX_VALUE
+            // Apparently there is a bug is updating the visual representation of the progress if it is initialized to zero.
+            // Neither the use of invalidate() and refreshDrawableState() solves the problem. The attribute android:saveEnabled="false"
+            // has also been set in the xml but the result is the same. The only workaround to solve the problem is setting the progress to a value
+            // different than zero, and then setting it to zero. In this way the visual representation is updated to the right value.
+            progress = 1
+            progress = 0
+            binding.seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    setPercentageText(progress * 10)
+                    onSliderChangeListener?.onProgressChanged(progress)
+                }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar?) = Unit
-            override fun onStopTrackingTouch(seekBar: SeekBar?) = Unit
-        })
+                override fun onStartTrackingTouch(seekBar: SeekBar?) = Unit
+                override fun onStopTrackingTouch(seekBar: SeekBar?) = Unit
+            })
+        }
     }
 
     fun setProgress(value: Int) {
         binding.seekbar.progress = value
+    }
+
+    fun incrementProgress() = with(binding.seekbar) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            setProgress(progress + PROGRESS_DELTA, true)
+        else
+            progress += PROGRESS_DELTA
+    }
+
+    fun decrementProgress() = with(binding.seekbar) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            setProgress(progress - PROGRESS_DELTA, true)
+        else
+            progress -= PROGRESS_DELTA
     }
 
     private fun setPercentageText(progress: Int) {
@@ -62,5 +92,10 @@ class BandyerSlider @JvmOverloads constructor(
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         return binding.seekbar.onTouchEvent(event)
+    }
+
+    private companion object {
+        const val PROGRESS_DELTA = 1
+        const val MAX_VALUE = 10
     }
 }
