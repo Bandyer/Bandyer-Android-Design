c4585e1c939c21a9ca9c5241658796410bb9d777
Stefano <bpbrusad@gmail.com>
2021-09-07 16:28:32 +0200

    refactor Iso8601

--- bandyer-android-design/src/main/java/com/bandyer/sdk_design/new_smartglass/Iso8601.kt
+++ bandyer-android-design/src/main/java/com/bandyer/sdk_design/new_smartglass/Iso8601.kt
@@ -6,10 +6,13 @@
 package com.bandyer.sdk_design.new_smartglass
 
 import android.content.Context
-import android.text.format.DateUtils
-import java.text.SimpleDateFormat
+import com.bandyer.sdk_design.R
+import java.time.*
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.time.format.TextStyle
+import java.time.temporal.ChronoUnit
 import java.util.*
-
 
 /**
  * @suppress
@@ -18,42 +21,22 @@ import java.util.*
  */
 object Iso8601 {
 
-//    val twelveHoursPattern = "hh:mm a"
-//    val twentyfourHoursPattern = "HH:mm"
-
-    // Quoted "Z" to indicate UTC, no timezone offset
-    private const val PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
-
-    private val sdf1
-        get() = SimpleDateFormat(
-            if (usesAmPm(Locale.getDefault())) "hh:mm a" else "HH:mm",
-            Locale.getDefault()
-        )
-    private val sdf2
-        get() = SimpleDateFormat("EEEE", Locale.getDefault())
-
-//    private var dateFormat: SimpleDateFormat? = null
-
-    private var bestDayFormat = ""
-
-    private val dateFormat
-        get() = SimpleDateFormat(PATTERN, Locale.getDefault()).also {
-            it.timeZone = TimeZone.getTimeZone("UTC")
-        }
+    private const val DAY_MILLIS = 86400000L
+    private const val WEEK_MILLIS = 604800000L
 
     /**
      * Get the current time in ISO8601 format
      *
      * @return String
      */
-    fun nowISO8601(): String = dateFormat.format(Date())
+    fun nowISO8601(): String = Instant.now().toString()
 
     /**
      * Get the current millis in UTC timezone
      *
      * @return Time in millis
      */
-    fun nowUTCMillis(): Long = Calendar.getInstance(TimeZone.getTimeZone("UTC")).timeInMillis
+    fun nowUTCMillis(): Long = Instant.now().toEpochMilli()
 
     /**
      * Get a iso8601 formatted timestamp as millis
@@ -61,141 +44,122 @@ object Iso8601 {
      * @param tstamp The timestamp
      * @return Time in millis
      */
-    fun getISO8601TstampInMillis(tstamp: String): Long? =
-        kotlin.runCatching {
-            dateFormat.parse(tstamp)?.time
-        }.getOrNull()
-
-
-    fun parseTimestampFromIso8601(context: Context, iso8601: Date?): String? {
-        iso8601 ?: kotlin.run {
-            return null
-        }
-        val calendar = Calendar.getInstance(Locale.getDefault())
-        calendar.time = iso8601
-        return parseTimestamp(context, calendar.timeInMillis)
-    }
+    fun getISO8601TstampInMillis(tstamp: String): Long = Instant.parse(tstamp).toEpochMilli()
 
     /**
-     * Transform ISO 8601 tstamp to human readable. This function has been modified to calculate UTC + offset
+     * Parse a millis timestamp into ISO8601 string
+     *
+     * @param millis The timestamp
+     * @return String The ISO8601 pattern string
      */
-    fun parseTimestamp(c: Context, timestamp: Long): String {
-        val tmp = getDateCurrentTimeZone(timestamp)
-        // check if is last week
-        return if (tmp.isLastWeek()) {
-            // check if today, than if yesterday, than return
+    fun parseMillisToIso8601(millis: Long): String = Instant.ofEpochMilli(millis).toString()
+
+    /**
+     * Parse a UTC millis timestamp into a human readable timestamp. This function takes into account the current zone offset.
+     *
+     * @param context The context
+     * @param timestamp The timestamp in millis
+     * @return String A human readable date time timestamp string
+     */
+    fun parseTimestamp(context: Context, timestamp: Long): String {
+        val zonedDateTime = Instant
+            .ofEpochMilli(timestamp)
+            .atZone(ZoneId.systemDefault())
+
+        return if (zonedDateTime.isLastWeek()) {
             when {
-                DateUtils.isToday(timestamp) -> sdf1.format(timestamp)
-                timestamp >= System.currentTimeMillis() - 86400000 -> "yesterday"
-//                    c.resources.getString(com.bandyer.android_chat_sdk.R.string.bandyer_chat_message_date_yesterday) + ", " + sdf1.format(
-//                    timestamp)
-                else -> {
-                    val cal = Calendar.getInstance()
-                    cal.timeInMillis = timestamp
-                    sdf2.format(cal.time) + ", " + sdf1.format(timestamp)
-                }
+                zonedDateTime.isToday() -> parseTime(timestamp)
+                zonedDateTime.isYesterday() -> context.resources.getString(R.string.bandyer_yesterday) + ", " + parseTime(timestamp)
+                else -> zonedDateTime.dayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault()).capitalize(Locale.getDefault()) + ", " + parseTime(timestamp)
             }
-        } else createDate(timestamp)
+        } else DateTimeFormatter
+            .ofLocalizedDateTime(FormatStyle.SHORT)
+            .withLocale(Locale.getDefault())
+            .withZone(ZoneId.systemDefault())
+            .format(Instant.ofEpochMilli(timestamp))
     }
 
-    fun parseTimestampToIso8601(timestamp: Long): String {
-        val date = Date()
-        date.time = timestamp
-        return dateFormat.format(date).toString()
-    }
+    /**
+     * Parse a UTC millis timestamp into a human readable day. This function takes into account the current zone offset.
+     *
+     * @param context The context
+     * @param timestamp The timestamp in millis
+     * @return String A human readable date day string
+     */
+    fun parseDay(context: Context, timestamp: Long): String {
+        val zonedDateTime = Instant
+            .ofEpochMilli(timestamp)
+            .atZone(ZoneId.systemDefault())
 
-    fun Date.isLastWeek(): Boolean = this.time >= System.currentTimeMillis() - 604800000
-
-    fun Date.isYesterday(): Boolean = this.time >= System.currentTimeMillis() - 86400000
-
-    private fun getDateCurrentTimeZone(timestamp: Long): Date {
-        try {
-            val calendar = Calendar.getInstance(Locale.getDefault())
-            calendar.time = Date(timestamp)
-            return calendar.time as Date
-        } catch (e: Exception) {
-        }
-        return Date()
-    }
-
-    /** @suppress
-     * Transform ISO 8601 tstamp to human readable only displaying day. This function has been modified to calculate UTC + offset  */
-    fun parseDay(c: Context, timestamp: Long): String {
-        // check if is last week
         return when {
-            DateUtils.isToday(timestamp) -> "today"
-//                c.resources.getString(R.string.bandyer_chat_message_date_today)
-            timestamp >= System.currentTimeMillis() - 86400000 -> "yesterday"
-//                c.resources.getString(R.string.bandyer_chat_message_date_yesterday)
-            else -> return createDateForDay(timestamp)
+            zonedDateTime.isToday() -> context.resources.getString(R.string.bandyer_today)
+            zonedDateTime.isYesterday() -> context.resources.getString(R.string.bandyer_yesterday)
+            else -> DateTimeFormatter
+                .ofLocalizedDate(FormatStyle.LONG)
+                .withLocale(Locale.getDefault())
+                .withZone(ZoneId.systemDefault())
+                .format(Instant.ofEpochMilli(timestamp))
         }
     }
 
-    /** @suppress
-     * Transform ISO 8601 tstamp to human readable only displaying day. This function has been modified to calculate UTC + offset  */
-    fun parseTime(timestamp: Long, mgtOffset: Boolean): String = sdf1.format(timestamp)
+    /**
+     * Parse a UTC millis timestamp into a human readable time. This function takes into account the current zone offset.
+     *
+     * @param timestamp The timestamp in millis
+     * @return String A human readable date day string
+     */
+    fun parseTime(timestamp: Long): String =
+        DateTimeFormatter
+            .ofLocalizedTime(FormatStyle.SHORT)
+            .withLocale(Locale.getDefault())
+            .withZone(ZoneId.systemDefault())
+            .format(Instant.ofEpochMilli(timestamp))
 
     /**
-     * @suppress
+     * ZonedDateTime extension function. Check if the instant resides in the last week time period.
+     *
+     * @receiver Instant
+     * @return Boolean True is the ZonedDateTime resides in the last week time period, false otherwise
      */
-    private fun createDate(timestamp: Long): String =
-        getModifiedDate(Locale.getDefault(), timestamp)
+    fun ZonedDateTime.isLastWeek(): Boolean =
+        this.isAfter(
+            ZonedDateTime
+                .now(this.zone)
+                .minus(WEEK_MILLIS, ChronoUnit.MILLIS)
+                .setMidnight()
+        )
 
     /**
-     * @suppress
+     * ZonedDateTime extension function. Check if the instant resides in the yesterday time period.
+     *
+     * @receiver Instant
+     * @return Boolean True is the ZonedDateTime resides in the yesterday time period, false otherwise
      */
-    private fun createDateForDay(timestamp: Long): String =
-        getModifiedDateForDay(Locale.getDefault(), timestamp)
-
-    /**
-     * @suppress
-     */
-    private fun getModifiedDateForDay(locale: Locale, modified: Long): String {
-        val instant = Date()
-        instant.time = modified
-        return java.text.DateFormat.getDateInstance(java.text.DateFormat.LONG, locale)
-            .format(instant)
-    }
-
-    /**
-     * @suppress
-     */
-    private fun getModifiedDate(locale: Locale, modified: Long): String {
-//        if (dateFormat == null)
-//            dateFormat = SimpleDateFormat(getDateFormat(locale), Locale.getDefault())
-
-        val instant = Date().apply {
-            time = modified
-        }
-        return java.text.DateFormat.getDateTimeInstance(
-            java.text.DateFormat.SHORT,
-            java.text.DateFormat.SHORT,
-            locale
-        ).format(instant)
-//        return java.text.DateFormat.getDateInstance(java.text.DateFormat.SHORT, locale)
-//            .format(instant) + " " + dateFormat!!.format(Date(modified))
-    }
-
-    /**
-     * @suppress
-     */
-//    private fun getDateFormat(locale: Locale): String {
-//        if (bestDayFormat == "")
-//            bestDayFormat = DateFormat.getBestDateTimePattern(
-//                locale,
-//                if (usesAmPm(Locale.getDefault())) twelveHoursPattern else twentyfourHoursPattern
-//            )
-//        return bestDayFormat
-//    }
+    fun ZonedDateTime.isYesterday(): Boolean =
+        this.isAfter(
+            ZonedDateTime
+                .now(this.zone)
+                .minus(DAY_MILLIS, ChronoUnit.MILLIS)
+                .setMidnight()
+        ) && !this.isToday()
 
 
     /**
-     * @suppress
+     * ZonedDateTime extension function. Check if the instant resides in the current day time period.
+     *
+     * @receiver Instant
+     * @return Boolean True is the ZonedDateTime resides in the current day time period, false otherwise
      */
-    private fun usesAmPm(locale: Locale): Boolean =
-        (SimpleDateFormat.getTimeInstance(
-            java.text.DateFormat.FULL,
-            locale
-        ) as? SimpleDateFormat)?.toPattern()?.contains("a") == true
+    fun ZonedDateTime.isToday(): Boolean =
+        this.isAfter(
+            ZonedDateTime
+                .now(this.zone)
+                .setMidnight()
+        )
 
+    private fun ZonedDateTime.setMidnight() =
+        this.withHour(0)
+            .withMinute(0)
+            .withSecond(0)
+            .withNano(0)
 }
