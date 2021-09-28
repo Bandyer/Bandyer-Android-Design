3d4702135542005dbec76fd2a8f3e14692df6ac4
Stefano <stefano.brusa@bandyer.com>
2021-09-28 12:29:21 +0200

    refactor ParticipantsFragment

--- glass-ui/src/main/java/com/bandyer/video_android_glass_ui/call/participants/ParticipantsFragment.kt
+++ glass-ui/src/main/java/com/bandyer/video_android_glass_ui/call/participants/ParticipantsFragment.kt
@@ -5,38 +5,96 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
-import com.bandyer.video_android_glass_ui.databinding.BandyerFragmentParticipantsBinding
-import com.bandyer.video_android_glass_ui.bottom_navigation.BottomNavigationView
-import com.bandyer.video_android_glass_ui.common.AvatarView
-import com.bandyer.video_android_glass_ui.common.item_decoration.MenuProgressIndicator
+import com.bandyer.video_android_core_ui.extensions.StringExtensions.parseToColor
 import com.bandyer.video_android_glass_ui.BaseFragment
+import com.bandyer.video_android_glass_ui.R
+import com.bandyer.video_android_glass_ui.TouchEvent
 import com.bandyer.video_android_glass_ui.common.item_decoration.HorizontalCenterItemDecoration
+import com.bandyer.video_android_glass_ui.common.item_decoration.MenuProgressIndicator
+import com.bandyer.video_android_glass_ui.databinding.BandyerGlassFragmentParticipantsBinding
+import com.bandyer.video_android_glass_ui.participants.ParticipantData
 import com.bandyer.video_android_glass_ui.participants.ParticipantStateTextView
-import com.google.android.material.imageview.ShapeableImageView
+import com.bandyer.video_android_glass_ui.utils.TiltController
+import com.bandyer.video_android_glass_ui.utils.extensions.ContextExtensions.getAttributeResourceId
+import com.bandyer.video_android_glass_ui.utils.extensions.horizontalSmoothScrollToNext
+import com.bandyer.video_android_glass_ui.utils.extensions.horizontalSmoothScrollToPrevious
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.adapters.ItemAdapter
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 /**
- * SmartGlassParticipantsFragment. A base class for the participants fragment.
+ * ParticipantsFragment
  */
-abstract class ParticipantsFragment : BaseFragment() {
+class ParticipantsFragment : BaseFragment(), TiltController.TiltListener {
 
-    private var binding: BandyerFragmentParticipantsBinding? = null
+//    private val activity by lazy { requireActivity() as SmartGlassActivity }
 
-    protected var itemAdapter: ItemAdapter<CallParticipantItem>? = null
-    protected var fastAdapter: FastAdapter<CallParticipantItem>? = null
+    private var _binding: BandyerGlassFragmentParticipantsBinding? = null
+    override val binding: BandyerGlassFragmentParticipantsBinding get() = _binding!!
 
-    protected var root: View? = null
-    protected var avatar: AvatarView? = null
-    protected var contactStateDot: ShapeableImageView? = null
-    protected var contactStateText: ParticipantStateTextView? = null
-    protected var rvParticipants: RecyclerView? = null
-    protected var bottomNavigation: BottomNavigationView? = null
+    private var itemAdapter: ItemAdapter<CallParticipantItem>? = null
 
-    protected var snapHelper: LinearSnapHelper? = null
+    private var tiltController: TiltController? = null
+
+    private var currentParticipantIndex = -1
+
+    private var participantsData: List<ParticipantData> = listOf(
+            ParticipantData(
+                "Mario Rossi",
+                "Mario Rossi",
+                ParticipantData.UserState.ONLINE,
+                null,
+                null,
+                Instant.now().toEpochMilli()
+            ),
+            ParticipantData(
+                "Felice Trapasso",
+                "Felice Trapasso",
+                ParticipantData.UserState.OFFLINE,
+                null,
+                "https://i.imgur.com/9I2qAlW.jpeg",
+                Instant.now().minus(8, ChronoUnit.DAYS).toEpochMilli()
+            ),
+            ParticipantData(
+                "Francesco Sala",
+                "Francesco Sala",
+                ParticipantData.UserState.INVITED,
+                null,
+                null,
+                Instant.now().toEpochMilli()
+            )
+        )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        tiltController = TiltController(requireContext(), this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        tiltController!!.requestAllSensors()
+//        activity.showStatusBarCenteredTitle()
+//        activity.setStatusBarColor(
+//            ResourcesCompat.getColor(
+//                resources,
+//                R.color.bandyer_glass_background_color,
+//                null
+//            )
+//        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        tiltController!!.releaseAllSensors()
+//        activity.hideStatusBarCenteredTitle()
+//        activity.setStatusBarColor(null)
+    }
 
     /**
      * @suppress
@@ -46,43 +104,75 @@ abstract class ParticipantsFragment : BaseFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = BandyerFragmentParticipantsBinding.inflate(inflater, container, false)
+//        activity.addNotificationListener(this)
 
-        // set the views
-        root = binding!!.root
-        avatar = binding!!.bandyerAvatar
-        contactStateDot = binding!!.bandyerContactStateDot
-        contactStateText = binding!!.bandyerContactStateText
-        rvParticipants = binding!!.bandyerParticipants
-        bottomNavigation = binding!!.bandyerBottomActionBar
-
-        // init the recycler view
-        itemAdapter = ItemAdapter()
-        fastAdapter = FastAdapter.with(itemAdapter!!)
-        val layoutManager =
-            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        rvParticipants!!.apply {
-            this.layoutManager = layoutManager
-            this.adapter = fastAdapter
-            this.isFocusable = false
-            this.addItemDecoration(HorizontalCenterItemDecoration())
-        }
-
-        snapHelper = LinearSnapHelper().apply {
-            attachToRecyclerView(rvParticipants)
-        }
-
-        rvParticipants!!.addItemDecoration(
-            MenuProgressIndicator(
-                requireContext(),
-                snapHelper!!
-            )
+        // Apply theme wrapper and add view binding
+        val themeResId = requireActivity().theme.getAttributeResourceId(R.attr.bandyer_participantsStyle)
+        _binding = BandyerGlassFragmentParticipantsBinding.inflate(
+            inflater.cloneInContext(ContextThemeWrapper(requireContext(), themeResId)),
+            container,
+            false
         )
 
-        // pass the root view's touch event to the recycler view
-        root!!.setOnTouchListener { _, event -> rvParticipants!!.onTouchEvent(event) }
+        // Set OnClickListeners for realwear voice commands
+        with(binding.bandyerBottomNavigation) {
+            setSwipeDownOnClickListener { onSwipeDown()}
+            setSwipeHorizontalOnClickListener { onSwipeForward(true) }
+        }
 
-        return root!!
+        // Init the RecyclerView
+        with(binding.bandyerParticipants) {
+            itemAdapter = ItemAdapter()
+            val fastAdapter = FastAdapter.with(itemAdapter!!)
+            val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            val snapHelper = LinearSnapHelper().also { it.attachToRecyclerView(this) }
+
+            this.layoutManager = layoutManager
+            adapter = fastAdapter
+            isFocusable = false
+
+            addItemDecoration(HorizontalCenterItemDecoration())
+            addItemDecoration(MenuProgressIndicator(requireContext(), snapHelper))
+
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    val foundView = snapHelper.findSnapView(layoutManager) ?: return
+                    currentParticipantIndex = layoutManager.getPosition(foundView)
+
+                    val data = participantsData[currentParticipantIndex]
+
+                    with(binding.bandyerAvatar) {
+                        setText(data.name.first().toString())
+                        setBackground(data.userAlias.parseToColor())
+
+                        when {
+                            data.avatarImageId != null -> setImage(data.avatarImageId)
+                            data.avatarImageUrl != null -> setImage(data.avatarImageUrl)
+                            else -> setImage(null)
+                        }
+                    }
+
+                    binding.bandyerContactStateDot.isActivated = data.userState == ParticipantData.UserState.ONLINE
+
+                    with(binding.bandyerContactStateText) {
+                        when (data.userState) {
+                            ParticipantData.UserState.INVITED -> setContactState(ParticipantStateTextView.State.INVITED)
+                            ParticipantData.UserState.OFFLINE -> setContactState(ParticipantStateTextView.State.LAST_SEEN, data.lastSeenTime)
+                            ParticipantData.UserState.ONLINE  -> setContactState(ParticipantStateTextView.State.ONLINE)
+                        }
+                    }
+                }
+            })
+
+            // Forward the root view's touch event to the recycler view
+            binding.root.setOnTouchListener { _, event -> onTouchEvent(event) }
+        }
+
+        participantsData.forEach {
+            itemAdapter!!.add(CallParticipantItem(it.userAlias))
+        }
+
+        return binding.root
     }
 
     /**
@@ -90,16 +180,36 @@ abstract class ParticipantsFragment : BaseFragment() {
      */
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
         itemAdapter = null
-        fastAdapter = null
-        binding = null
-        root = null
-        rvParticipants = null
-        bottomNavigation = null
-        avatar = null
-        contactStateDot = null
-        contactStateText = null
-        snapHelper = null
+//        activity.removeNotificationListener(this)
+    }
+
+    override fun onTilt(deltaAzimuth: Float, deltaPitch: Float, deltaRoll: Float) =
+        binding.bandyerParticipants.scrollBy((deltaAzimuth * resources.displayMetrics.densityDpi / 5).toInt(), 0)
+
+    override fun onTouch(event: TouchEvent): Boolean = when (event.type) {
+        TouchEvent.Type.SWIPE_DOWN      -> onSwipeDown()
+        TouchEvent.Type.SWIPE_FORWARD   -> onSwipeForward(event.source == TouchEvent.Source.KEY)
+        TouchEvent.Type.SWIPE_BACKWARD  -> onSwipeBackward(event.source == TouchEvent.Source.KEY)
+        else -> super.onTouch(event)
+    }
+
+    private fun onSwipeDown(): Boolean {
+        findNavController().popBackStack()
+        return true
+    }
+
+    private fun onSwipeForward(isKeyEvent: Boolean): Boolean {
+        if (isKeyEvent && currentParticipantIndex != -1)
+            binding.bandyerParticipants.horizontalSmoothScrollToNext(currentParticipantIndex)
+        return isKeyEvent
+    }
+
+    private fun onSwipeBackward(isKeyEvent: Boolean): Boolean {
+        if (isKeyEvent && currentParticipantIndex != -1)
+            binding.bandyerParticipants.horizontalSmoothScrollToPrevious(currentParticipantIndex)
+        return isKeyEvent
     }
 }
 
