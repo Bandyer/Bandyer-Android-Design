48aadcca4431742ee503f4b16bda63d17125d958
Stefano <bpbrusad@gmail.com>
2021-11-05 18:00:35 +0100

    refactor CallAudioManager

--- glass-ui/src/main/java/com/bandyer/video_android_glass_ui/utils/CallAudioManager.kt
+++ glass-ui/src/main/java/com/bandyer/video_android_glass_ui/utils/CallAudioManager.kt
@@ -1,6 +1,7 @@
 package com.bandyer.video_android_glass_ui.utils
 
 import android.content.Context
+import android.os.Build
 import androidx.annotation.IntRange
 
 /**
@@ -19,17 +20,22 @@ internal class CallAudioManager(context: Context) {
         get() = manager.getStreamVolume(android.media.AudioManager.STREAM_VOICE_CALL)
 
     /**
+     * The max call volume
+     */
+    val maxVolume = manager.getStreamMaxVolume(android.media.AudioManager.STREAM_VOICE_CALL)
+
+    /**
+     * The min call volume
+     */
+    val minVolume = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) manager.getStreamMinVolume(android.media.AudioManager.STREAM_VOICE_CALL) else 1
+
+    /**
      * Set the call volume. The accepted values are from 0 to 5.
      *
      * @param value Int
      */
-    fun setVolume(@IntRange(from = 0, to = 5) value: Int) =
-        when {
-            value < 0 || value > 5 -> Unit
-            value == 0 -> {
-                manager.setStreamVolume(android.media.AudioManager.STREAM_VOICE_CALL, 1, 0)
-                manager.adjustStreamVolume(android.media.AudioManager.STREAM_VOICE_CALL, android.media.AudioManager.ADJUST_LOWER, 0)
-            }
-            else -> manager.setStreamVolume(android.media.AudioManager.STREAM_VOICE_CALL, value, 0)
-        }
+    fun setVolume(value: Int) {
+        if(value < minVolume || value > maxVolume) return
+        manager.setStreamVolume(android.media.AudioManager.STREAM_VOICE_CALL, value, 0)
+    }
 }
