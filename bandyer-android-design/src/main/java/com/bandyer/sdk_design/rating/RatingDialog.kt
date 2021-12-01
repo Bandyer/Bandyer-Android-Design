6e4aa1c32a151ab436b5013988b29dcf8700623b
Stefano <bpbrusad@gmail.com>
2021-12-01 17:21:13 +0100

    add rating dialog

---
+++ bandyer-android-design/src/main/java/com/bandyer/sdk_design/rating/RatingDialog.kt
@@ -0,0 +1,26 @@
+package com.bandyer.sdk_design.rating
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.bandyer.sdk_design.databinding.BandyerRatingDialogLayoutBinding
+
+class RatingDialog : DialogFragment() {
+
+    private var _binding: BandyerRatingDialogLayoutBinding? = null
+    private val binding: BandyerRatingDialogLayoutBinding
+        get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = BandyerRatingDialogLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
