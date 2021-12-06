755f7cc2182569401780f35d48dd56051f86c2e1
Stefano <bpbrusad@gmail.com>
2021-12-06 19:03:28 +0100

    refactor RateFragment

--- bandyer-android-design/src/main/java/com/bandyer/sdk_design/rating/RateFragment.kt
+++ bandyer-android-design/src/main/java/com/bandyer/sdk_design/rating/RateFragment.kt
@@ -1,10 +1,13 @@
 package com.bandyer.sdk_design.rating
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.bandyer.sdk_design.R
 import com.bandyer.sdk_design.databinding.FragmentRateBinding
 
 class RateFragment : Fragment() {
@@ -13,11 +16,18 @@ class RateFragment : Fragment() {
     private val binding: FragmentRateBinding
         get() = _binding!!
 
+    private val parentDialogFragment: DialogFragment
+        get() = requireParentFragment().requireParentFragment() as DialogFragment
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentRateBinding.inflate(inflater, container, false)
+        _binding = FragmentRateBinding.inflate(inflater, container, false).apply {
+            bandyerClose.setOnClickListener { parentDialogFragment.dismiss() }
+            bandyerButton.setOnClickListener { findNavController().navigate(R.id.action_rateFragment_to_rateAfterFragment)  }
+        }
+
         return binding.root
     }
 }
