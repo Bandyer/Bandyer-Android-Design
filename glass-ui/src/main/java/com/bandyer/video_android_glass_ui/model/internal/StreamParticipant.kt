07c0b0495f9fb607cae8f4f9ad56986c1a167627
Stefano <bpbrusad@gmail.com>
2022-01-21 10:35:32 +0100

    remove hashcode and equals override from StreamParticipant

--- glass-ui/src/main/java/com/bandyer/video_android_glass_ui/model/internal/StreamParticipant.kt
+++ glass-ui/src/main/java/com/bandyer/video_android_glass_ui/model/internal/StreamParticipant.kt
@@ -7,20 +7,4 @@ internal data class StreamParticipant(
     val participant: CallParticipant,
     val isMyStream: Boolean,
     val stream: Stream?
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is StreamParticipant) return false
-
-        if (participant.userAlias != other.participant.userAlias) return false
-        if (stream?.id != other.stream?.id) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = participant.userAlias.hashCode()
-        result = 31 * result + stream?.id.hashCode()
-        return result
-    }
-}
+)
