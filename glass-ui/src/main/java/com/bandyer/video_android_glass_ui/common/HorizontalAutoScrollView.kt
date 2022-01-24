a6f818932850cb51192b2a8323973f2cdafa3954
Stefano <bpbrusad@gmail.com>
2022-01-24 10:32:38 +0100

    refactor HorizontalAutoScrollView to better support user scroll and improve behaviour

--- glass-ui/src/main/java/com/bandyer/video_android_glass_ui/common/HorizontalAutoScrollView.kt
+++ glass-ui/src/main/java/com/bandyer/video_android_glass_ui/common/HorizontalAutoScrollView.kt
@@ -1,16 +1,14 @@
 package com.bandyer.video_android_glass_ui.common
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Handler
 import android.os.Looper
 import android.util.AttributeSet
 import android.view.MotionEvent
+import android.view.View
 import android.widget.HorizontalScrollView
-import android.animation.ObjectAnimator
-import android.view.ViewTreeObserver
-import android.view.animation.LinearInterpolator
-import androidx.core.view.ViewCompat
 import kotlin.math.abs
 
 // TODO Add RTL support and onKeyEvent support
@@ -18,43 +16,50 @@ internal class HorizontalAutoScrollView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : HorizontalScrollView(context, attrs, defStyleAttr), ViewTreeObserver.OnScrollChangedListener {
+) : HorizontalScrollView(context, attrs, defStyleAttr) {
 
-    private var mainHandler: Handler? = Handler(Looper.getMainLooper())
-
+    private var mainHandler: Handler? = null
     private var animator: ObjectAnimator? = null
 
-    private var manual = false
+    private var lastScrollPosition = 0
+    private val scrollStopMs = 100L
+    private var scrollStopRunner: Runnable? = null
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        viewTreeObserver.addOnScrollChangedListener(this)
+        mainHandler = Handler(Looper.getMainLooper())
+        scrollStopRunner = object : Runnable {
+            override fun run() {
+                val currentScrollPosition = scrollX
+                if (lastScrollPosition == currentScrollPosition) {
+                    performAutoScroll()
+                } else {
+                    lastScrollPosition = currentScrollPosition
+                    mainHandler?.postDelayed(this, scrollStopMs)
+                }
+            }
+        }
+        performAutoScroll()
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-        viewTreeObserver.removeOnScrollChangedListener(this)
         animator?.cancel()
         mainHandler?.removeCallbacksAndMessages(null)
         animator = null
+        scrollStopRunner = null
         mainHandler = null
-    }
-
-    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        super.onLayout(changed, l, t, r, b)
-        if (!ViewCompat.isLaidOut(this)) return
-        performAutoScroll()
     }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(ev: MotionEvent?): Boolean {
-        when (ev?.action) {
-            MotionEvent.ACTION_UP -> {
-                manual = true
-            }
-            MotionEvent.ACTION_DOWN -> {
-                animator?.cancel()
-                mainHandler?.removeCallbacksAndMessages(null)
+        if (ev != null && mainHandler != null && scrollStopRunner != null && animator != null) {
+            when (ev.action) {
+                MotionEvent.ACTION_UP -> mainHandler!!.postDelayed(scrollStopRunner!!, scrollStopMs)
+                MotionEvent.ACTION_DOWN -> {
+                    animator!!.cancel()
+                    mainHandler!!.removeCallbacksAndMessages(null)
+                }
             }
         }
 
@@ -64,8 +69,6 @@ internal class HorizontalAutoScrollView @JvmOverloads constructor(
     private fun performAutoScroll() {
         animator?.cancel()
         mainHandler?.removeCallbacksAndMessages(null)
-        manual = false
-
         val maxScroll = (getChildAt(0).width - width)
         val duration = maxScroll * 4L
         val target = if (abs(scrollX - maxScroll) < 5) 0 else maxScroll
@@ -74,10 +77,5 @@ internal class HorizontalAutoScrollView @JvmOverloads constructor(
             start()
         }
         mainHandler?.postDelayed({ performAutoScroll() }, duration)
-    }
-
-    override fun onScrollChanged() {
-        if (!manual) return
-        mainHandler?.postDelayed({ performAutoScroll() }, 500)
     }
 }
