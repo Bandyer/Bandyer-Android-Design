0dc4baa1456ce13d8900fe080b48f30d5acfddf8
Stefano <bpbrusad@gmail.com>
2022-01-24 18:09:45 +0100

    update CallEndedFragment

--- glass-ui/src/main/java/com/bandyer/video_android_glass_ui/call/CallEndedFragment.kt
+++ glass-ui/src/main/java/com/bandyer/video_android_glass_ui/call/CallEndedFragment.kt
@@ -45,6 +45,8 @@ internal class CallEndedFragment : BaseFragment() {
             false
         ).apply {
             if(GlassDeviceUtils.isRealWear) bandyerBottomNavigation.setListenersForRealwear()
+            bandyerProgress.visibility = View.GONE
+            bandyerParticipantsScrollView.visibility = View.GONE
             bandyerTitle.text = args.title
             bandyerSubtitle.text = args.subtitle
         }
