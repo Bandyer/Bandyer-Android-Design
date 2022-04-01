e08c226d080ea0c995c91aa18d686fb801d3309e
Stefano <bpbrusad@gmail.com>
2022-04-01 17:04:05 +0200

    fix roundBitmap function

--- collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/utils/BitmapUtils.kt
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/utils/BitmapUtils.kt
@@ -1,6 +1,5 @@
 package com.kaleyra.collaboration_suite_core_ui.utils
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
@@ -10,15 +9,13 @@ import android.graphics.PorterDuffXfermode
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import androidx.annotation.Px
-import com.squareup.picasso.MemoryPolicy
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.Target
-import kotlinx.coroutines.suspendCancellableCoroutine
 
 object BitmapUtils {
     fun roundBitmap(bitmap: Bitmap): Bitmap {
-        val mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
-        val canvas = Canvas(mutable)
+        val newBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val canvas = Canvas(newBitmap)
         val path = Path().apply {
             val halfWidth = bitmap.width / 2f
             val halfHeight = bitmap.width / 2f
@@ -29,7 +26,7 @@ object BitmapUtils {
             xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
         }
         canvas.drawPath(path, paint)
-        return bitmap
+        return newBitmap
     }
 
 //    suspend fun uriToBitmap(uri: Uri, @Px width: Int, @Px height: Int) =
