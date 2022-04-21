b7d7ced2b032fee87e049e1cc3be7965017ecb92
Stefano <bpbrusad@gmail.com>
2022-04-21 12:38:35 +0200

    add zoom fragment realwear style

--- collaboration-suite-glass-ui/src/main/java/com/kaleyra/collaboration_suite_glass_ui/settings/zoom/ZoomFragment.kt
+++ collaboration-suite-glass-ui/src/main/java/com/kaleyra/collaboration_suite_glass_ui/settings/zoom/ZoomFragment.kt
@@ -21,13 +21,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.view.ContextThemeWrapper
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.kaleyra.collaboration_suite_core_ui.utils.DeviceUtils
 import com.kaleyra.collaboration_suite_glass_ui.BaseFragment
 import com.kaleyra.collaboration_suite_glass_ui.GlassViewModel
+import com.kaleyra.collaboration_suite_glass_ui.R
 import com.kaleyra.collaboration_suite_glass_ui.common.SettingSlider
 import com.kaleyra.collaboration_suite_glass_ui.databinding.KaleyraGlassFragmentZoomBinding
+import com.kaleyra.collaboration_suite_glass_ui.utils.extensions.ContextExtensions.getAttributeResourceId
 import kotlin.math.roundToInt
 
 /**
@@ -50,11 +53,15 @@ internal class ZoomFragment : BaseFragment() {
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
 
+        val themeResId =
+            requireActivity().theme.getAttributeResourceId(if (DeviceUtils.isRealWear) R.attr.kaleyra_zoomRealWearStyle else R.attr.kaleyra_zoomStyle)
         // Add view binding
         _binding = KaleyraGlassFragmentZoomBinding
-            .inflate(inflater, container, false)
-            .apply {
-                if(DeviceUtils.isRealWear) kaleyraBottomNavigation.setListenersForRealWear()
+            .inflate(
+                inflater.cloneInContext(ContextThemeWrapper(requireContext(), themeResId)),
+                container,
+                false
+            ).apply {
                 if (DeviceUtils.isRealWear) setListenersForRealWear(kaleyraBottomNavigation)
                 root.setOnTouchListener { _, _ -> true }
             }
@@ -68,7 +75,8 @@ internal class ZoomFragment : BaseFragment() {
             val upperValue = viewModel.zoom!!.range.upper
             val lowerValue = viewModel.zoom!!.range.lower
             maxProgress = MAX_ZOOM_PROGRESS
-            progress = (((currentValue - lowerValue) * MAX_ZOOM_PROGRESS) / (upperValue - lowerValue)).roundToInt()
+            progress =
+                (((currentValue - lowerValue) * MAX_ZOOM_PROGRESS) / (upperValue - lowerValue)).roundToInt()
 
             onSliderChangeListener = object : SettingSlider.OnSliderChangeListener {
                 override fun onProgressChanged(progress: Int) {
@@ -92,9 +100,11 @@ internal class ZoomFragment : BaseFragment() {
 
     override fun onSwipeDown() = true.also { findNavController().popBackStack() }
 
-    override fun onSwipeForward(isKeyEvent: Boolean) = true.also { binding.kaleyraSlider.increaseProgress() }
+    override fun onSwipeForward(isKeyEvent: Boolean) =
+        true.also { binding.kaleyraSlider.increaseProgress() }
 
-    override fun onSwipeBackward(isKeyEvent: Boolean) = true.also { binding.kaleyraSlider.decreaseProgress() }
+    override fun onSwipeBackward(isKeyEvent: Boolean) =
+        true.also { binding.kaleyraSlider.decreaseProgress() }
 
     private companion object {
         const val MAX_ZOOM_PROGRESS = 20
