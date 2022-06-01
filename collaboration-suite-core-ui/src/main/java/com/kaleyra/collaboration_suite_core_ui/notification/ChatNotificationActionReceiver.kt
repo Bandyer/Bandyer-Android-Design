b51dc1de224c8cc26b1ec784bd0539da3edcef09
Stefano <bpbrusad@gmail.com>
2022-06-01 18:53:57 +0200

    add new ChatNotificationManager

--- collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/notification/ChatNotificationActionReceiver.kt
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/notification/ChatNotificationActionReceiver.kt
@@ -4,19 +4,15 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
-class MessagingNotificationReceiver: BroadcastReceiver() {
+class ChatNotificationActionReceiver: BroadcastReceiver() {
 
     /**
      * @suppress
      */
     companion object {
-        /**
-         * ActionAnswer
-         */
         const val ACTION_REPLY = "com.kaleyra.collaboration_suite_core_ui.ACTION_REPLY"
 
-        const val EXTRA_REPLY =
-            "com.kaleyra.collaboration_suite_core_ui.EXTRA_REPLY"
+        const val ACTION_DELETE = "com.kaleyra.collaboration_suite_core_ui.ACTION_DELETE"
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
--- collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/notification/ChatNotificationManager.kt
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/notification/ChatNotificationManager.kt
@@ -1,65 +1,109 @@
 package com.kaleyra.collaboration_suite_core_ui.notification
 
-import android.app.Activity
-import android.app.Application
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
-import android.os.Bundle
+import android.net.Uri
+import android.os.Build
+import com.kaleyra.collaboration_suite_core_ui.R
+import com.kaleyra.collaboration_suite_core_ui.chat.ChatActivity
+import com.kaleyra.collaboration_suite_core_ui.utils.PendingIntentExtensions
 import com.kaleyra.collaboration_suite_utils.ContextRetainer
 
-class ChatNotificationManager(private val chatNotificationActivityClazz: Class<*>) :
-    Application.ActivityLifecycleCallbacks {
-
-    private val application = ContextRetainer.context.applicationContext as? Application
-    private var context: Activity? = null
-
-    init {
-        application?.registerActivityLifecycleCallbacks(this)
-    }
-
-    /**
-     * Do Not Disturb flag. If set to true, the notifications are no longer shown.
-     */
-    var dnd: Boolean = false
-
-    fun notify(notification: ChatNotificationData) {
-        if (dnd) return
-        startNotificationActivity(notification)
-    }
-
-    private fun startNotificationActivity(notification: ChatNotificationData) {
-        val currentContext = context ?: ContextRetainer.context
-        val intent = Intent(currentContext, chatNotificationActivityClazz).apply {
-            context ?: addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            putExtra ("username", notification.name)
-            putExtra("userId", notification.userId)
-            putExtra("message", notification.message)
-            putExtra("imageUri", notification.imageUri)
-            putExtra("participants", notification.usersList.toTypedArray())
-        }
-        currentContext.startActivity(intent)
-    }
-
-    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) = Unit
-
-    override fun onActivityStarted(activity: Activity) = Unit
-
-    override fun onActivityResumed(activity: Activity) {
-        if (context == chatNotificationActivityClazz) return
-        context = activity
-    }
-
-    override fun onActivityPaused(activity: Activity) {
-        if (context != activity) return
-        context = null
-    }
-
-    override fun onActivityStopped(activity: Activity) = Unit
-
-    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
-
-    override fun onActivityDestroyed(activity: Activity) = Unit
+interface ChatNotificationManager {
 
     companion object {
-        const val AUTO_DISMISS_TIME = 3000L
+        private const val DEFAULT_CHANNEL_ID =
+            "com.kaleyra.collaboration_suite_core_ui.chat_notification_channel_default"
+
+        private const val FULL_SCREEN_REQUEST_CODE = 321
+        private const val CONTENT_REQUEST_CODE = 654
+        private const val REPLY_REQUEST_CODE = 987
+        private const val DELETE_REQUEST_CODE = 1110
+    }
+
+    fun <T : ChatActivity> buildChatNotification(
+        userId: String,
+        username: String,
+        avatar: Uri,
+        isGroupChat: Boolean,
+        messages: List<ChatNotificationMessage>,
+        activityClazz: Class<T>,
+        asActivity: Boolean
+    ): Notification {
+        val context = ContextRetainer.context
+
+
+        val contentIntent = contentPendingIntent(context, activityClazz)
+        // Pending intent =
+        //      API <24 (M and below): activity so the lock-screen presents the auth challenge.
+        //      API 24+ (N and above): this should be a Service or BroadcastReceiver.
+        val replyIntent = replyPendingIntent(context) ?: contentIntent
+
+        val builder = ChatNotification
+            .Builder(
+                context,
+                DEFAULT_CHANNEL_ID,
+                context.resources.getString(R.string.kaleyra_notification_chat_channel_name)
+            )
+            .userId(userId)
+            .username(username)
+            .avatar(avatar)
+            .isGroupChat(isGroupChat)
+            .contentIntent(contentIntent)
+            .replyIntent(replyIntent)
+            .deleteIntent(deletePendingIntent(context))
+            .messages(messages)
+
+        return builder.build()
+    }
+
+    private fun contentPendingIntent(context: Context, activityClazz: Class<*>) =
+        createChatActivityPendingIntent(context, CONTENT_REQUEST_CODE, activityClazz)
+
+    private fun <T> createChatActivityPendingIntent(
+        context: Context,
+        requestCode: Int,
+        activityClazz: Class<T>
+    ): PendingIntent {
+        val applicationContext = context.applicationContext
+        val intent = Intent(applicationContext, activityClazz).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        return PendingIntent.getActivity(
+            applicationContext,
+            requestCode,
+            intent,
+            PendingIntentExtensions.updateFlags
+        )
+    }
+
+    private fun replyPendingIntent(context: Context) =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            val intent = Intent(
+                context.applicationContext,
+                ChatNotificationActionReceiver::class.java
+            ).apply {
+                action = ChatNotificationActionReceiver.ACTION_REPLY
+            }
+            PendingIntent.getBroadcast(
+                context.applicationContext,
+                REPLY_REQUEST_CODE,
+                intent,
+                PendingIntentExtensions.mutableFlags
+            )
+        } else null
+
+    private fun deletePendingIntent(context: Context): PendingIntent {
+        val intent = Intent(context, ChatNotificationActionReceiver::class.java).apply {
+            this.action = ChatNotificationActionReceiver.ACTION_DELETE
+        }
+        return PendingIntent.getBroadcast(
+            context,
+            DELETE_REQUEST_CODE,
+            intent,
+            PendingIntentExtensions.updateFlags
+        )
     }
 }
