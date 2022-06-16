41a76c84875234250ee64f7513a47322fe573543
Stefano <bpbrusad@gmail.com>
2022-06-16 16:08:57 +0200

    pass context to battery and wifi observer

--- collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/DeviceStatusObserver.kt
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/DeviceStatusObserver.kt
@@ -1,6 +1,7 @@
 package com.kaleyra.collaboration_suite_core_ui
 
 import com.kaleyra.collaboration_suite_core_ui.common.DeviceStatusDelegate
+import com.kaleyra.collaboration_suite_utils.ContextRetainer
 import com.kaleyra.collaboration_suite_utils.battery_observer.BatteryInfo
 import com.kaleyra.collaboration_suite_utils.battery_observer.BatteryObserver
 import com.kaleyra.collaboration_suite_utils.network_observer.WiFiInfo
@@ -9,12 +10,12 @@ import kotlinx.coroutines.flow.SharedFlow
 
 class DeviceStatusObserver: DeviceStatusDelegate {
 
-    private val batteryObserver = BatteryObserver()
+    private val batteryObserver = BatteryObserver(ContextRetainer.context)
 
-    private val wiFiObserver = WiFiObserver()
+    private val wiFiObserver = WiFiObserver(ContextRetainer.context)
 
-    override val battery: SharedFlow<BatteryInfo> = batteryObserver.batteryInfo
-    override val wifi: SharedFlow<WiFiInfo> = wiFiObserver.wifiInfo
+    override val battery: SharedFlow<BatteryInfo> = batteryObserver.observe()
+    override val wifi: SharedFlow<WiFiInfo> = wiFiObserver.observe()
 
     fun start() {
         batteryObserver.start()
