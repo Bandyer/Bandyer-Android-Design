4b76c8cd280e7cb0d004bdcab243dd3ed6991192
Stefano <bpbrusad@gmail.com>
2022-07-27 18:30:39 +0200

    refactor GlassChatActivity to use ChatActivity

--- collaboration-suite-glass-ui/src/main/java/com/kaleyra/collaboration_suite_glass_ui/chat/GlassChatActivity.kt
+++ collaboration-suite-glass-ui/src/main/java/com/kaleyra/collaboration_suite_glass_ui/chat/GlassChatActivity.kt
@@ -1,18 +1,15 @@
 package com.kaleyra.collaboration_suite_glass_ui.chat
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.MotionEvent
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.kaleyra.collaboration_suite.chatbox.ChatBox
-import com.kaleyra.collaboration_suite_core_ui.notification.DisplayedChatActivity
+import com.kaleyra.collaboration_suite_core_ui.ChatActivity
 import com.kaleyra.collaboration_suite_core_ui.utils.DeviceUtils
 import com.kaleyra.collaboration_suite_core_ui.utils.extensions.ActivityExtensions.turnScreenOff
 import com.kaleyra.collaboration_suite_core_ui.utils.extensions.ActivityExtensions.turnScreenOn
-import com.kaleyra.collaboration_suite_core_ui.utils.extensions.ContextExtensions.goToLaunchingActivity
-import com.kaleyra.collaboration_suite_glass_ui.GlassBaseActivity
 import com.kaleyra.collaboration_suite_glass_ui.GlassTouchEventManager
 import com.kaleyra.collaboration_suite_glass_ui.TouchEvent
 import com.kaleyra.collaboration_suite_glass_ui.TouchEventListener
@@ -22,19 +19,16 @@ import com.kaleyra.collaboration_suite_glass_ui.status_bar_views.StatusBarView
 import com.kaleyra.collaboration_suite_glass_ui.utils.currentNavigationFragment
 import com.kaleyra.collaboration_suite_glass_ui.utils.extensions.ActivityExtensions.enableImmersiveMode
 import com.kaleyra.collaboration_suite_glass_ui.utils.extensions.LifecycleOwnerExtensions.repeatOnStarted
-import com.kaleyra.collaboration_suite_utils.ContextRetainer
 import com.kaleyra.collaboration_suite_utils.battery_observer.BatteryInfo
 import com.kaleyra.collaboration_suite_utils.network_observer.WiFiInfo
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.take
 
-internal class GlassChatActivity : GlassBaseActivity(), OnDestinationChangedListener,
-    GlassTouchEventManager.Listener {
+internal class GlassChatActivity : ChatActivity(), GlassTouchEventManager.Listener {
 
     private lateinit var binding: KaleyraChatActivityGlassBinding
 
-    private val viewModel: GlassChatViewModel by viewModels()
+    override val viewModel: GlassChatViewModel by viewModels()
 
     private var glassTouchEventManager: GlassTouchEventManager? = null
 
@@ -43,18 +37,8 @@ internal class GlassChatActivity : GlassBaseActivity(), OnDestinationChangedList
         binding = KaleyraChatActivityGlassBinding.inflate(layoutInflater)
         setContentView(binding.root)
         glassTouchEventManager = GlassTouchEventManager(this, this)
-        if (DeviceUtils.isSmartGlass) enableImmersiveMode()
+        enableImmersiveMode()
         turnScreenOn()
-
-        viewModel.isCollaborationConfigured
-            .take(1)
-            .onEach {
-                if (it) onNewChatIntent(intent)
-                else {
-                    finishAndRemoveTask()
-                    ContextRetainer.context.goToLaunchingActivity()
-                }
-            }.launchIn(lifecycleScope)
 
         viewModel.chatBoxState
             .onEach {
@@ -90,19 +74,11 @@ internal class GlassChatActivity : GlassBaseActivity(), OnDestinationChangedList
         }
     }
 
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        onNewChatIntent(intent)
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         turnScreenOff()
         glassTouchEventManager = null
-        sendCustomNotificationBroadcast(DisplayedChatActivity.ACTION_CHAT_CLOSE)
     }
-
-    override fun onDestinationChanged(destinationId: Int) = Unit
 
     /**
      * @suppress
@@ -123,18 +99,5 @@ internal class GlassChatActivity : GlassBaseActivity(), OnDestinationChangedList
             supportFragmentManager.currentNavigationFragment as? TouchEventListener
                 ?: return false
         return currentDest.onTouch(glassEvent)
-    }
-
-    private fun onNewChatIntent(intent: Intent) {
-        val userId = intent.extras?.getString("userId") ?: return
-        val chat = viewModel.setChat(userId) ?: return
-        sendCustomNotificationBroadcast(DisplayedChatActivity.ACTION_CHAT_OPEN, chat.id)
-    }
-
-    private fun sendCustomNotificationBroadcast(action: String, chatId: String? = null) {
-        sendBroadcast(Intent(this, DisplayedChatActivity::class.java).apply {
-            this.action = action
-            chatId?.let { putExtra(DisplayedChatActivity.EXTRA_CHAT_ID, it) }
-        })
     }
 }
