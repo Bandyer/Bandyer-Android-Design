dbfe79bb216f4d2a7f4ac1e62b56f80d08596091
Stefano <bpbrusad@gmail.com>
2022-08-12 12:01:06 +0200

    refactor Fab Style

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/chat/PhoneChatActivity.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/chat/PhoneChatActivity.kt
@@ -11,19 +11,7 @@ import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
@@ -36,6 +24,7 @@ import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
@@ -63,7 +52,10 @@ import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -196,23 +188,38 @@ fun ChatScreen(
     }
 }
 
+@Preview
+@Composable
+fun FabPreview() {
+    ScrollToBottomFab(unreadMessagesCounter = 5, scrollState = LazyListState())
+}
+
 @Composable
 fun ScrollToBottomFab(unreadMessagesCounter: Int, scrollState: LazyListState) {
     val scope = rememberCoroutineScope()
 
     FloatingActionButton(
-        modifier = Modifier.size(40.dp),
         onClick = {
             scope.launch {
                 scrollState.scrollToItem(0)
             }
-        }
+        },
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.defaultMinSize(32.dp, 32.dp)
     ) {
-        Row {
-            if (unreadMessagesCounter > 0) Text("$unreadMessagesCounter")
+        Row(
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (unreadMessagesCounter > 0)
+                Text(
+                    text = "$unreadMessagesCounter",
+                    modifier = Modifier.paddingFromBaseline(bottom = 6.dp).padding(end = 4.dp)
+                )
             Icon(
-                imageVector = Icons.Filled.ArrowDropDown,
-                contentDescription = null
+                painter = painterResource(id = R.drawable.ic_kaleyra_double_arrow_down),
+                contentDescription = null,
+                modifier = Modifier.size(16.dp)
             )
         }
     }
