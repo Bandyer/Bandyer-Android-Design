16c9a3400ec368b7c01348282e50aa116adc1811
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2022-09-16 13:08:08 +0200

    enable focus highlight only when not in touch mode

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/chat/compose/utility/Modifiers.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/chat/compose/utility/Modifiers.kt
@@ -3,13 +3,19 @@ package com.kaleyra.collaboration_suite_phone_ui.chat.compose.utility
 import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.InputMode
+import androidx.compose.ui.platform.LocalInputModeManager
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+
+private val FocusHighlightStroke = 2.dp
+private val FocusHighlightColor = Color.Red
 
 internal fun Modifier.supportRtl(): Modifier =
     composed {
@@ -18,9 +24,11 @@ internal fun Modifier.supportRtl(): Modifier =
 
 internal fun Modifier.highlightOnFocus(interactionSource: MutableInteractionSource): Modifier =
     composed {
+        val inputModeManager = LocalInputModeManager.current
         val isFocused = interactionSource.collectIsFocusedAsState().value
+        val enableHighlight = derivedStateOf { inputModeManager.inputMode != InputMode.Touch && isFocused }.value
         border(
-            width = if (isFocused) 2.dp else 0.dp,
-            color = if (isFocused) Color.Red else Color.Transparent
+            width = if (enableHighlight) FocusHighlightStroke else 0.dp,
+            color = if (enableHighlight) FocusHighlightColor else Color.Transparent
         )
     }
