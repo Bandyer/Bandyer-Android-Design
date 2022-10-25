d0209a783f021ca8784181c45fb4935a230884a6
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2022-10-25 13:17:00 +0200

    add CallAction composable

---
+++ collaboration-suite-phone-ui/src/androidTest/java/com/kaleyra/collaboration_suite_phone_ui/CallActionTest.kt
@@ -0,0 +1,51 @@
+package com.kaleyra.collaboration_suite_phone_ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.CallAction
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CallActionTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private var toggled by mutableStateOf(false)
+
+    private var enabled by mutableStateOf(true)
+
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {
+            CallAction(
+                toggled = toggled,
+                onToggled = { toggled = it },
+                text = "",
+                icon = painterResource(id = R.drawable.ic_kaleyra_mic_off),
+                enabled = enabled
+            )
+        }
+    }
+
+    @Test
+    fun notToggled_userPerformsClick_onToggledInvoked() {
+        composeTestRule.onRoot().performClick()
+        assert(toggled)
+    }
+
+    @Test
+    fun disabled_userPerformsClick_buttonDisabled() {
+        enabled = false
+        composeTestRule.onRoot().performClick()
+        composeTestRule.onRoot().onChildAt(0).assertIsNotEnabled()
+    }
+}
---
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/CallAction.kt
@@ -0,0 +1,226 @@
+package com.kaleyra.collaboration_suite_phone_ui.call.compose
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kaleyra.collaboration_suite_phone_ui.R
+import com.kaleyra.collaboration_suite_phone_ui.chat.theme.KaleyraTheme
+
+@Composable
+internal fun CallAction(
+    toggled: Boolean,
+    onToggled: (Boolean) -> Unit,
+    text: String,
+    icon: Painter,
+    enabled: Boolean,
+    iconDescription: String = text,
+    colors: CallActionColors = CallActionDefaults.colors()
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        val backgroundColor by animateColorAsState(
+            colors.backgroundColor(toggled = toggled, enabled = enabled).value
+        )
+        val iconTint by animateColorAsState(
+            colors.iconColor(toggled = toggled, enabled = enabled).value
+        )
+        Box(
+            modifier = Modifier
+                .background(
+                    color = backgroundColor,
+                    shape = CircleShape
+                )
+                .size(CallActionDefaults.Size)
+                .toggleable(
+                    value = toggled,
+                    onValueChange = onToggled,
+                    enabled = enabled,
+                    role = Role.Checkbox,
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = rememberRipple(
+                        bounded = false,
+                        radius = CallActionDefaults.RippleRadius
+                    )
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = icon,
+                contentDescription = iconDescription,
+                tint = iconTint,
+                modifier = Modifier.size(CallActionDefaults.IconSize)
+            )
+        }
+        Text(
+            text = text,
+            color = colors.textColor(enabled = enabled).value,
+            fontSize = 12.sp,
+            textAlign = TextAlign.Center,
+            maxLines = 2
+        )
+    }
+}
+
+internal interface CallActionColors {
+    @Composable
+    fun backgroundColor(toggled: Boolean, enabled: Boolean): State<Color>
+
+    @Composable
+    fun iconColor(toggled: Boolean, enabled: Boolean): State<Color>
+
+    @Composable
+    fun textColor(enabled: Boolean): State<Color>
+}
+
+internal object CallActionDefaults {
+
+    val Size = 56.dp
+
+    val IconSize = 24.dp
+
+    val RippleRadius = 28.dp
+
+    @Composable
+    fun colors(
+        backgroundColor: Color = MaterialTheme.colors.onSurface.copy(alpha = .12f),
+        iconColor: Color = contentColorFor(backgroundColor),
+        textColor: Color = LocalContentColor.current,
+        disabledBackgroundColor: Color = MaterialTheme.colors.onSurface.copy(alpha = .12f),
+        disabledIconColor: Color = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.disabled),
+        disabledTextColor: Color = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
+        toggledBackgroundColor: Color = MaterialTheme.colors.secondaryVariant,
+        toggledIconColor: Color = contentColorFor(toggledBackgroundColor)
+    ): CallActionColors = DefaultColors(
+        backgroundColor = backgroundColor,
+        iconColor = iconColor,
+        textColor = textColor,
+        disabledBackgroundColor = disabledBackgroundColor,
+        disabledIconColor = disabledIconColor,
+        disabledTextColor = disabledTextColor,
+        toggledBackgroundColor = toggledBackgroundColor,
+        toggledIconColor = toggledIconColor
+    )
+}
+
+@Immutable
+private class DefaultColors(
+    private val backgroundColor: Color,
+    private val iconColor: Color,
+    private val textColor: Color,
+    private val disabledBackgroundColor: Color,
+    private val disabledIconColor: Color,
+    private val disabledTextColor: Color,
+    private val toggledBackgroundColor: Color,
+    private val toggledIconColor: Color,
+) : CallActionColors {
+    @Composable
+    override fun backgroundColor(toggled: Boolean, enabled: Boolean): State<Color> {
+        val color = when {
+            !enabled -> disabledBackgroundColor
+            !toggled -> backgroundColor
+            else -> toggledBackgroundColor
+        }
+        return rememberUpdatedState(color)
+    }
+
+    @Composable
+    override fun iconColor(toggled: Boolean, enabled: Boolean): State<Color> {
+        val color = when {
+            !enabled -> disabledIconColor
+            !toggled -> iconColor
+            else -> toggledIconColor
+        }
+        return rememberUpdatedState(color)
+    }
+
+    @Composable
+    override fun textColor(enabled: Boolean): State<Color> {
+        return rememberUpdatedState(if (enabled) textColor else disabledTextColor)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as DefaultColors
+
+        if (backgroundColor != other.backgroundColor) return false
+        if (iconColor != other.iconColor) return false
+        if (textColor != other.textColor) return false
+        if (disabledBackgroundColor != other.disabledBackgroundColor) return false
+        if (disabledIconColor != other.disabledIconColor) return false
+        if (disabledTextColor != other.disabledTextColor) return false
+        if (toggledBackgroundColor != other.toggledBackgroundColor) return false
+        if (toggledIconColor != other.toggledIconColor) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = backgroundColor.hashCode()
+        result = 31 * result + iconColor.hashCode()
+        result = 31 * result + textColor.hashCode()
+        result = 31 * result + disabledBackgroundColor.hashCode()
+        result = 31 * result + disabledIconColor.hashCode()
+        result = 31 * result + disabledTextColor.hashCode()
+        result = 31 * result + toggledBackgroundColor.hashCode()
+        result = 31 * result + toggledIconColor.hashCode()
+        return result
+    }
+}
+
+@Preview
+@Composable
+internal fun CallActionPreview() {
+    KaleyraTheme {
+        PreviewLayout(toggled = false, enabled = true)
+    }
+}
+
+@Preview
+@Composable
+internal fun CallActionToggledPreview() {
+    KaleyraTheme {
+        PreviewLayout(toggled = true, enabled = true)
+    }
+}
+
+@Preview
+@Composable
+internal fun CallActionDisabledPreview() {
+    KaleyraTheme {
+        PreviewLayout(toggled = true, enabled = false)
+    }
+}
+
+@Composable
+private fun PreviewLayout(toggled: Boolean, enabled: Boolean) {
+    Surface {
+        CallAction(
+            toggled = toggled,
+            onToggled = { },
+            icon = painterResource(id = R.drawable.ic_kaleyra_mic_off),
+            text = stringResource(id = R.string.kaleyra_call_action_mic_mute),
+            enabled = enabled
+        )
+    }
+}
