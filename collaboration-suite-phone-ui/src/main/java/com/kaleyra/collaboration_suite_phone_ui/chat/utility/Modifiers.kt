8396ea34ec5f11b10490fe7e086eadc7209c4ade
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2022-10-27 15:32:22 +0200

    add fadeBelowOfRootBottomBound modifier

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/chat/utility/Modifiers.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/chat/utility/Modifiers.kt
@@ -3,20 +3,29 @@ package com.kaleyra.collaboration_suite_phone_ui.chat.utility
 import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
-import androidx.compose.runtime.Stable
-import androidx.compose.runtime.derivedStateOf
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.InputMode
+import androidx.compose.ui.layout.boundsInRoot
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalInputModeManager
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.findRoot
 
 private val FocusHighlightStroke = 2.dp
 private val FocusHighlightColor = Color.Red
+
+private const val FadeVisibilityThreshold = 0.23
 
 @Stable
 internal fun Modifier.supportRtl(): Modifier =
@@ -34,4 +43,22 @@ internal fun Modifier.highlightOnFocus(interactionSource: MutableInteractionSour
             width = if (enableHighlight) FocusHighlightStroke else 0.dp,
             color = if (enableHighlight) FocusHighlightColor else Color.Transparent
         )
+    }
+
+@Stable
+internal fun Modifier.fadeBelowOfRootBottomBound(): Modifier =
+    composed {
+        val navigationBarsPadding = with(LocalDensity.current) {
+            WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding().toPx()
+        }
+        var alpha by remember { mutableStateOf(0f) }
+        this
+            .onGloballyPositioned { layoutCoordinates ->
+                val rootHeight = layoutCoordinates.findRoot().size.height
+                val boundInRoot = layoutCoordinates.boundsInRoot()
+                val height = layoutCoordinates.size.height.toFloat()
+                val out = (boundInRoot.bottom - rootHeight + navigationBarsPadding).coerceIn(0f, height)
+                alpha = (1 - out / height).takeIf { it > FadeVisibilityThreshold } ?: 0f
+            }
+            .alpha(alpha)
     }
