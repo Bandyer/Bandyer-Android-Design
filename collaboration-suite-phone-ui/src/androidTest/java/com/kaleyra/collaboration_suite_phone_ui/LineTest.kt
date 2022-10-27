1e794cdfa53f39bf9527aac29d0c1499ab422d07
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2022-10-27 18:05:16 +0200

    refactor LineTest tests

--- collaboration-suite-phone-ui/src/androidTest/java/com/kaleyra/collaboration_suite_phone_ui/LineTest.kt
+++ collaboration-suite-phone-ui/src/androidTest/java/com/kaleyra/collaboration_suite_phone_ui/LineTest.kt
@@ -3,14 +3,10 @@ package com.kaleyra.collaboration_suite_phone_ui
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.kaleyra.collaboration_suite_phone_ui.call.compose.CollapsedLineWidth
-import com.kaleyra.collaboration_suite_phone_ui.call.compose.ExpandedLineWidth
-import com.kaleyra.collaboration_suite_phone_ui.call.compose.Line
-import com.kaleyra.collaboration_suite_phone_ui.call.compose.LineTag
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -22,31 +18,35 @@ class LineTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    private var clicked = false
+    private var sheetState by mutableStateOf(BottomSheetState(BottomSheetValue.Collapsed))
 
-    private var collapsed by mutableStateOf(false)
+    private var clicked = false
 
     @Before
     fun setUp() {
         composeTestRule.setContent {
             Line(
-                collapsed = collapsed,
-                color = Color.Black,
-                onClickLabel = "clickLabel",
+                sheetState = sheetState,
+                onClickLabel = "",
                 onClick = { clicked = true }
             )
         }
     }
 
     @Test
-    fun collapsedFalse_lineWidthIsCollapsedLineWidth() {
-        collapsed = false
+    fun sheetCollapsed_lineIsCollapsed() {
+        composeTestRule.onNodeWithTag(LineTag, useUnmergedTree = true).assertWidthIsEqualTo(CollapsedLineWidth)
+    }
+
+    @Test
+    fun sheetExpanded_lineIsExpanded() {
+        sheetState = BottomSheetState(initialValue = BottomSheetValue.Expanded)
         composeTestRule.onNodeWithTag(LineTag, useUnmergedTree = true).assertWidthIsEqualTo(ExpandedLineWidth)
     }
 
     @Test
-    fun collapsedTrue_lineWidthIsExpandedLineWidth() {
-        collapsed = true
+    fun sheetNotCollapsableAndHalfExpanded_lineIsCollapsed() {
+        sheetState = BottomSheetState(initialValue = BottomSheetValue.HalfExpanded, collapsable = false)
         composeTestRule.onNodeWithTag(LineTag, useUnmergedTree = true).assertWidthIsEqualTo(CollapsedLineWidth)
     }
 
