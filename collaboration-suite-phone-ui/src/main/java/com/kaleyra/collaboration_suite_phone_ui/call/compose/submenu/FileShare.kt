3a34783e6afaa2848201de64c0c8c1423d2cc048
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2022-11-08 12:30:11 +0100

    add file share fab composable

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/submenu/FileShare.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/submenu/FileShare.kt
@@ -1,11 +1,16 @@
 package com.kaleyra.collaboration_suite_phone_ui.call.compose.submenu
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -13,14 +18,72 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kaleyra.collaboration_suite_phone_ui.R
+import com.kaleyra.collaboration_suite_phone_ui.chat.model.ImmutableList
 import com.kaleyra.collaboration_suite_phone_ui.chat.theme.KaleyraTheme
 
-@Composable
-internal fun FileShare() {
+private val FabSize = 56.dp
+private val FabIconPadding = 16.dp
+private val FabPadding = 20.dp
 
-//    LazyColumn {
-//
-//    }
+@Composable
+internal fun FileShare(items: ImmutableList<Int>, onClick: () -> Unit) {
+    Box(Modifier.fillMaxSize()) {
+        if (items.count < 1) EmptyList()
+        else {
+            LazyColumn(contentPadding = PaddingValues(bottom = 72.dp)) {
+                items(items = items.value) {
+
+                }
+            }
+        }
+        ExtendedFloatingActionButton(
+            text = if (items.count < 1) { { Text(text = stringResource(id = R.string.kaleyra_fileshare_add).uppercase()) } } else null,
+            onClick = onClick,
+            icon = { Icon(painter = painterResource(id = R.drawable.ic_kaleyra_add), contentDescription = null) },
+            contentColor = MaterialTheme.colors.surface,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(32.dp)
+        )
+    }
+}
+
+@Composable
+internal fun ExtendedFloatingActionButton(
+    icon: @Composable () -> Unit,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    text: @Composable (() -> Unit)? = null,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = MaterialTheme.shapes.small.copy(CornerSize(percent = 50)),
+    backgroundColor: Color = MaterialTheme.colors.secondary,
+    contentColor: Color = contentColorFor(backgroundColor),
+    elevation: FloatingActionButtonElevation = FloatingActionButtonDefaults.elevation()
+) {
+    FloatingActionButton(
+        modifier = modifier.sizeIn(
+            minWidth = FabSize,
+            minHeight = FabSize
+        ),
+        onClick = onClick,
+        interactionSource = interactionSource,
+        shape = shape,
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        elevation = elevation
+    ) {
+        val padding = if (text == null) 0.dp else FabPadding
+        Row(
+            modifier = Modifier.padding(horizontal = padding),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            icon()
+            if (text != null) {
+                Spacer(Modifier.width(FabIconPadding))
+                text()
+            }
+        }
+    }
 }
 
 @Composable
@@ -63,7 +126,9 @@ internal fun FileShareItem() {
 internal fun FileSharePreview() {
     KaleyraTheme {
         Surface {
-            EmptyList()
+            FileShare(items = ImmutableList(listOf(0))) {
+                
+            }
         }
     }
 }
