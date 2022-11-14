d8a9d7d17599c9fc00925e4562d6727abcf32eb4
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2022-11-14 17:21:16 +0100

    refactor WhiteboardTextEditor

--- collaboration-suite-phone-ui/src/androidTest/java/com/kaleyra/collaboration_suite_phone_ui/WhiteboardTextEditorTest.kt
+++ collaboration-suite-phone-ui/src/androidTest/java/com/kaleyra/collaboration_suite_phone_ui/WhiteboardTextEditorTest.kt
@@ -1,17 +1,16 @@
-@file:OptIn(ExperimentalMaterialApi::class)
-
 package com.kaleyra.collaboration_suite_phone_ui
 
 import androidx.activity.ComponentActivity
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.whiteboard.TextEditorState
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.whiteboard.WhiteboardTextEditor
-import com.kaleyra.collaboration_suite_phone_ui.chat.input.TextFieldTag
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
@@ -24,47 +23,100 @@ class WhiteboardTextEditorTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
-    private var confirmedText: TextFieldValue? = null
+    private var state by mutableStateOf(TextEditorState.Empty)
+
+    private var text = ""
+
+    private var dismissClicked = false
+
+    private var confirmClicked = false
 
     @Before
     fun setUp() {
         composeTestRule.setContent {
             WhiteboardTextEditor(
-                modalSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Expanded),
-                onConfirmClick = { confirmedText = it }
+                state = state,
+                textFieldValue = TextFieldValue(),
+                onTextChanged = { text = it.text },
+                onDismissClick = { dismissClicked = true },
+                onConfirmClick = { confirmClicked = true }
             )
         }
+        dismissClicked = false
     }
 
     @Test
-    fun userClicksConfirm_textFieldValueReceived() {
-        val confirm = composeTestRule.activity.getString(R.string.kaleyra_action_confirm)
-        assertEquals(null, confirmedText)
-        composeTestRule.onNode(hasTestTag(TextFieldTag)).performTextInput("Text")
-        composeTestRule.onNodeWithContentDescription(confirm).performClick()
-        assertEquals("Text", confirmedText!!.text)
-    }
-
-    @Test
-    fun emptyText_dismissButtonDisplayed() {
+    fun emptyState_dismissButtonDisplayed() {
+        state = TextEditorState.Empty
         val dismiss = composeTestRule.activity.getString(R.string.kaleyra_action_dismiss)
         composeTestRule.onNodeWithContentDescription(dismiss).assertIsDisplayed()
         composeTestRule.onNodeWithText(dismiss).assertIsDisplayed()
     }
 
     @Test
-    fun userTypesText_discardChangesButtonDisplayed() {
-        val discardChanges = composeTestRule.activity.getString(R.string.kaleyra_action_discard_changes)
-        composeTestRule.onNodeWithContentDescription(discardChanges).assertDoesNotExist()
-        composeTestRule.onNodeWithText(discardChanges).assertDoesNotExist()
-        composeTestRule.onNode(hasTestTag(TextFieldTag)).performTextInput("Text")
-        composeTestRule.onNodeWithContentDescription(discardChanges).assertIsDisplayed()
-        composeTestRule.onNodeWithText(discardChanges).assertIsDisplayed()
+    fun editingState_discardButtonDisplayed() {
+        state = TextEditorState.Editing
+        val discard = composeTestRule.activity.getString(R.string.kaleyra_action_discard_changes)
+        composeTestRule.onNodeWithContentDescription(discard).assertIsDisplayed()
+        composeTestRule.onNodeWithText(discard).assertIsDisplayed()
     }
 
     @Test
-    fun userClicksDiscardChanges_confirmDiscardChangesDisplayed() {
+    fun discardState_cancelButtonDisplayed() {
+        state = TextEditorState.Discard
+        val cancel = composeTestRule.activity.getString(R.string.kaleyra_action_cancel)
+        composeTestRule.onNodeWithContentDescription(cancel).assertIsDisplayed()
+        composeTestRule.onNodeWithText(cancel).assertIsDisplayed()
+    }
 
+    @Test
+    fun discardState_confirmDiscardChangesDisplayed() {
+        state = TextEditorState.Discard
+        val confirmDiscard = composeTestRule.activity.getString(R.string.kaleyra_data_loss_confirm_message)
+        composeTestRule.onNodeWithText(confirmDiscard).assertIsDisplayed()
+    }
+
+    @Test
+    fun discardState_editTextNotDisplayed() {
+        state = TextEditorState.Discard
+        composeTestRule.onNode(hasSetTextAction()).assertDoesNotExist()
+    }
+
+    @Test
+    fun emptyState_userClicksDismiss_onDismissClickInvoked() {
+        state = TextEditorState.Empty
+        val dismiss = composeTestRule.activity.getString(R.string.kaleyra_action_dismiss)
+        composeTestRule.onNodeWithContentDescription(dismiss).performClick()
+        assert(dismissClicked)
+    }
+
+    @Test
+    fun editingState_userClicksDismiss_onDismissClickInvoked() {
+        state = TextEditorState.Editing
+        val discard = composeTestRule.activity.getString(R.string.kaleyra_action_discard_changes)
+        composeTestRule.onNodeWithContentDescription(discard).performClick()
+        assert(dismissClicked)
+    }
+
+    @Test
+    fun discardState_userClicksDismiss_onDismissClickInvoked() {
+        state = TextEditorState.Discard
+        val cancel = composeTestRule.activity.getString(R.string.kaleyra_action_cancel)
+        composeTestRule.onNodeWithContentDescription(cancel).performClick()
+        assert(dismissClicked)
+    }
+
+    @Test
+    fun userClicksConfirm_onConfirmClickInvoked() {
+        val confirm = composeTestRule.activity.getString(R.string.kaleyra_action_confirm)
+        composeTestRule.onNodeWithContentDescription(confirm).performClick()
+        assert(confirmClicked)
+    }
+
+    @Test
+    fun userTypesText_onTextChangedInvoked() {
+        composeTestRule.onNode(hasSetTextAction()).performTextInput("Text")
+        assertEquals("Text", text)
     }
 
 }
--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/whiteboard/WhiteboardTextEditor.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/whiteboard/WhiteboardTextEditor.kt
@@ -1,15 +1,10 @@
-@file:OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialApi::class)
-
 package com.kaleyra.collaboration_suite_phone_ui.call.compose.whiteboard
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -21,98 +16,70 @@ import com.kaleyra.collaboration_suite_phone_ui.R
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.IconButton
 import com.kaleyra.collaboration_suite_phone_ui.chat.input.UserInputText
 import com.kaleyra.collaboration_suite_phone_ui.chat.theme.KaleyraTheme
-import kotlinx.coroutines.launch
 
-private enum class TextEditorState {
+internal enum class TextEditorState {
     Empty,
     Editing,
     Discard
 }
 
-// TODO Add test to check the keyboard is no more shown on dismiss when it was previously opened
 @Composable
 internal fun WhiteboardTextEditor(
-    modalSheetState: ModalBottomSheetState,
-    onConfirmClick: (TextFieldValue) -> Unit
+    state: TextEditorState,
+    textFieldValue: TextFieldValue,
+    onTextChanged: (TextFieldValue) -> Unit,
+    onDismissClick: () -> Unit,
+    onConfirmClick: () -> Unit
 ) {
-    val focusManager = LocalFocusManager.current
-    var textState by remember { mutableStateOf(TextFieldValue()) }
-    val isTextBlank by remember {
-        derivedStateOf {
-            textState.text.isBlank()
-        }
-    }
-    var editorState by remember { mutableStateOf(TextEditorState.Empty) }
-    val scope = rememberCoroutineScope()
+//    val focusManager = LocalFocusManager.current
+//    var textState by remember { mutableStateOf(TextFieldValue()) }
+//    val isTextBlank by remember {
+//        derivedStateOf {
+//            textState.text.isBlank()
+//        }
+//    }
+//    var editorState by remember { mutableStateOf(TextEditorState.Empty) }
+//    val scope = rememberCoroutineScope()
 
-    LaunchedEffect(isTextBlank) {
-        editorState = when {
-            editorState == TextEditorState.Editing && isTextBlank -> TextEditorState.Empty
-            editorState == TextEditorState.Empty && !isTextBlank -> TextEditorState.Editing
-            else -> editorState
-        }
-    }
+//    LaunchedEffect(isTextBlank) {
+//        editorState = when {
+//            editorState == TextEditorState.Editing && isTextBlank -> TextEditorState.Empty
+//            editorState == TextEditorState.Empty && !isTextBlank -> TextEditorState.Editing
+//            else -> editorState
+//        }
+//    }
 
-    Column(
-        modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 48.dp)
-    ) {
-        if (editorState == TextEditorState.Discard) {
+    Column(Modifier.padding(start = 16.dp, end = 16.dp, bottom = 48.dp)) {
+        if (state != TextEditorState.Discard) {
+            Box(Modifier.weight(1f)) {
+                UserInputText(
+                    textFieldValue = textFieldValue,
+                    onTextChanged = onTextChanged,
+                    onDirectionLeft = { /**TODO**/ }
+                )
+            }
+        } else {
             Text(
                 text = stringResource(id = R.string.kaleyra_data_loss_confirm_message),
                 fontWeight = FontWeight.SemiBold,
                 modifier = Modifier.padding(vertical = 24.dp)
             )
-        } else {
-            Box(Modifier.weight(1f)) {
-                UserInputText(
-                    textFieldValue = textState,
-                    onTextChanged = { textState = it },
-                    onDirectionLeft = { /**TODO**/ }
-                )
-            }
         }
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .imePadding()
         ) {
-            AnimatedContent(
-                modifier = Modifier.weight(1f),
-                targetState = editorState
-            ) {
-                when (it) {
-                    TextEditorState.Empty -> {
-                        EditorButton(
-                            icon = painterResource(id = R.drawable.ic_kaleyra_close),
-                            text = stringResource(id = R.string.kaleyra_action_dismiss),
-                            onClick = {
-                                scope.launch {
-                                    focusManager.clearFocus()
-                                    modalSheetState.hide()
-                                }
-                            }
-                        )
-                    }
-                    TextEditorState.Editing -> {
-                        EditorButton(
-                            icon = painterResource(id = R.drawable.ic_kaleyra_cancel),
-                            text = stringResource(id = R.string.kaleyra_action_discard_changes),
-                            onClick = { editorState = TextEditorState.Discard }
-                        )
-                    }
-                    TextEditorState.Discard -> {
-                        EditorButton(
-                            icon = painterResource(id = R.drawable.ic_kaleyra_cancel),
-                            text = stringResource(id = R.string.kaleyra_action_cancel),
-                            onClick = { editorState = TextEditorState.Editing }
-                        )
-                    }
-                }
-            }
-            EditorButton(
+            IconTextButton(
+                icon = iconFor(state),
+                text = textFor(state),
+                onClick = onDismissClick,
+                modifier = Modifier.weight(1f)
+            )
+            IconTextButton(
                 icon = painterResource(id = R.drawable.ic_kaleyra_confirm),
                 text = stringResource(id = R.string.kaleyra_action_confirm),
-                onClick = { onConfirmClick(textState) },
+                onClick = onConfirmClick,
                 modifier = Modifier.weight(1f)
             )
         }
@@ -120,7 +87,24 @@ internal fun WhiteboardTextEditor(
 }
 
 @Composable
-private fun EditorButton(
+private fun iconFor(state: TextEditorState) = painterResource(
+    id = when (state) {
+        TextEditorState.Empty -> R.drawable.ic_kaleyra_close
+        else -> R.drawable.ic_kaleyra_cancel
+    }
+)
+
+@Composable
+private fun textFor(state: TextEditorState) = stringResource(
+    id = when (state) {
+        TextEditorState.Empty -> R.string.kaleyra_action_dismiss
+        TextEditorState.Editing -> R.string.kaleyra_action_discard_changes
+        TextEditorState.Discard -> R.string.kaleyra_action_cancel
+    }
+)
+
+@Composable
+internal fun IconTextButton(
     icon: Any,
     text: String,
     onClick: () -> Unit,
@@ -141,10 +125,41 @@ private fun EditorButton(
 
 @Preview
 @Composable
-internal fun TextEditorPreview() {
+internal fun EmptyTextEditorPreview() {
     KaleyraTheme {
         WhiteboardTextEditor(
-            modalSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Expanded),
+            state = TextEditorState.Empty,
+            textFieldValue = TextFieldValue(),
+            onTextChanged = {},
+            onDismissClick = {},
+            onConfirmClick = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+internal fun EditingTextEditorPreview() {
+    KaleyraTheme {
+        WhiteboardTextEditor(
+            state = TextEditorState.Editing,
+            textFieldValue = TextFieldValue(),
+            onTextChanged = {},
+            onDismissClick = {},
+            onConfirmClick = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+internal fun DiscardTextEditorPreview() {
+    KaleyraTheme {
+        WhiteboardTextEditor(
+            state = TextEditorState.Discard,
+            textFieldValue = TextFieldValue(),
+            onTextChanged = {},
+            onDismissClick = {},
             onConfirmClick = {}
         )
     }
