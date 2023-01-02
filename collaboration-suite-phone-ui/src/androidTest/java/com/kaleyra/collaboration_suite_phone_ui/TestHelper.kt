d9d601858e063069c3bb861c98fc4b3e2e1dd6ab
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-01-02 14:54:42 +0100

    add findAvatar and findBackButton utility functions

--- collaboration-suite-phone-ui/src/androidTest/java/com/kaleyra/collaboration_suite_phone_ui/TestHelper.kt
+++ collaboration-suite-phone-ui/src/androidTest/java/com/kaleyra/collaboration_suite_phone_ui/TestHelper.kt
@@ -1,8 +1,13 @@
 package com.kaleyra.collaboration_suite_phone_ui
 
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.unit.Dp
+import androidx.core.view.KeyEventDispatcher.Component
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 
 internal fun SemanticsNodeInteraction.performScrollUp() {
     performTouchInput {
@@ -19,4 +24,13 @@ internal fun SemanticsNodeInteraction.assertRightPositionInRootIsEqualTo(
 ): SemanticsNodeInteraction {
     getUnclippedBoundsInRoot().right.assertIsEqualTo(expectedRight, "right")
     return this
+}
+
+internal fun <T: ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<T>, T>.findBackButton(): SemanticsNodeInteraction {
+    val back = activity.getString(R.string.kaleyra_back)
+    return onNodeWithContentDescription(back)
+}
+internal fun <T: ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<T>, T>.findAvatar(): SemanticsNodeInteraction {
+    val avatar = activity.getString(R.string.kaleyra_avatar)
+    return onNodeWithContentDescription(avatar)
 }
