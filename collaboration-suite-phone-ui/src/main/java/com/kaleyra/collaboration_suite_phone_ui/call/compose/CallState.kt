a210ffefc8db8ea01e409b5a4bcb187d41ff881f
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-01-04 16:39:45 +0100

    refactor CallState and CallUiState

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/CallState.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/CallState.kt
@@ -1,5 +1,45 @@
 package com.kaleyra.collaboration_suite_phone_ui.call.compose
 
-enum class CallState {
-    Ringing, Dialing, InCall, Ended
+sealed class CallState {
+
+    object Ringing : CallState()
+
+    object Dialing : CallState()
+
+    object Connecting : CallState()
+
+    object Connected : CallState()
+
+    object Reconnecting : CallState()
+
+    sealed class Disconnected : CallState() {
+
+        companion object : Disconnected()
+
+        sealed class Ended : Disconnected() {
+
+            companion object : Ended()
+
+            object HangUp: Ended()
+
+            object Declined : Ended()
+
+            data class Kicked(val userId: String) : Ended()
+
+            object AnsweredOnAnotherDevice : Ended()
+
+            object LineBusy : Ended()
+
+            object Timeout : Ended()
+
+            sealed class Error: Ended() {
+
+                companion object : Error()
+
+                object Server : Error()
+
+                object Unknown : Error()
+            }
+        }
+    }
 }
--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/CallUiState.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/CallUiState.kt
@@ -6,8 +6,7 @@ import com.kaleyra.collaboration_suite_phone_ui.chat.model.ImmutableList
 
 @Immutable
 data class CallUiState(
-    val callState: CallState = CallState.Ended,
-    val callInfo: CallInfoUi = CallInfoUi(title = ""),
+    val callInfo: CallInfoUi = CallInfoUi(),
     val thumbnailStreams: ImmutableList<StreamUi> = ImmutableList(listOf()),
     val featuredStream: ImmutableList<StreamUi> = ImmutableList(listOf()),
     val groupCall: Boolean = false
