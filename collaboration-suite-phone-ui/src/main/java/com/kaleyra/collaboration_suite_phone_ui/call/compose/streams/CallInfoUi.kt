a60b7dc0b819f81265c15fafd89226ee75588dae
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-01-04 16:38:51 +0100

    refactor CallInfo

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/streams/CallInfoUi.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/streams/CallInfoUi.kt
@@ -1,12 +1,13 @@
 package com.kaleyra.collaboration_suite_phone_ui.call.compose.streams
 
 import androidx.compose.runtime.Immutable
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.CallState
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.Recording
 
 @Immutable
 data class CallInfoUi(
-    val title: String,
-    val subtitle: String? = null,
+    val callState: CallState = CallState.Disconnected,
+    val otherParticipants: List<String> = listOf(),
     val watermarkInfo: WatermarkInfo? = null,
     val recording: Recording? = null
 )
