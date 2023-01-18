e967cfa5fa37a5e71d607f0382325474483fd17f
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-01-18 18:13:58 +0100

    make PreCallUiState immutable

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/precall/model/PreCallUiState.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/precall/model/PreCallUiState.kt
@@ -1,11 +1,14 @@
 package com.kaleyra.collaboration_suite_phone_ui.call.compose.precall.model
 
+import androidx.compose.runtime.Immutable
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.Recording
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.StreamUi
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.core.model.UiState
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.streams.WatermarkInfo
 
-data class PreCallUiState(
+// TODO change participants to ImmutableList
+@Immutable
+internal data class PreCallUiState(
     val stream: StreamUi? = null,
     val participants: List<String> = listOf(),
     val watermarkInfo: WatermarkInfo? = null,
