d20e861055d8492ebf7d4ec36f6d627a1684052c
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-01-27 12:50:01 +0100

    refactor CallScreenContent

--- collaboration-suite-phone-ui/src/androidTest/java/com/kaleyra/collaboration_suite_phone_ui/CallScreenContentTest.kt
+++ collaboration-suite-phone-ui/src/androidTest/java/com/kaleyra/collaboration_suite_phone_ui/CallScreenContentTest.kt
@@ -1,18 +1,21 @@
 package com.kaleyra.collaboration_suite_phone_ui
 
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.unit.dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.*
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.precall.view.dialing.DialingContentTag
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.precall.view.ringing.RingingContentTag
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.streams.CallInfoWidgetTag
-import com.kaleyra.collaboration_suite_phone_ui.call.compose.streams.StreamViewTestTag
-import com.kaleyra.collaboration_suite_phone_ui.call.compose.streams.callInfoMock
 import com.kaleyra.collaboration_suite_phone_ui.chat.model.ImmutableList
+import io.mockk.every
+import io.mockk.mockkConstructor
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -24,336 +27,235 @@ class CallScreenContentTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
-    private var streams by mutableStateOf(ImmutableList(listOf(streamUiMock)))
-
-    private var callInfo by mutableStateOf(callInfoMock)
+    private var callState by mutableStateOf<CallState>(CallState.Disconnected)
 
     private var backPressed = false
-
-    private var answerClicked = false
-
-    private var declineClicked = false
 
     @Before
     fun setUp() {
         composeTestRule.setContent {
             CallScreenContent(
-                streams = streams,
-                callInfo = callInfo,
-                onBackPressed = { backPressed = true },
-                onAnswerClick = { answerClicked = true },
-                onDeclineClick = { declineClicked = true }
+                callState = callState,
+                maxWidth = 600.dp,
+                onBackPressed = { backPressed = true }
             )
         }
         backPressed = false
-        answerClicked = false
-        declineClicked = false
     }
 
     @Test
     fun callStateRinging_ringingContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Ringing)
+        callState = CallState.Ringing
         composeTestRule.onNodeWithTag(RingingContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateDialing_dialingContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Dialing)
+        callState = CallState.Dialing
         composeTestRule.onNodeWithTag(DialingContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateConnecting_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Connecting)
+        callState = CallState.Connecting
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateConnected_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Connected)
+        callState = CallState.Connected
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateReconnecting_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Reconnecting)
+        callState = CallState.Reconnecting
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateDisconnected_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected)
+        callState = CallState.Disconnected
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateEnded_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended)
+        callState = CallState.Disconnected.Ended
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
-    fun callStateHangUp_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.HangUp)
+    fun callStateHungUp_callContentIsDisplayed() {
+        callState = CallState.Disconnected.Ended.HungUp
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateDeclined_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.Declined)
+        callState = CallState.Disconnected.Ended.Declined
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateKicked_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.Kicked(""))
+        callState = CallState.Disconnected.Ended.Kicked("")
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateAnsweredOnAnotherDevice_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.AnsweredOnAnotherDevice)
+        callState = CallState.Disconnected.Ended.AnsweredOnAnotherDevice
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateLineBusy_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.LineBusy)
+        callState = CallState.Disconnected.Ended.LineBusy
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateTimeout_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.Timeout)
+        callState = CallState.Disconnected.Ended.Timeout
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateError_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.Error)
+        callState = CallState.Disconnected.Ended.Error
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateServerError_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.Error.Server)
+        callState = CallState.Disconnected.Ended.Error.Server
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateUnknownError_callContentIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.Error.Unknown)
+        callState = CallState.Disconnected.Ended.Error.Unknown
         composeTestRule.onNodeWithTag(CallContentTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateRinging_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Ringing)
+        callState = CallState.Ringing
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateDialing_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Dialing)
+        callState = CallState.Dialing
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateReconnecting_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Reconnecting)
+        callState = CallState.Reconnecting
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateConnecting_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Connecting)
+        callState = CallState.Connecting
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateDisconnected_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected)
+        callState = CallState.Disconnected
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateEnded_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended)
+        callState = CallState.Disconnected.Ended
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
-    fun callStateHangUp_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.HangUp)
+    fun callStateHungUp_callInfoWidgetIsDisplayed() {
+        callState = CallState.Disconnected.Ended.HungUp
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateDeclined_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.Declined)
+        callState = CallState.Disconnected.Ended.Declined
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateKicked_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.Kicked(""))
+        callState = CallState.Disconnected.Ended.Kicked("")
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateAnsweredOnAnotherDevice_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.AnsweredOnAnotherDevice)
+        callState = CallState.Disconnected.Ended.AnsweredOnAnotherDevice
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateLineBusy_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.LineBusy)
+        callState = CallState.Disconnected.Ended.LineBusy
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateTimeout_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.Timeout)
+        callState = CallState.Disconnected.Ended.Timeout
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateError_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.Error)
+        callState = CallState.Disconnected.Ended.Error
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateServerError_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.Error.Server)
+        callState = CallState.Disconnected.Ended.Error.Server
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
     fun callStateUnknownError_callInfoWidgetIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Disconnected.Ended.Error.Unknown)
+        callState = CallState.Disconnected.Ended.Error.Unknown
         composeTestRule.onNodeWithTag(CallInfoWidgetTag).assertIsDisplayed()
     }
 
     @Test
-    fun callStateConnected_userClicksBackButton_onBackPressedInvoked() {
-        callInfo = callInfo.copy(callState = CallState.Connected)
+    fun callStateConnected_userClicksStreamBackButton_onBackPressedInvoked() {
+        mockkConstructor(CallViewModel::class)
+        every { anyConstructed<CallViewModel>().uiState } returns MutableStateFlow(CallUiState(featuredStreams = ImmutableList(listOf(streamUiMock))))
+        callState = CallState.Connected
         composeTestRule.findBackButton().performClick()
         assert(backPressed)
     }
 
     @Test
-    fun callStateConnecting_userClicksBackButton_onBackPressedInvoked() {
-        callInfo = callInfo.copy(callState = CallState.Connecting)
+    fun callStateConnecting_userClicksInfoWidgetBackButton_onBackPressedInvoked() {
+        callState = CallState.Connecting
         composeTestRule.findBackButton().performClick()
         assert(backPressed)
     }
 
     @Test
-    fun callStateDialing_userClicksBackButton_onBackPressedInvoked() {
-        callInfo = callInfo.copy(callState = CallState.Dialing)
+    fun callStateDialing_userClicksInfoWidgetBackButton_onBackPressedInvoked() {
+        callState = CallState.Dialing
         composeTestRule.findBackButton().performClick()
         assert(backPressed)
     }
 
     @Test
-    fun callStateRinging_userClicksBackButton_onBackPressedInvoked() {
-        callInfo = callInfo.copy(callState = CallState.Ringing)
+    fun callStateRinging_userClicksInfoWidgetBackButton_onBackPressedInvoked() {
+        callState = CallState.Ringing
         composeTestRule.findBackButton().performClick()
         assert(backPressed)
-    }
-
-    @Test
-    fun callStateRinging_userClicksAnswerButton_onAnswerClickInvoked() {
-        callInfo = callInfo.copy(callState = CallState.Ringing)
-        val answer = composeTestRule.activity.getString(R.string.kaleyra_ringing_answer)
-        composeTestRule.onAllNodesWithContentDescription(answer).onFirst().performClick()
-        assert(answerClicked)
-    }
-
-    @Test
-    fun callStateRinging_userClicksDeclineButton_onDeclineClickInvoked() {
-        callInfo = callInfo.copy(callState = CallState.Ringing)
-        val decline = composeTestRule.activity.getString(R.string.kaleyra_ringing_decline)
-        composeTestRule.onAllNodesWithContentDescription(decline).onFirst().performClick()
-        assert(declineClicked)
-    }
-
-    @Test
-    fun callStateRinging_emptyStreamList_avatarIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Ringing)
-        streams = ImmutableList(listOf())
-        composeTestRule.findAvatar().assertIsDisplayed()
-    }
-
-    @Test
-    fun callStateRinging_streamViewNull_avatarIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Ringing)
-        streams = ImmutableList(listOf(streamUiMock.copy(view = null)))
-        composeTestRule.findAvatar().assertIsDisplayed()
-    }
-
-    @Test
-    fun callStateRinging_streamHasVideoDisabled_avatarIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Ringing)
-        streams = ImmutableList(listOf(streamUiMock.copy(view = View(composeTestRule.activity), isVideoEnabled = false)))
-        composeTestRule.findAvatar().assertIsDisplayed()
-    }
-
-    @Test
-    fun callStateRinging_streamHasVideoEnabled_streamIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Ringing)
-        streams = ImmutableList(listOf(streamUiMock.copy(view = View(composeTestRule.activity), isVideoEnabled = true)))
-        composeTestRule.onNodeWithTag(StreamViewTestTag).assertIsDisplayed()
-    }
-
-    @Test
-    fun callStateDialing_emptyStreamList_avatarIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Dialing)
-        streams = ImmutableList(listOf())
-        composeTestRule.findAvatar().assertIsDisplayed()
-    }
-
-    @Test
-    fun callStateDialing_streamViewNull_avatarIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Dialing)
-        streams = ImmutableList(listOf(streamUiMock.copy(view = null)))
-        composeTestRule.findAvatar().assertIsDisplayed()
-    }
-
-    @Test
-    fun callStateDialing_streamHasVideoDisabled_avatarIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Dialing)
-        streams = ImmutableList(listOf(streamUiMock.copy(view = View(composeTestRule.activity), isVideoEnabled = false)))
-        composeTestRule.findAvatar().assertIsDisplayed()
-    }
-
-    @Test
-    fun callStateDialing_streamHasVideoEnabled_streamIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Dialing)
-        streams = ImmutableList(listOf(streamUiMock.copy(view = View(composeTestRule.activity), isVideoEnabled = true)))
-        composeTestRule.onNodeWithTag(StreamViewTestTag).assertIsDisplayed()
-    }
-
-    @Test
-    fun callStateConnected_streamViewNull_avatarIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Connected)
-        streams = ImmutableList(listOf(streamUiMock.copy(view = null)))
-        composeTestRule.findAvatar().assertIsDisplayed()
-    }
-
-    @Test
-    fun callStateConnected_streamHasVideoDisabled_avatarIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Connected)
-        streams = ImmutableList(listOf(streamUiMock.copy(view = View(composeTestRule.activity), isVideoEnabled = false)))
-        composeTestRule.findAvatar().assertIsDisplayed()
-    }
-
-    @Test
-    fun callStateConnected_streamHasVideoEnabled_streamIsDisplayed() {
-        callInfo = callInfo.copy(callState = CallState.Connected)
-        streams = ImmutableList(listOf(streamUiMock.copy(view = View(composeTestRule.activity), isVideoEnabled = true)))
-        composeTestRule.onNodeWithTag(StreamViewTestTag).assertIsDisplayed()
     }
 }
--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/CallScreenContent.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/CallScreenContent.kt
@@ -1,21 +1,21 @@
 package com.kaleyra.collaboration_suite_phone_ui.call.compose
 
-import androidx.compose.animation.Crossfade
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.precall.view.dialing.DialingComponent
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.precall.view.ringing.RingingComponent
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 internal fun CallScreenContent(
-    callUiState: CallUiState,
+    callState: CallState,
     maxWidth: Dp,
     onBackPressed: () -> Unit,
     modifier: Modifier = Modifier
@@ -25,14 +25,16 @@ internal fun CallScreenContent(
             .fillMaxSize()
             .background(color = Color.Black)
     ) {
-        Crossfade(targetState = callUiState.callState) { target ->
+        AnimatedContent(
+            targetState = callState,
+            transitionSpec = {
+                fadeIn(animationSpec = tween(220, delayMillis = 90)) with fadeOut(animationSpec = tween(90))
+            }
+        ) { target ->
             when(target) {
                 CallState.Ringing -> RingingComponent(onBackPressed = onBackPressed)
                 CallState.Dialing -> DialingComponent(onBackPressed = onBackPressed)
-                else -> {
-                    val callContentState = rememberCallComponentState(callUiState, LocalConfiguration.current, maxWidth)
-                    CallComponent(state = callContentState, onBackPressed = onBackPressed)
-                }
+                else -> CallComponent(maxWidth = maxWidth, onBackPressed = onBackPressed)
             }
         }
     }
