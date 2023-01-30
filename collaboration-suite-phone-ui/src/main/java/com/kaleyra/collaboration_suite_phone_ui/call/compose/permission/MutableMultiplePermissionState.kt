abfc0ab75dab05cda444dd69ae2c08d1b67cd9b3
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-01-30 18:20:08 +0100

    add permission classes

---
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/permission/MutableMultiplePermissionState.kt
@@ -0,0 +1,119 @@
+package com.kaleyra.collaboration_suite_phone_ui.call.compose.permission
+
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+
+// ***Code taken from google accompanist permission library***
+// TODO remove this file when the apis will be no longer experimental
+
+@Composable
+internal fun rememberMutableMultiplePermissionsState(
+    permissions: List<String>,
+    onPermissionsResult: (Map<String, Boolean>) -> Unit = {}
+): MultiplePermissionsState {
+    // Create mutable permissions that can be requested individually
+    val mutablePermissions = rememberMutablePermissionsState(permissions)
+    // Refresh permissions when the lifecycle is resumed.
+    PermissionsLifecycleCheckerEffect(mutablePermissions)
+
+    val multiplePermissionsState = remember(permissions) {
+        MutableMultiplePermissionsState(mutablePermissions)
+    }
+
+    // Remember RequestMultiplePermissions launcher and assign it to multiplePermissionsState
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissionsResult ->
+        multiplePermissionsState.updatePermissionsStatus(permissionsResult)
+        onPermissionsResult(permissionsResult)
+    }
+    DisposableEffect(multiplePermissionsState, launcher) {
+        multiplePermissionsState.launcher = launcher
+        onDispose {
+            multiplePermissionsState.launcher = null
+        }
+    }
+
+    return multiplePermissionsState
+}
+
+@Composable
+private fun rememberMutablePermissionsState(
+    permissions: List<String>
+): List<MutablePermissionState> {
+    // Create list of MutablePermissionState for each permission
+    val context = LocalContext.current
+    val activity = context.findActivity()
+    val mutablePermissions: List<MutablePermissionState> = remember(permissions) {
+        permissions.map { MutablePermissionState(it, context, activity) }
+    }
+    // Update each permission with its own launcher
+    for (permissionState in mutablePermissions) {
+        key(permissionState.permission) {
+            // Remember launcher and assign it to the permissionState
+            val launcher = rememberLauncherForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) {
+                permissionState.refreshPermissionStatus()
+            }
+            DisposableEffect(launcher) {
+                permissionState.launcher = launcher
+                onDispose {
+                    permissionState.launcher = null
+                }
+            }
+        }
+    }
+
+    return mutablePermissions
+}
+
+/**
+ * A state object that can be hoisted to control and observe multiple permission status changes.
+ *
+ * In most cases, this will be created via [rememberMutableMultiplePermissionsState].
+ *
+ * @param mutablePermissions list of mutable permissions to control and observe.
+ */
+@Stable
+internal class MutableMultiplePermissionsState(
+    private val mutablePermissions: List<MutablePermissionState>
+) : MultiplePermissionsState {
+
+    override val permissions: List<PermissionState> = mutablePermissions
+
+    override val revokedPermissions: List<PermissionState> by derivedStateOf {
+        permissions.filter { it.status != PermissionStatus.Granted }
+    }
+
+    override val allPermissionsGranted: Boolean by derivedStateOf {
+        permissions.all { it.status.isGranted } || // Up to date when the lifecycle is resumed
+                revokedPermissions.isEmpty() // Up to date when the user launches the action
+    }
+
+    override val shouldShowRationale: Boolean by derivedStateOf {
+        permissions.any { it.status.shouldShowRationale }
+    }
+
+    override fun launchMultiplePermissionRequest() {
+        launcher?.launch(
+            permissions.map { it.permission }.toTypedArray()
+        ) ?: throw IllegalStateException("ActivityResultLauncher cannot be null")
+    }
+
+    internal var launcher: ActivityResultLauncher<Array<String>>? = null
+
+    internal fun updatePermissionsStatus(permissionsStatus: Map<String, Boolean>) {
+        // Update all permissions with the result
+        for (permission in permissionsStatus.keys) {
+            mutablePermissions.firstOrNull { it.permission == permission }?.apply {
+                permissionsStatus[permission]?.let {
+                    this.refreshPermissionStatus()
+                }
+            }
+        }
+    }
+}
