95b7bc7ad22f2d49cbe14f395c6ffd687c9406dd
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-01-31 15:38:30 +0100

    refactor CallActionsMapper

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/callactions/model/CallActionsMapper.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/callactions/model/CallActionsMapper.kt
@@ -1,41 +1,43 @@
 package com.kaleyra.collaboration_suite_phone_ui.call.compose.callactions.model
 
-import com.kaleyra.collaboration_suite.phonebox.Call
+import com.bandyer.android_audiosession.model.AudioOutputDevice
 import com.kaleyra.collaboration_suite.phonebox.CallParticipant
 import com.kaleyra.collaboration_suite.phonebox.Input
 import com.kaleyra.collaboration_suite_core_ui.CallUI
+import com.kaleyra.collaboration_suite_extension_audio.extensions.CollaborationAudioExtensions.currentAudioOutputDevice
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.audiooutput.model.AudioDeviceUi
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.audiooutput.model.AudioOutputMapper.mapToAudioDeviceUi
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.audiooutput.model.AudioOutputMapper.mapToBluetoothDeviceState
 import kotlinx.coroutines.flow.*
 
 internal object CallActionsMapper {
 
-    // TODO add test for this
-    fun Flow<CallUI>.toMe(): Flow<CallParticipant.Me> = flatMapLatest { it.participants }.map { it.me }
+    fun Flow<CallUI>.toCurrentAudioDeviceUi(): Flow<AudioDeviceUi?> =
+        flatMapLatest { it.currentAudioOutputDevice }.map { it?.mapToAudioDeviceUi() }
 
     fun Flow<CallUI>.toCallActions(): Flow<List<CallAction>> {
-        return combine(
-            flatMapLatest { it.actions },
-            flatMapLatest { it.state }
-        ) { actions, callState ->
-            val isConnected = callState is Call.State.Connected
-            actions.mapNotNull { list ->
-                when (list) {
-                    is CallUI.Action.ToggleMicrophone -> CallAction.Microphone()
-                    is CallUI.Action.ToggleCamera -> CallAction.Camera()
-                    is CallUI.Action.SwitchCamera -> CallAction.SwitchCamera()
-                    is CallUI.Action.HangUp -> CallAction.HangUp()
-                    is CallUI.Action.Audio -> CallAction.Audio()
-                    is CallUI.Action.OpenChat.Full -> CallAction.Chat()
-                    is CallUI.Action.FileShare -> CallAction.FileShare(isEnabled = isConnected)
-                    is CallUI.Action.ScreenShare -> CallAction.ScreenShare(isEnabled = isConnected)
-                    is CallUI.Action.OpenWhiteboard.Full -> CallAction.Whiteboard(isEnabled = isConnected)
-                    else -> null
+        return flatMapLatest { it.actions }
+            .map { actions ->
+                actions.mapNotNull {
+                    when (it) {
+                        is CallUI.Action.ToggleMicrophone -> CallAction.Microphone()
+                        is CallUI.Action.ToggleCamera -> CallAction.Camera()
+                        is CallUI.Action.SwitchCamera -> CallAction.SwitchCamera()
+                        is CallUI.Action.HangUp -> CallAction.HangUp()
+                        is CallUI.Action.Audio -> CallAction.Audio()
+                        is CallUI.Action.OpenChat.Full -> CallAction.Chat()
+                        is CallUI.Action.FileShare -> CallAction.FileShare()
+                        is CallUI.Action.ScreenShare -> CallAction.ScreenShare()
+                        is CallUI.Action.OpenWhiteboard.Full -> CallAction.Whiteboard()
+                        else -> null
+                    }
                 }
             }
-        }
     }
 
-    fun Flow<CallParticipant.Me>.isCameraEnabled(): Flow<Boolean> =
-        this.flatMapLatest { it.streams }
+    fun Flow<CallUI>.isMyCameraEnabled(): Flow<Boolean> =
+        this.toMe()
+            .flatMapLatest { it.streams }
             .map { streams ->
                 streams.firstOrNull { stream ->
                     stream.video.firstOrNull { it is Input.Video.Camera } != null
@@ -46,8 +48,9 @@ internal object CallActionsMapper {
             .filterNotNull()
             .flatMapLatest { it.enabled }
 
-    fun Flow<CallParticipant.Me>.isMicEnabled(): Flow<Boolean> =
-        this.flatMapLatest { it.streams }
+    fun Flow<CallUI>.isMyMicEnabled(): Flow<Boolean> =
+        this.toMe()
+            .flatMapLatest { it.streams }
             .map { streams ->
                 streams.firstOrNull { stream ->
                     stream.audio.firstOrNull { it != null } != null
@@ -57,4 +60,21 @@ internal object CallActionsMapper {
             .flatMapLatest { it.audio }
             .filterNotNull()
             .flatMapLatest { it.enabled }
+
+    private fun Flow<CallUI>.toMe(): Flow<CallParticipant.Me> =
+        flatMapLatest { it.participants }.map { it.me }
+
+    fun AudioOutputDevice.mapToAudioDeviceUi(): AudioDeviceUi =
+        when (this) {
+            is AudioOutputDevice.NONE -> AudioDeviceUi.Muted
+            is AudioOutputDevice.EARPIECE -> AudioDeviceUi.EarPiece
+            is AudioOutputDevice.LOUDSPEAKER -> AudioDeviceUi.LoudSpeaker
+            is AudioOutputDevice.WIRED_HEADSET -> AudioDeviceUi.WiredHeadset
+            is AudioOutputDevice.BLUETOOTH -> AudioDeviceUi.Bluetooth(
+                id = identifier,
+                name = name,
+                connectionState = bluetoothConnectionStatus.mapToBluetoothDeviceState(),
+                batteryLevel = batteryLevel
+            )
+        }
 }
--- collaboration-suite-phone-ui/src/test/java/com/kaleyra/collaboration_suite_phone_ui/CallActionsMapperTest.kt
+++ collaboration-suite-phone-ui/src/test/java/com/kaleyra/collaboration_suite_phone_ui/CallActionsMapperTest.kt
@@ -1,16 +1,24 @@
 package com.kaleyra.collaboration_suite_phone_ui
 
+import com.bandyer.android_audiosession.model.AudioOutputDevice
 import com.kaleyra.collaboration_suite.phonebox.Call
 import com.kaleyra.collaboration_suite.phonebox.CallParticipant
 import com.kaleyra.collaboration_suite.phonebox.Input
 import com.kaleyra.collaboration_suite.phonebox.Stream
 import com.kaleyra.collaboration_suite_core_ui.CallUI
+import com.kaleyra.collaboration_suite_extension_audio.extensions.CollaborationAudioExtensions
+import com.kaleyra.collaboration_suite_extension_audio.extensions.CollaborationAudioExtensions.currentAudioOutputDevice
+import com.kaleyra.collaboration_suite_phone_ui.Mocks.callMock
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.audiooutput.model.AudioDeviceUi
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.callactions.model.CallAction
-import com.kaleyra.collaboration_suite_phone_ui.call.compose.callactions.model.CallActionsMapper.isMicEnabled
-import com.kaleyra.collaboration_suite_phone_ui.call.compose.callactions.model.CallActionsMapper.isCameraEnabled
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.callactions.model.CallActionsMapper.isMyCameraEnabled
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.callactions.model.CallActionsMapper.isMyMicEnabled
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.callactions.model.CallActionsMapper.toCallActions
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.callactions.model.CallActionsMapper.toCurrentAudioDeviceUi
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.unmockkAll
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
@@ -37,18 +45,77 @@ class CallActionsMapperTest {
 
     @Before
     fun setUp() {
+        every { callMock.participants } returns MutableStateFlow(mockk {
+            every { me } returns participantMeMock
+        })
         every { participantMeMock.streams } returns MutableStateFlow(listOf(streamMock))
     }
 
     @After
     fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun loudSpeakerAudioOutputDevice_toCurrentAudioDeviceUi_loudspeakerAudioDeviceUi() = runTest {
+        mockkObject(CollaborationAudioExtensions)
+        every { any<Call>().currentAudioOutputDevice } returns MutableStateFlow(AudioOutputDevice.LOUDSPEAKER())
+        val call = MutableStateFlow(callMock)
+        val result = call.toCurrentAudioDeviceUi()
+        val actual = result.first()
+        val expected = AudioDeviceUi.LoudSpeaker
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun earPieceAudioOutputDevice_toCurrentAudioDeviceUi_earPieceAudioDeviceUi() = runTest {
+        mockkObject(CollaborationAudioExtensions)
+        every { any<Call>().currentAudioOutputDevice } returns MutableStateFlow(AudioOutputDevice.EARPIECE())
+        val call = MutableStateFlow(callMock)
+        val result = call.toCurrentAudioDeviceUi()
+        val actual = result.first()
+        val expected = AudioDeviceUi.EarPiece
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun wiredHeadsetAudioOutputDevice_toCurrentAudioDeviceUi_wiredHeadsetAudioDeviceUi() = runTest {
+        mockkObject(CollaborationAudioExtensions)
+        every { any<Call>().currentAudioOutputDevice } returns MutableStateFlow(AudioOutputDevice.WIRED_HEADSET())
+        val call = MutableStateFlow(callMock)
+        val result = call.toCurrentAudioDeviceUi()
+        val actual = result.first()
+        val expected = AudioDeviceUi.WiredHeadset
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun mutedAudioOutputDevice_toCurrentAudioDeviceUi_mutedAudioDeviceUi() = runTest {
+        mockkObject(CollaborationAudioExtensions)
+        every { any<Call>().currentAudioOutputDevice } returns MutableStateFlow(AudioOutputDevice.NONE())
+        val call = MutableStateFlow(callMock)
+        val result = call.toCurrentAudioDeviceUi()
+        val actual = result.first()
+        val expected = AudioDeviceUi.Muted
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun bluetoothAudioOutputDevice_toCurrentAudioDeviceUi_bluetoothAudioDeviceUi() = runTest {
+        mockkObject(CollaborationAudioExtensions)
+        every { any<Call>().currentAudioOutputDevice } returns MutableStateFlow(AudioOutputDevice.BLUETOOTH())
+        val call = MutableStateFlow(callMock)
+        val result = call.toCurrentAudioDeviceUi()
+        val actual = result.first()?.javaClass
+        val expected = AudioDeviceUi.Bluetooth::class.java
+        assertEquals(expected, actual)
     }
 
     @Test
     fun emptyCallActions_toCallActions_emptyList() = runTest {
-        every { Mocks.callMock.state } returns MutableStateFlow(mockk())
-        every { Mocks.callMock.actions } returns MutableStateFlow(emptySet())
-        val call = MutableStateFlow(Mocks.callMock)
+        every { callMock.state } returns MutableStateFlow(mockk())
+        every { callMock.actions } returns MutableStateFlow(emptySet())
+        val call = MutableStateFlow(callMock)
         val result = call.toCallActions()
         val actual = result.first()
         val expected = listOf<CallAction>()
@@ -57,8 +124,8 @@ class CallActionsMapperTest {
 
     @Test
     fun filledCallActions_toCallActions_mappedCallActions() = runTest {
-        every { Mocks.callMock.state } returns MutableStateFlow(mockk())
-        every { Mocks.callMock.actions } returns MutableStateFlow(
+        every { callMock.state } returns MutableStateFlow(mockk())
+        every { callMock.actions } returns MutableStateFlow(
             setOf(
                 CallUI.Action.ToggleMicrophone,
                 CallUI.Action.ToggleCamera,
@@ -71,7 +138,7 @@ class CallActionsMapperTest {
                 CallUI.Action.ScreenShare
             )
         )
-        val call = MutableStateFlow(Mocks.callMock)
+        val call = MutableStateFlow(callMock)
         val result = call.toCallActions()
         val actual = result.first()
         val expected = listOf(
@@ -80,112 +147,46 @@ class CallActionsMapperTest {
             CallAction.SwitchCamera(),
             CallAction.HangUp(),
             CallAction.Chat(),
-            CallAction.Whiteboard(isEnabled = false),
+            CallAction.Whiteboard(),
             CallAction.Audio(),
-            CallAction.FileShare(isEnabled = false),
-            CallAction.ScreenShare(isEnabled = false),
+            CallAction.FileShare(),
+            CallAction.ScreenShare(),
         )
         assertEquals(expected, actual)
     }
 
     @Test
-    fun callIsConnected_toCallActions_fileShareIsEnabled() = runTest {
-        every { Mocks.callMock.state } returns MutableStateFlow(Call.State.Connected)
-        every { Mocks.callMock.actions } returns MutableStateFlow(setOf(CallUI.Action.FileShare))
-        val call = MutableStateFlow(Mocks.callMock)
-        val result = call.toCallActions()
-        val actual = result.first()
-        val expected = listOf(CallAction.FileShare(isEnabled = true))
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun callIsConnected_toCallActions_screenShareIsEnabled() = runTest {
-        every { Mocks.callMock.state } returns MutableStateFlow(Call.State.Connected)
-        every { Mocks.callMock.actions } returns MutableStateFlow(setOf(CallUI.Action.ScreenShare))
-        val call = MutableStateFlow(Mocks.callMock)
-        val result = call.toCallActions()
-        val actual = result.first()
-        val expected = listOf(CallAction.ScreenShare(isEnabled = true))
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun callIsConnected_toCallActions_whiteboardIsEnabled() = runTest {
-        every { Mocks.callMock.state } returns MutableStateFlow(Call.State.Connected)
-        every { Mocks.callMock.actions } returns MutableStateFlow(setOf(CallUI.Action.OpenWhiteboard.Full))
-        val call = MutableStateFlow(Mocks.callMock)
-        val result = call.toCallActions()
-        val actual = result.first()
-        val expected = listOf(CallAction.Whiteboard(isEnabled = true))
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun callIsNotConnected_toCallActions_fileShareIsDisabled() = runTest {
-        every { Mocks.callMock.state } returns MutableStateFlow(mockk())
-        every { Mocks.callMock.actions } returns MutableStateFlow(setOf(CallUI.Action.FileShare))
-        val call = MutableStateFlow(Mocks.callMock)
-        val result = call.toCallActions()
-        val actual = result.first()
-        val expected = listOf(CallAction.FileShare(isEnabled = false))
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun callIsNotConnected_toCallActions_screenShareIsDisabled() = runTest {
-        every { Mocks.callMock.state } returns MutableStateFlow(mockk())
-        every { Mocks.callMock.actions } returns MutableStateFlow(setOf(CallUI.Action.ScreenShare))
-        val call = MutableStateFlow(Mocks.callMock)
-        val result = call.toCallActions()
-        val actual = result.first()
-        val expected = listOf(CallAction.ScreenShare(isEnabled = false))
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun callIsNotConnected_toCallActions_whiteboardIsDisabled() = runTest {
-        every { Mocks.callMock.state } returns MutableStateFlow(mockk())
-        every { Mocks.callMock.actions } returns MutableStateFlow(setOf(CallUI.Action.OpenWhiteboard.Full))
-        val call = MutableStateFlow(Mocks.callMock)
-        val result = call.toCallActions()
-        val actual = result.first()
-        val expected = listOf(CallAction.Whiteboard(isEnabled = false))
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun isCameraEnabled_true() = runTest {
+    fun videoEnabled_isMyCameraEnabled_true() = runTest {
         every { videoMock.enabled } returns MutableStateFlow(true)
-        val me = MutableStateFlow(participantMeMock)
-        val result = me.isCameraEnabled()
+        val call = MutableStateFlow(callMock)
+        val result = call.isMyCameraEnabled()
         val actual = result.first()
         assertEquals(true, actual)
     }
 
     @Test
-    fun isCameraEnabled_false() = runTest {
+    fun videoDisabled_isMyCameraEnabled_false() = runTest {
         every { videoMock.enabled } returns MutableStateFlow(false)
-        val me = MutableStateFlow(participantMeMock)
-        val result = me.isCameraEnabled()
+        val call = MutableStateFlow(callMock)
+        val result = call.isMyCameraEnabled()
         val actual = result.first()
         assertEquals(false, actual)
     }
 
     @Test
-    fun isAudioEnabled_true() = runTest {
+    fun audioEnabled_isMyMicEnabled_true() = runTest {
         every { audioMock.enabled } returns MutableStateFlow(true)
-        val me = MutableStateFlow(participantMeMock)
-        val result = me.isMicEnabled()
+        val call = MutableStateFlow(callMock)
+        val result = call.isMyMicEnabled()
         val actual = result.first()
         assertEquals(true, actual)
     }
 
     @Test
-    fun isAudioEnabled_false() = runTest {
+    fun audioDisable_isMyMicEnabled_false() = runTest {
         every { audioMock.enabled } returns MutableStateFlow(false)
-        val me = MutableStateFlow(participantMeMock)
-        val result = me.isMicEnabled()
+        val call = MutableStateFlow(callMock)
+        val result = call.isMyMicEnabled()
         val actual = result.first()
         assertEquals(false, actual)
     }
