bcb423a229d110f796ee1c261e016c7f4b3cc253
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-02-21 17:17:58 +0100

    add CameraStreamPublisher

---
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/call/CameraStreamPublisher.kt
@@ -0,0 +1,24 @@
+package com.kaleyra.collaboration_suite_core_ui.call
+
+import com.kaleyra.collaboration_suite.phonebox.Call
+
+interface CameraStreamPublisher {
+
+    companion object {
+        const val CAMERA_STREAM_ID = "camera"
+    }
+
+    /**
+     * Publish my stream
+     *
+     * @param call The call
+     */
+    fun addCameraStream(call: Call) {
+        val me = call.participants.value.me
+        if (me.streams.value.firstOrNull { it.id == CAMERA_STREAM_ID } != null) return
+        me.addStream(CAMERA_STREAM_ID).let {
+            it.audio.value = null
+            it.video.value = null
+        }
+    }
+}
