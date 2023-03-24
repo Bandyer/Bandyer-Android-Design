2c599b4bd1fb32cf5955ad26699cfb077dbbc540
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-03-24 11:22:08 +0100

    add TimeHelper object

--- collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/utils/TimeHelper.kt
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/utils/TimeHelper.kt
@@ -1,4 +1,6 @@
 package com.kaleyra.collaboration_suite_core_ui.utils
 
-class TimeHelper {
+// Added for testing purpose
+internal object TimeHelper {
+    fun getNow() = System.currentTimeMillis()
 }
