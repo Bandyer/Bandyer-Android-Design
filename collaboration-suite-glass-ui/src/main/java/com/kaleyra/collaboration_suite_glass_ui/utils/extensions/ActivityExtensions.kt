b3985d741a69b0d9eb886fccef2282fef7f1577b
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-03-29 18:53:40 +0200

    refactor immersive mode to support all apis

--- collaboration-suite-glass-ui/src/main/java/com/kaleyra/collaboration_suite_glass_ui/utils/extensions/ActivityExtensions.kt
+++ collaboration-suite-glass-ui/src/main/java/com/kaleyra/collaboration_suite_glass_ui/utils/extensions/ActivityExtensions.kt
@@ -16,63 +16,16 @@
 
 package com.kaleyra.collaboration_suite_glass_ui.utils.extensions
 
-import android.os.Build
-import android.view.View
-import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.LifecycleOwner
+import androidx.core.view.WindowInsetsControllerCompat
 
 internal object ActivityExtensions {
     fun AppCompatActivity.enableImmersiveMode() {
-        lifecycle.addObserver(object : DefaultLifecycleObserver {
-            override fun onCreate(owner: LifecycleOwner) {
-                super.onCreate(owner)
-                enterImmersiveMode()
-            }
-
-            override fun onResume(owner: LifecycleOwner) {
-                super.onResume(owner)
-                hideSystemUI()
-            }
-
-            override fun onDestroy(owner: LifecycleOwner) {
-                super.onDestroy(owner)
-                lifecycle.removeObserver(this)
-            }
-        })
-    }
-
-    @Suppress("DEPRECATION")
-    private fun AppCompatActivity.enterImmersiveMode() {
-        supportActionBar?.hide()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) return
-        // ATM there is no way of doing this on api > 30
-        window.decorView.apply {
-            setOnSystemUiVisibilityChangeListener { visibility ->
-                if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0)
-                    hideSystemUI()
-            }
-        }
-    }
-
-    @Suppress("DEPRECATION")
-    private fun AppCompatActivity.hideSystemUI() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
-            window.insetsController?.apply {
-                hide(WindowInsetsCompat.Type.systemBars())
-                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        } else {
-            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
-        }
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val insetsController = WindowCompat.getInsetsController(window, window.decorView) ?: return
+        insetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        insetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 }
