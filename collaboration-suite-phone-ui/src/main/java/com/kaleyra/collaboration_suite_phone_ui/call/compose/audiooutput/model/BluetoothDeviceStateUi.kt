ead166a5609918be9f2db05ee5ee5b152868d6a2
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-04-05 18:15:35 +0200

    add ConnectingAudio and PlayingAudio bluetooth ui state

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/audiooutput/model/BluetoothDeviceStateUi.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/audiooutput/model/BluetoothDeviceStateUi.kt
@@ -3,6 +3,8 @@ package com.kaleyra.collaboration_suite_phone_ui.call.compose.audiooutput.model
 enum class BluetoothDeviceState {
     Available,
     Connecting,
+    ConnectingAudio,
+    PlayingAudio,
     Connected,
     Activating,
     Active,
