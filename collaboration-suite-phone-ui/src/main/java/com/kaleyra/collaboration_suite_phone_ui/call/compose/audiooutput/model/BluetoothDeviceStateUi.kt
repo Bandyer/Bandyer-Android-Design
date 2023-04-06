afeab915bbbc3e42b9a16458a50c04b1cc832beb
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-04-06 16:39:41 +0200

    update bluetooth audio state ext

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/audiooutput/model/BluetoothDeviceStateUi.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/audiooutput/model/BluetoothDeviceStateUi.kt
@@ -14,7 +14,7 @@ enum class BluetoothDeviceState {
 }
 
 internal fun BluetoothDeviceState.isConnecting() =
-    this == BluetoothDeviceState.Connecting || this == BluetoothDeviceState.ConnectingAudio
+    this == BluetoothDeviceState.Connecting || this == BluetoothDeviceState.ConnectingAudio || this == BluetoothDeviceState.Activating
 
 internal fun BluetoothDeviceState.isConnectedOrPlaying() =
-    this == BluetoothDeviceState.Active || this == BluetoothDeviceState.Connected || this == BluetoothDeviceState.PlayingAudio
+    this == BluetoothDeviceState.Active || this == BluetoothDeviceState.Connected || this == BluetoothDeviceState.PlayingAudio || this == BluetoothDeviceState.Activating || this == BluetoothDeviceState.Connecting || this == BluetoothDeviceState.ConnectingAudio
