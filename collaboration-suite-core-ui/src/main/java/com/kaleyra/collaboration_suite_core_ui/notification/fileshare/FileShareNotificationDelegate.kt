77a3bf058caa2b2ac917bcc05edf5e1028c1313d
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-04-12 10:37:05 +0200

    split FileShareNotificationDelegate and FileShareNotificationManager files

---
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/notification/fileshare/FileShareNotificationDelegate.kt
@@ -0,0 +1,46 @@
+package com.kaleyra.collaboration_suite_core_ui.notification.fileshare
+
+import android.app.Notification
+import com.kaleyra.collaboration_suite.phonebox.Call
+import com.kaleyra.collaboration_suite.sharedfolder.SharedFile
+import com.kaleyra.collaboration_suite_core_ui.notification.NotificationManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.transform
+
+interface FileShareNotificationDelegate {
+
+    companion object {
+        const val EXTRA_DOWNLOAD_ID = "com.kaleyra.collaboration_suite_core_ui.EXTRA_DOWNLOAD_ID"
+    }
+
+    fun syncFileShareNotification(
+        call: Call,
+        activityClazz: Class<*>,
+        scope: CoroutineScope
+    ) {
+        var lastNotifiedDownload: SharedFile? = null
+        val me = call.participants.value.me
+        call.sharedFolder.files
+            .transform { files ->
+                val lastDownload = files.lastOrNull { file -> file.sender.userId != me.userId } ?: return@transform
+                if (lastNotifiedDownload != lastDownload) emit(lastDownload)
+                lastNotifiedDownload = lastDownload
+            }
+            .onEach {
+                val notification = buildNotification(call, it, activityClazz)
+                NotificationManager.notify(it.id.hashCode(), notification)
+            }
+            .launchIn(scope)
+    }
+
+    private suspend fun buildNotification(call: Call, sharedFile: SharedFile, activityClazz: Class<*>): Notification {
+        val participants = call.participants.first()
+        val participant = participants.others.firstOrNull { it.userId == sharedFile.sender.userId }
+        val username = participant?.displayName?.first() ?: ""
+        return NotificationManager.buildIncomingFileNotification(username, sharedFile.id, activityClazz)
+    }
+}
--- collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/notification/fileshare/FileShareNotificationManager.kt
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/notification/fileshare/FileShareNotificationManager.kt
@@ -4,52 +4,10 @@ import android.app.Notification
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import com.kaleyra.collaboration_suite.phonebox.Call
-import com.kaleyra.collaboration_suite.sharedfolder.SharedFile
 import com.kaleyra.collaboration_suite_core_ui.R
-import com.kaleyra.collaboration_suite_core_ui.notification.NotificationManager
 import com.kaleyra.collaboration_suite_core_ui.notification.fileshare.FileShareNotificationDelegate.Companion.EXTRA_DOWNLOAD_ID
 import com.kaleyra.collaboration_suite_core_ui.utils.PendingIntentExtensions
 import com.kaleyra.collaboration_suite_utils.ContextRetainer
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.transform
-
-interface FileShareNotificationDelegate {
-
-    companion object {
-        const val EXTRA_DOWNLOAD_ID = "com.kaleyra.collaboration_suite_core_ui.EXTRA_DOWNLOAD_ID"
-    }
-
-    fun syncFileShareNotification(
-        call: Call,
-        activityClazz: Class<*>,
-        scope: CoroutineScope
-    ) {
-        var lastNotifiedDownload: SharedFile? = null
-        val me = call.participants.value.me
-        call.sharedFolder.files
-            .transform { files ->
-                val lastDownload = files.lastOrNull { file -> file.sender.userId != me.userId } ?: return@transform
-                if (lastNotifiedDownload != lastDownload) emit(lastDownload)
-                lastNotifiedDownload = lastDownload
-            }
-            .onEach {
-                val notification = buildNotification(call, it, activityClazz)
-                NotificationManager.notify(it.id.hashCode(), notification)
-            }
-            .launchIn(scope)
-    }
-
-    private suspend fun buildNotification(call: Call, sharedFile: SharedFile, activityClazz: Class<*>): Notification {
-        val participants = call.participants.first()
-        val participant = participants.others.firstOrNull { it.userId == sharedFile.sender.userId }
-        val username = participant?.displayName?.first() ?: ""
-        return NotificationManager.buildIncomingFileNotification(username, sharedFile.id, activityClazz)
-    }
-}
 
 interface FileShareNotificationManager {
 
