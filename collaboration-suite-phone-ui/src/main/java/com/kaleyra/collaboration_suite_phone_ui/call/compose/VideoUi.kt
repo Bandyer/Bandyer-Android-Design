3b3d7f4a3ecd9f930c0600b93038992af9656bc1
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-04-19 16:41:28 +0200

    add pointer UI model

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/VideoUi.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/VideoUi.kt
@@ -2,10 +2,12 @@ package com.kaleyra.collaboration_suite_phone_ui.call.compose
 
 import android.view.View
 import androidx.compose.runtime.Immutable
+import com.kaleyra.collaboration_suite_phone_ui.chat.model.ImmutableList
 
 @Immutable
 data class VideoUi(
     val id: String,
     val view: View?,
     val isEnabled: Boolean
+    val pointers: ImmutableList<PointerUi> = ImmutableList(emptyList())
 )
