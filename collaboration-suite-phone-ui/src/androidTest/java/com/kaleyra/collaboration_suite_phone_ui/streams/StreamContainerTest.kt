bb616ccad2be73c41d54be414ae560c3254e1f06
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-04-19 17:38:50 +0200

    add StreamContainer

---
+++ collaboration-suite-phone-ui/src/androidTest/java/com/kaleyra/collaboration_suite_phone_ui/streams/StreamContainerTest.kt
@@ -0,0 +1,44 @@
+package com.kaleyra.collaboration_suite_phone_ui.streams
+
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assertLeftPositionInRootIsEqualTo
+import androidx.compose.ui.test.assertTopPositionInRootIsEqualTo
+import androidx.compose.ui.test.getBoundsInRoot
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onParent
+import androidx.compose.ui.unit.height
+import androidx.compose.ui.unit.width
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.streams.StreamContainer
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class StreamContainerTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {
+            StreamContainer {
+                Spacer(modifier = Modifier.testTag("StreamTag"))
+            }
+        }
+    }
+
+    @Test
+    fun childIsCentered() {
+        val child = composeTestRule.onNodeWithTag("StreamTag")
+        val parent = child.onParent()
+        child.assertTopPositionInRootIsEqualTo(parent.getBoundsInRoot().height / 2)
+        child.assertLeftPositionInRootIsEqualTo(parent.getBoundsInRoot().width / 2)
+    }
+}
