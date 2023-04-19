da147d726e4fa4cc5fc71d7214945c0cd6e215d6
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-04-19 17:47:41 +0200

    add Pointer view

---
+++ collaboration-suite-phone-ui/src/androidTest/java/com/kaleyra/collaboration_suite_phone_ui/pointer/TextPointerTest.kt
@@ -0,0 +1,38 @@
+package com.kaleyra.collaboration_suite_phone_ui.pointer
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.pointer.TextPointer
+import org.junit.Assert.assertNotEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TextPointerTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private var textWidth = 0
+
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {
+            TextPointer(username = "username", onTextWidth = { textWidth = it })
+        }
+    }
+
+    @Test
+    fun usernameIsDisplayed() {
+        composeTestRule.onNodeWithText("username").assertIsDisplayed()
+    }
+
+    @Test
+    fun textWidthIsInvoked() {
+        assertNotEquals(0, textWidth)
+    }
+}
---
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/pointer/Pointer.kt
@@ -0,0 +1,45 @@
+package com.kaleyra.collaboration_suite_phone_ui.call.compose.pointer
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.dp
+
+val PointerSize = 16.dp
+
+// TODO test username, onTextWidth is invoked and Pointer size
+@Composable
+internal fun TextPointer(
+    username: String,
+    onTextWidth: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = modifier) {
+        Pointer()
+        Text(
+            text = username,
+            color = MaterialTheme.colors.secondary,
+            modifier = Modifier.onGloballyPositioned { onTextWidth(it.size.width) }
+        )
+    }
+}
+
+@Composable
+internal fun Pointer(modifier: Modifier = Modifier) {
+    Spacer(
+        modifier
+            .size(PointerSize)
+            .background(
+                color = MaterialTheme.colors.secondary,
+                shape = CircleShape
+            )
+    )
+}
