2960ecd0f2d19e7f90afb2b8df0d96600c718107
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-04-19 16:42:50 +0200

    add ImmutableView class to optimize recompositions and set default values in video UI model

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/VideoUi.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/VideoUi.kt
@@ -1,13 +1,12 @@
 package com.kaleyra.collaboration_suite_phone_ui.call.compose
 
-import android.view.View
 import androidx.compose.runtime.Immutable
 import com.kaleyra.collaboration_suite_phone_ui.chat.model.ImmutableList
 
 @Immutable
 data class VideoUi(
     val id: String,
-    val view: View?,
-    val isEnabled: Boolean
+    val view: ImmutableView? = null,
+    val isEnabled: Boolean = false,
     val pointers: ImmutableList<PointerUi> = ImmutableList(emptyList())
 )
