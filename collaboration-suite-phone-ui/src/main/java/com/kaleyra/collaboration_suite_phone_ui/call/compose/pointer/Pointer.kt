bf86a901e6a694692690c2a4c6101d1f3aeea88f
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-04-20 12:32:47 +0200

    add shadow to pointer

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/pointer/Pointer.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/pointer/Pointer.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -12,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
+import com.kaleyra.collaboration_suite_phone_ui.call.applyShadow
 
 val PointerSize = 16.dp
 
@@ -21,12 +23,14 @@ internal fun TextPointer(
     onTextWidth: (Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val textStyle = LocalTextStyle.current
     Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = modifier) {
         Pointer()
         Text(
             text = username,
             color = MaterialTheme.colors.secondary,
-            modifier = Modifier.onGloballyPositioned { onTextWidth(it.size.width) }
+            modifier = Modifier.onGloballyPositioned { onTextWidth(it.size.width) },
+            style = textStyle.applyShadow()
         )
     }
 }
