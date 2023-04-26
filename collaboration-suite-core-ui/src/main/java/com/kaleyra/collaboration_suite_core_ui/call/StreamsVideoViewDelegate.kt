7fa0c0c02f9f8756b1facca2d4df82907031e97a
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-04-26 14:46:46 +0200

    refactor StreamVideoViewDelegate to fix video stream view not set in some cases

--- collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/call/StreamsVideoViewDelegate.kt
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/call/StreamsVideoViewDelegate.kt
@@ -1,23 +1,68 @@
 package com.kaleyra.collaboration_suite_core_ui.call
 
 import android.content.Context
+import com.kaleyra.collaboration_suite.phonebox.CallParticipant
 import com.kaleyra.collaboration_suite.phonebox.CallParticipants
+import com.kaleyra.collaboration_suite.phonebox.Input
+import com.kaleyra.collaboration_suite.phonebox.Stream
 import com.kaleyra.collaboration_suite.phonebox.VideoStreamView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.*
 
 interface StreamsVideoViewDelegate {
-    fun setStreamsVideoView(context: Context, participants: Flow<CallParticipants>, scope: CoroutineScope) {
+    fun setStreamsVideoView(
+        context: Context,
+        participants: Flow<CallParticipants>,
+        scope: CoroutineScope
+    ) {
         participants
             .map { it.list }
-            .flatMapLatest { participantsList ->
-                participantsList.map { it.streams }.merge()
-            }
-            .flatMapLatest { it.map { stream -> stream.video }.merge() }
+            .mapParticipantsToVideos()
+            .transform { videos -> videos.forEach { emit(it) } }
             .onEach { video ->
-                if (video == null || video.view.value != null) return@onEach
+                if (video.view.value != null) return@onEach
                 video.view.value = VideoStreamView(context.applicationContext)
             }
             .launchIn(scope)
+    }
+
+    private fun Flow<List<CallParticipant>>.mapParticipantsToVideos(): Flow<List<Input.Video>> {
+        return this.flatMapLatest { participants ->
+            val participantVideos = mutableMapOf<String, List<Input.Video>>()
+            participants.map { participant ->
+                participant.streams
+                    .mapStreamsToVideos()
+                    .map { Pair(participant.userId, it) }
+            }
+                .merge()
+                .transform { (userId, videos) ->
+                    participantVideos[userId] = videos
+                    val values = participantVideos.values.toList()
+                    if (values.size == participants.size) {
+                        emit(values.flatten())
+                    }
+                }
+        }
+    }
+
+    private fun Flow<List<Stream>>.mapStreamsToVideos(): Flow<List<Input.Video>> {
+        return this.flatMapLatest { streams ->
+                val streamVideos = mutableMapOf<String, Input.Video>()
+                streams
+                    .map { stream ->
+                        stream.video
+                            .filterIsInstance<Input.Video>()
+                            .map { Pair(stream.id, it) }
+                    }
+                    .merge()
+                    .transform { (streamId, video) ->
+                        streamVideos[streamId] = video
+                        val values = streamVideos.values.toList()
+                        if (values.size == streams.size) {
+                            emit(values)
+                        }
+                    }
+            }
+
     }
 }
--- collaboration-suite-core-ui/src/test/java/com/kaleyra/collaboration_suite_core_ui/StreamsVideoViewDelegateTest.kt
+++ collaboration-suite-core-ui/src/test/java/com/kaleyra/collaboration_suite_core_ui/StreamsVideoViewDelegateTest.kt
@@ -2,13 +2,28 @@ package com.kaleyra.collaboration_suite_core_ui
 
 import com.kaleyra.collaboration_suite.phonebox.*
 import com.kaleyra.collaboration_suite_core_ui.call.StreamsVideoViewDelegate
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.unmockkAll
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert.assertNotEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
 
 @OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(RobolectricTestRunner::class)
 class StreamsVideoViewDelegateTest {
 
     private val streamsVideoViewDelegate = object : StreamsVideoViewDelegate { }
+
+    private val context = RuntimeEnvironment.getApplication()
 
     private val participantsMock = mockk<CallParticipants>(relaxed = true)
 
@@ -16,37 +31,107 @@ class StreamsVideoViewDelegateTest {
 
     private val participantMock = mockk<CallParticipant>(relaxed = true)
 
-    private val streamMock = mockk<Stream>(relaxed = true)
+    private val participantMock2 = mockk<CallParticipant>(relaxed = true)
 
-    private val videoMock = mockk<Input.Video>(relaxed = true)
+    private val streamMock1 = mockk<Stream>()
 
-    // TODO check to test this
+    private val streamMock2 = mockk<Stream>()
 
-//    @Test
-//    fun setStreamsVideoView_viewSetOnOtherParticipantStreamVideo() = runTest(UnconfinedTestDispatcher()) {
-//        every { participantsMock.list } returns listOf(participantMock)
-//        every { participantMock.streams } returns MutableStateFlow(listOf(streamMock))
-//        every { streamMock.video } returns MutableStateFlow(videoMock)
-//        every { videoMock.view } returns MutableStateFlow(null)
-//        mockkConstructor(VideoStreamView::class)
-////        every { anyConstructed<ViewGroup>().findViewById<View>(any()) } returns mockk()
-//        streamsVideoViewDelegate.setStreamsVideoView(mockk(relaxed = true), flowOf(participantsMock), this)
-//        assertNotEquals(null, videoMock.view)
-//        stopCollecting()
-//    }
-//
-//    @Test
-//    fun setStreamsVideoView_viewSetOnMyStreamVideo() = runTest(UnconfinedTestDispatcher()) {
-//
-//    }
-//
-//    @Test
-//    fun setStreamsVideoView_viewSetOnNewStream() = runTest(UnconfinedTestDispatcher()) {
-//
-//    }
-//
-//    @Test
-//    fun setStreamsVideoView_viewSetOnNewVideo() = runTest(UnconfinedTestDispatcher()) {
-//
-//    }
+    private val myStreamMock = mockk<Stream.Mutable>()
+
+    private val myVideoMock = mockk<Input.Video.Camera.Internal>(relaxed = true)
+
+    private val videoMock1 = mockk<Input.Video>(relaxed = true)
+
+    private val videoMock2 = mockk<Input.Video>(relaxed = true)
+
+    @Before
+    fun setUp() {
+        every { participantsMock.list } returns listOf(meMock, participantMock)
+        with(meMock) {
+            every { userId } returns "myUserId"
+            every { streams } returns MutableStateFlow(listOf(myStreamMock))
+        }
+        with(participantMock) {
+            every { userId } returns "userId1"
+            every { streams } returns MutableStateFlow(listOf(streamMock1))
+        }
+        with(participantMock2) {
+            every { userId } returns "userId2"
+            every { streams } returns MutableStateFlow(listOf(streamMock2))
+        }
+        with(myStreamMock) {
+            every { id } returns "myStreamId"
+            every { video } returns MutableStateFlow(myVideoMock)
+        }
+        with(streamMock1) {
+            every { id } returns "streamId1"
+            every { video } returns MutableStateFlow(videoMock1)
+        }
+        with(streamMock2) {
+            every { id } returns "streamId2"
+            every { video } returns MutableStateFlow(videoMock2)
+        }
+        with(myVideoMock) {
+            every { id } returns "myVideoId"
+            every { view } returns MutableStateFlow(null)
+        }
+        with(videoMock1) {
+            every { id } returns "videoId"
+            every { view } returns MutableStateFlow(null)
+        }
+        with(videoMock2) {
+            every { id } returns "videoId2"
+            every { view } returns MutableStateFlow(null)
+        }
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+    
+    @Test
+    fun setStreamVideoView_viewsAreSet() = runTest(UnconfinedTestDispatcher()) {
+        streamsVideoViewDelegate.setStreamsVideoView(context, MutableStateFlow(participantsMock), backgroundScope)
+        assertNotEquals(null, myVideoMock.view.value)
+        assertNotEquals(null, videoMock1.view.value)
+    }
+
+    @Test
+    fun setStreamsVideoView_viewSetOnNewParticipant() = runTest(UnconfinedTestDispatcher()) {
+        val participants = MutableStateFlow(participantsMock)
+        streamsVideoViewDelegate.setStreamsVideoView(context, participants, backgroundScope)
+        assertNotEquals(null, myVideoMock.view.value)
+        assertNotEquals(null, videoMock1.view.value)
+
+        val newParticipantsMock = mockk<CallParticipants>()
+        every { newParticipantsMock.list } returns listOf(meMock, participantMock, participantMock2)
+        participants.value = newParticipantsMock
+        assertNotEquals(null, videoMock2.view.value)
+    }
+
+    @Test
+    fun setStreamsVideoView_viewSetOnNewStream() = runTest(UnconfinedTestDispatcher()) {
+        val streams = MutableStateFlow(listOf(streamMock1))
+        every { participantMock.streams } returns streams
+        streamsVideoViewDelegate.setStreamsVideoView(context, MutableStateFlow(participantsMock), backgroundScope)
+        assertNotEquals(null, myVideoMock.view.value)
+        assertNotEquals(null, videoMock1.view.value)
+
+        streams.value = listOf(streamMock1, streamMock2)
+        assertNotEquals(null, videoMock2.view.value)
+    }
+
+    @Test
+    fun setStreamsVideoView_viewSetOnNewVideo() = runTest(UnconfinedTestDispatcher()) {
+        val video = MutableStateFlow(videoMock1)
+        every { streamMock1.video } returns video
+        streamsVideoViewDelegate.setStreamsVideoView(context, MutableStateFlow(participantsMock), backgroundScope)
+        assertNotEquals(null, myVideoMock.view.value)
+        assertNotEquals(null, videoMock1.view.value)
+
+        video.value = videoMock2
+        assertNotEquals(null, videoMock2.view.value)
+    }
 }
