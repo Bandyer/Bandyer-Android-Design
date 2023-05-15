307ac28aafc53999f9dca8d76dd2b726e336883c
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-05-15 17:07:22 +0200

    add FilePickBroadcastReceiverTest

---
+++ collaboration-suite-phone-ui/src/test/java/com/kaleyra/collaboration_suite_phone_ui/FilePickBroadcastReceiverTest.kt
@@ -0,0 +1,33 @@
+package com.kaleyra.collaboration_suite_phone_ui
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.fileshare.filepick.FilePickBroadcastReceiver
+import io.mockk.mockk
+import io.mockk.spyk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(RobolectricTestRunner::class)
+class FilePickBroadcastReceiverTest {
+
+    private val contextMock = mockk<Context>(relaxed = true)
+
+    private val receiver = FilePickBroadcastReceiver()
+
+    @Test
+    fun testOnReceive() = runTest {
+        val uriMock = mockk<Uri>()
+        receiver.onReceive(contextMock, Intent(FilePickBroadcastReceiver.ACTION_FILE_PICK_EVENT).apply {
+          putExtra("uri", uriMock)
+        })
+        assertEquals(uriMock, FilePickBroadcastReceiver.uri.first())
+    }
+}
