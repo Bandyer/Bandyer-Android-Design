99e1d812f1c019fca2b934327c044813936eaaa4
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-05-18 13:08:25 +0200

    add UserMessage model

---
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/usermessages/model/UserMessage.kt
@@ -0,0 +1,5 @@
+package com.kaleyra.collaboration_suite_phone_ui.call.compose.usermessages.model
+
+interface UserMessage {
+    val id: String
+}
---
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/usermessages/model/UserMessages.kt
@@ -0,0 +1,6 @@
+package com.kaleyra.collaboration_suite_phone_ui.call.compose.usermessages.model
+
+data class UserMessages(
+    val recordingMessage: RecordingMessage? = null,
+    val mutedMessage: MutedMessage? = null
+)
