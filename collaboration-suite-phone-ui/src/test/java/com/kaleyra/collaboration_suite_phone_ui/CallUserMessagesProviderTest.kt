a893c385eb23fa003a1a2f572bb14b0cc303fab2
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-05-18 16:17:12 +0200

    add CallUserMessagesProviderTest

--- collaboration-suite-phone-ui/src/test/java/com/kaleyra/collaboration_suite_phone_ui/CallUserMessagesProviderTest.kt
+++ collaboration-suite-phone-ui/src/test/java/com/kaleyra/collaboration_suite_phone_ui/CallUserMessagesProviderTest.kt
@@ -1,6 +1,69 @@
 package com.kaleyra.collaboration_suite_phone_ui
 
+import com.kaleyra.collaboration_suite.phonebox.Call
+import com.kaleyra.collaboration_suite.phonebox.CallParticipant
+import com.kaleyra.collaboration_suite.phonebox.CallParticipants
+import com.kaleyra.collaboration_suite.phonebox.Input
+import com.kaleyra.collaboration_suite.phonebox.Stream
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.usermessages.model.RecordingMessage
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.usermessages.provider.CallUserMessagesProvider
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withTimeout
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
 class CallUserMessagesProviderTest {
 
+    private var callMock = mockk<Call>()
 
+    @Test
+    fun testRecordingStartedUserMessage() = runTest {
+        every { callMock.extras.recording.state } returns MutableStateFlow(Call.Recording.State.Started)
+        val provider = CallUserMessagesProvider(flowOf(callMock))
+        val actual = provider.recordingUserMessage().first()
+        assert(actual is RecordingMessage.Started)
+    }
+
+    @Test
+    fun testRecordingStoppedUserMessage() = runTest {
+        every { callMock.extras.recording.state } returns MutableStateFlow(Call.Recording.State.Stopped)
+        val provider = CallUserMessagesProvider(flowOf(callMock))
+        val actual = provider.recordingUserMessage().first()
+        assert(actual is RecordingMessage.Stopped)
+    }
+
+    @Test
+    fun testRecordingFailedUserMessage() = runTest {
+        every { callMock.extras.recording.state } returns MutableStateFlow(Call.Recording.State.Stopped.Error)
+        val provider = CallUserMessagesProvider(flowOf(callMock))
+        val actual = provider.recordingUserMessage().first()
+        assert(actual is RecordingMessage.Failed)
+    }
+
+    @Test
+    fun testMutedUserMessage() = runTest {
+        val event = mockk<Input.Audio.Event.Request.Mute>()
+        val producer = mockk<CallParticipant>()
+        val callParticipants = mockk<CallParticipants>()
+        val me = mockk<CallParticipant.Me>()
+        val streamMock = mockk<Stream.Mutable>()
+        val audio = mockk<Input.Audio>()
+        every { callMock.participants } returns MutableStateFlow(callParticipants)
+        every { producer.displayName } returns MutableStateFlow("username")
+        every { event.producer } returns producer
+        every { callParticipants.me } returns me
+        every { me.streams } returns MutableStateFlow(listOf(streamMock))
+        every { streamMock.audio } returns MutableStateFlow(audio)
+        every { audio.events } returns MutableStateFlow(event)
+        val provider = CallUserMessagesProvider(flowOf(callMock))
+        withTimeout(100) {
+            provider.mutedUserMessage().first()
+        }
+    }
 }
