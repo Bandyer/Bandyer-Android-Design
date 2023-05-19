5920b75a53139bc70c4d21a11aa94b1701458387
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-05-19 18:10:28 +0200

    add new BaseViewModel call flow behaviour

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/core/viewmodel/BaseViewModel.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/core/viewmodel/BaseViewModel.kt
@@ -1,17 +1,23 @@
 package com.kaleyra.collaboration_suite_phone_ui.call.compose.core.viewmodel
 
 import androidx.lifecycle.viewModelScope
+import com.kaleyra.collaboration_suite_core_ui.CallUI
 import com.kaleyra.collaboration_suite_core_ui.CollaborationViewModel
 import com.kaleyra.collaboration_suite_core_ui.Configuration
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.core.model.UiState
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.usermessages.provider.CallUserMessagesProvider
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 
 abstract class BaseViewModel<UI_STATE: UiState>(configure: suspend () -> Configuration) : CollaborationViewModel(configure) {
 
-    protected val call = phoneBox.flatMapLatest { it.call }.shareInEagerly(viewModelScope)
+    private val _call = MutableSharedFlow<CallUI>(replay = 1)
+    protected val call = _call.asSharedFlow()
 
     protected val callUserMessageProvider = CallUserMessagesProvider(call)
 
@@ -19,4 +25,11 @@ abstract class BaseViewModel<UI_STATE: UiState>(configure: suspend () -> Configu
     val uiState = _uiState.asStateFlow()
 
     abstract fun initialState(): UI_STATE
+
+    init {
+        viewModelScope.launch {
+            val currentCall = phoneBox.flatMapLatest { it.call }.first()
+            _call.emit(currentCall)
+        }
+    }
 }
