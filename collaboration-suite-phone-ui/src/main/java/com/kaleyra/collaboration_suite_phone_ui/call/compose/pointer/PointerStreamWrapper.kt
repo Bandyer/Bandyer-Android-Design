a52be19d48f1f28599a62e97a0c2ca6a2290c934
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-05-29 10:50:26 +0200

    update pointer wrapper size on stream view video size

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/pointer/PointerStreamWrapper.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/pointer/PointerStreamWrapper.kt
@@ -1,32 +1,49 @@
 package com.kaleyra.collaboration_suite_phone_ui.call.compose.pointer
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.IntSize
+import com.kaleyra.collaboration_suite.phonebox.VideoStreamView
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.ImmutableView
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.PointerUi
 import com.kaleyra.collaboration_suite_phone_ui.chat.model.ImmutableList
+import com.kaleyra.collaboration_suite_phone_ui.chat.utility.collectAsStateWithLifecycle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
 
 @Composable
 internal fun PointerStreamWrapper(
     modifier: Modifier = Modifier,
+    streamView: ImmutableView?,
     pointerList: ImmutableList<PointerUi>?,
-    stream: @Composable () -> Unit
+    stream: @Composable (Boolean) -> Unit
 ) {
-    var size by remember { mutableStateOf(IntSize(0, 0)) }
-    Box {
-        stream()
-        Box(
-            modifier = modifier
-                .matchParentSize()
-                .onGloballyPositioned { size = it.size }
-        ) {
-            pointerList?.value?.forEach { MovablePointer(it, size) }
+    if (streamView != null) {
+        val size by getSize(streamView).collectAsStateWithLifecycle(IntSize(0,0))
+        Box(contentAlignment = Alignment.Center) {
+            stream(!pointerList.isNullOrEmpty())
+            Box(modifier = modifier.size(size.toDpSize())) {
+                pointerList?.value?.forEach { MovablePointer(it, size) }
+            }
         }
     }
 }
+
+@Composable
+internal fun IntSize.toDpSize() = with(LocalDensity.current) {
+    DpSize(width.toDp(), height.toDp())
+}
+
+internal fun getSize(view: ImmutableView): Flow<IntSize> {
+    val streamView = view.value as? VideoStreamView ?: return MutableStateFlow(IntSize(0,0))
+    return streamView.videoSize.map { IntSize(it.width, it.height) }
+}
+
+private fun ImmutableList<PointerUi>?.isNullOrEmpty() = this == null || this.count() == 0
