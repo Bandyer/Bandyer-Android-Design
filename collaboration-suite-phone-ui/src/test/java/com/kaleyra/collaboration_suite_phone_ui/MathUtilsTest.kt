6284ec690419adea0d9d0f0ff7ab0f85eb917f8c
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-06-05 10:34:53 +0200

    add greater common divisor function

---
+++ collaboration-suite-phone-ui/src/test/java/com/kaleyra/collaboration_suite_phone_ui/MathUtilsTest.kt
@@ -0,0 +1,44 @@
+package com.kaleyra.collaboration_suite_phone_ui
+
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.utility.MathUtils
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MathUtilsTest {
+
+    @Test
+    fun testFindGreatestCommonDivisor1() {
+        val gcd = MathUtils.findGreatestCommonDivisor(0, 0)
+        assertEquals(0, gcd)
+    }
+
+    @Test
+    fun testFindGreatestCommonDivisor2() {
+        val gcd = MathUtils.findGreatestCommonDivisor(0, 12)
+        assertEquals(12, gcd)
+    }
+
+    @Test
+    fun testFindGreatestCommonDivisor3() {
+        val gcd = MathUtils.findGreatestCommonDivisor(12, 0)
+        assertEquals(12, gcd)
+    }
+
+    @Test
+    fun testFindGreatestCommonDivisor4() {
+        val gcd = MathUtils.findGreatestCommonDivisor(178, 178)
+        assertEquals(178, gcd)
+    }
+
+    @Test
+    fun testFindGreatestCommonDivisor5() {
+        val gcd = MathUtils.findGreatestCommonDivisor(1920, 1080)
+        assertEquals(120, gcd)
+    }
+
+    @Test
+    fun testFindGreatestCommonDivisor6() {
+        val gcd = MathUtils.findGreatestCommonDivisor(33, 98)
+        assertEquals(1, gcd)
+    }
+}
