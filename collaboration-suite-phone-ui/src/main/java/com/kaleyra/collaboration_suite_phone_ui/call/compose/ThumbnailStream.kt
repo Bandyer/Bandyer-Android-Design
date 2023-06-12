0e19de19b0e2c6427b8d3b09575df4f91be919db
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-06-12 17:19:30 +0200

    refactor ThumbnailStream modifier

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/ThumbnailStream.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/ThumbnailStream.kt
@@ -1,32 +1,39 @@
 package com.kaleyra.collaboration_suite_phone_ui.call.compose
 
-import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import com.kaleyra.collaboration_suite.phonebox.StreamView
+import com.kaleyra.collaboration_suite_phone_ui.R
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.StreamViewSettings.thumbnailSettings
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.pointer.PointerStreamWrapper
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.streams.Stream
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.streams.StreamContainer
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun ThumbnailStream(
     stream: StreamUi,
-    modifier: Modifier = Modifier,
-    onClick: (() -> Unit)? = null,
-    onDoubleClick: (() -> Unit)? = null
+    onClick: () -> Unit,
+    onDoubleClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     Thumbnail(
-        modifier = modifier
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onDoubleTap = { onDoubleClick?.invoke() },
-                    onTap = { onClick?.invoke() }
-                )
-            }
+        modifier = modifier.combinedClickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = null,
+            onClickLabel = stringResource(id = R.string.kaleyra_move_to_featured_streams),
+            role = Role.Button,
+            onClick = onClick,
+            onDoubleClick = onDoubleClick
+        )
     ) {
         StreamContainer(
             backgroundColor = Color.DarkGray,
