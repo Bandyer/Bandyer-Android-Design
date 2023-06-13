eeb7e4590838b09bed3606175e9a8a7d7f3ba527
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-06-13 12:19:42 +0200

    add default color to EllipsizeText

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/EllipsizeText.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/EllipsizeText.kt
@@ -3,6 +3,8 @@ package com.kaleyra.collaboration_suite_phone_ui.call.compose
 import android.graphics.Typeface
 import android.text.TextUtils
 import android.widget.TextView
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -28,7 +30,7 @@ enum class Ellipsize(val value: TextUtils.TruncateAt) {
 @Composable
 internal fun EllipsizeText(
     text: String,
-    color: Color,
+    color: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current),
     fontWeight: FontWeight = FontWeight.Normal,
     fontSize: TextUnit = 16.sp,
     ellipsize: Ellipsize,
