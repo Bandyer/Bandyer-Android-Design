cac700e63be9fdd5f8afa51b40dfd2dad80d234e
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-06-16 11:43:00 +0200

    add dialing ui state

---
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/dialing/view/DialingUiState.kt
@@ -0,0 +1,41 @@
+package com.kaleyra.collaboration_suite_phone_ui.call.compose.dialing.view
+
+import androidx.compose.runtime.Immutable
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.ImmutableUri
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.VideoUi
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.precall.model.PreCallUiState
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.streams.WatermarkInfo
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.usermessages.model.UserMessages
+import com.kaleyra.collaboration_suite_phone_ui.chat.model.ImmutableList
+
+@Immutable
+data class DialingUiState(
+    override val video: VideoUi? = null,
+    override val avatar: ImmutableUri? = null,
+    override val participants: ImmutableList<String> = ImmutableList(listOf()),
+    override val watermarkInfo: WatermarkInfo? = null,
+    override val isLink: Boolean = false,
+    override val isConnecting: Boolean = false,
+    override val userMessages: UserMessages = UserMessages()
+): PreCallUiState<DialingUiState> {
+
+    override fun clone(
+        video: VideoUi?,
+        avatar: ImmutableUri?,
+        participants: ImmutableList<String>,
+        watermarkInfo: WatermarkInfo?,
+        isLink: Boolean,
+        isConnecting: Boolean,
+        userMessages: UserMessages
+    ): DialingUiState {
+        return copy(
+            video = video,
+            avatar = avatar,
+            participants = participants,
+            watermarkInfo = watermarkInfo,
+            isLink = isLink,
+            isConnecting = isConnecting,
+            userMessages = userMessages
+        )
+    }
+}
