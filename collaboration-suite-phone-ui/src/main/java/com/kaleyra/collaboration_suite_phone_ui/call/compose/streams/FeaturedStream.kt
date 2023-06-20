c350b4a9f9afb6c2682704a018967ab7994595e9
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-06-20 11:59:44 +0200

    add featured stream header auto hide

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/streams/FeaturedStream.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/streams/FeaturedStream.kt
@@ -1,5 +1,7 @@
 package com.kaleyra.collaboration_suite_phone_ui.call.compose.streams
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -9,9 +11,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -24,6 +33,7 @@ import com.kaleyra.collaboration_suite_phone_ui.call.compose.IconButton
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.StreamUi
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.StreamViewSettings.featuredSettings
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.pointer.PointerStreamWrapper
+import com.kaleyra.collaboration_suite_phone_ui.call.compose.rememberCountdownTimerState
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.streamUiMock
 import com.kaleyra.collaboration_suite_phone_ui.chat.theme.KaleyraTheme
 
@@ -39,8 +49,31 @@ internal fun FeaturedStream(
     modifier: Modifier = Modifier,
     isTesting: Boolean = false
 ) {
+    var resetCountDown by remember { mutableStateOf(false) }
+    val countDown = if (stream.video?.view != null && stream.video.isEnabled) {
+        rememberCountdownTimerState(initialMillis = 5000L, resetFlag = resetCountDown)
+    } else {
+        remember(stream) { mutableStateOf(1L) }
+    }
+
+    val headerTargetAlpha by remember(countDown) {
+        derivedStateOf {
+            if (countDown.value > 0L) 1f else 0f
+        }
+    }
+    val headerAlpha by animateFloatAsState(targetValue = headerTargetAlpha)
+    val disableHeaderButtons by remember(headerTargetAlpha) {
+        derivedStateOf {
+            headerTargetAlpha == 1f
+        }
+    }
+
     Box(
-        modifier = modifier.testTag(FeaturedStreamTag)
+        modifier = modifier
+            .pointerInput(Unit) {
+                detectTapGestures(onPress = { resetCountDown = !resetCountDown })
+            }
+            .testTag(FeaturedStreamTag)
     ) {
         CompositionLocalProvider(LocalContentColor provides Color.White) {
             StreamContainer {
@@ -63,9 +96,21 @@ internal fun FeaturedStream(
             Header(
                 username = stream.username,
                 fullscreen = isFullscreen,
-                onBackPressed = onBackPressed,
-                onFullscreenClick = onFullscreenClick,
-                modifier = headerModifier
+                onBackPressed = remember(resetCountDown, disableHeaderButtons, onBackPressed) {
+                    {
+                        resetCountDown = !resetCountDown
+                        if (disableHeaderButtons) onBackPressed?.invoke()
+                    }
+                },
+                onFullscreenClick = remember(resetCountDown, disableHeaderButtons, onFullscreenClick) {
+                    {
+                        resetCountDown = !resetCountDown
+                        if (disableHeaderButtons) onFullscreenClick()
+                    }
+                },
+                modifier = Modifier
+                    .graphicsLayer { alpha = headerAlpha }
+                    .then(headerModifier)
             )
         }
     }
