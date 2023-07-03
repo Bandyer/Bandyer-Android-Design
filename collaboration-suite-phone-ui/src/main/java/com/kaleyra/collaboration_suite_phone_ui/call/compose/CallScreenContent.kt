79a668d3764ebec30e5946f0c39253399d056e78
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-07-03 17:25:02 +0200

    remove CallScreenContent animation

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/CallScreenContent.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/CallScreenContent.kt
@@ -1,18 +1,18 @@
 package com.kaleyra.collaboration_suite_phone_ui.call.compose
 
-import androidx.compose.animation.*
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.dialing.DialingComponent
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.ringing.RingingComponent
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 internal fun CallScreenContent(
     callState: CallStateUi,
@@ -36,22 +36,15 @@ internal fun CallScreenContent(
                 }
             }
         }
-        AnimatedContent(
-            targetState = targetState,
-            transitionSpec = {
-                fadeIn(animationSpec = tween(220, delayMillis = 90)) with fadeOut(animationSpec = tween(90))
-            }
-        ) { target ->
-            when (target) {
-                0 -> RingingComponent(onBackPressed = onBackPressed)
-                1 -> DialingComponent(onBackPressed = onBackPressed)
-                else -> CallComponent(
-                    shouldShowUserMessages = shouldShowUserMessages,
-                    maxWidth = maxWidth,
-                    onBackPressed = onBackPressed,
-                    onStreamFullscreenClick = onStreamFullscreenClick
-                )
-            }
+        when (targetState) {
+            0 -> RingingComponent(onBackPressed = onBackPressed)
+            1 -> DialingComponent(onBackPressed = onBackPressed)
+            else -> CallComponent(
+                shouldShowUserMessages = shouldShowUserMessages,
+                maxWidth = maxWidth,
+                onBackPressed = onBackPressed,
+                onStreamFullscreenClick = onStreamFullscreenClick
+            )
         }
     }
 }
