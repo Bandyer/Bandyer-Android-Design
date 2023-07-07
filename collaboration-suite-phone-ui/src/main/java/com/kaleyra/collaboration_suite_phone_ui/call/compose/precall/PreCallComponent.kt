7220b3ad4fe9f09667a07d9158fa541cbd78b9fc
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-07-07 18:00:29 +0200

    refactor PreCallComponent, simplify layout

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/precall/PreCallComponent.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/call/compose/precall/PreCallComponent.kt
@@ -5,24 +5,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.IntOffset
 import com.kaleyra.collaboration_suite_phone_ui.R
-import com.kaleyra.collaboration_suite_phone_ui.call.compose.Avatar
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.ImmutableUri
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.StreamViewSettings.featuredSettings
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.precall.model.PreCallUiState
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.streams.CallInfoWidget
-import com.kaleyra.collaboration_suite_phone_ui.call.compose.streams.DefaultStreamAvatarSize
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.streams.Stream
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.streams.StreamContainer
 import com.kaleyra.collaboration_suite_phone_ui.call.compose.usermessages.model.UserMessage
@@ -39,21 +33,14 @@ internal fun <T: PreCallUiState<T>> PreCallComponent(
 ) {
     CompositionLocalProvider(LocalContentColor provides Color.White) {
         Box(modifier = modifier.fillMaxSize()) {
-            if (uiState.video?.view != null && uiState.video?.isEnabled == true) {
-                StreamContainer {
-                    Stream(streamView = uiState.video?.view?.featuredSettings(), avatar = null, avatarVisible = false)
-                }
-            } else {
+            StreamContainer {
                 val isGroupCall = uiState.participants.count() > 1
-                Avatar(
-                    uri = if (isGroupCall) ImmutableUri(Uri.EMPTY) else uiState.avatar,
-                    contentDescription = stringResource(id = R.string.kaleyra_avatar),
-                    placeholder = if (isGroupCall) R.drawable.ic_kaleyra_avatars_bold else R.drawable.ic_kaleyra_avatar_bold,
-                    error = if (isGroupCall) R.drawable.ic_kaleyra_avatars_bold else R.drawable.ic_kaleyra_avatar_bold,
-                    contentColor = LocalContentColor.current,
-                    backgroundColor = colorResource(id = R.color.kaleyra_color_background_dark),
-                    size = DefaultStreamAvatarSize,
-                    modifier = Modifier.align(Alignment.Center)
+                Stream(
+                    streamView = uiState.video?.view?.featuredSettings(),
+                    avatar = if (isGroupCall) ImmutableUri(Uri.EMPTY) else uiState.avatar,
+                    avatarPlaceholder = if (isGroupCall) R.drawable.ic_kaleyra_avatars_bold else R.drawable.ic_kaleyra_avatar_bold,
+                    avatarError = if (isGroupCall) R.drawable.ic_kaleyra_avatars_bold else R.drawable.ic_kaleyra_avatar_bold,
+                    avatarVisible = uiState.video?.view == null || uiState.video?.isEnabled == false
                 )
             }
 
