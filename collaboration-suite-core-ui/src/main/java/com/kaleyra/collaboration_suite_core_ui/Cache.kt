a408431f37abd7e0ff187b0db3fe06c2f1c4c3f3
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-07-12 17:03:08 +0200

    implement cache

---
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/Cache.kt
@@ -0,0 +1,33 @@
+package com.kaleyra.collaboration_suite_core_ui
+
+interface Cache<K, V> {
+
+    val size: Int
+
+    operator fun set(key: K, value: V)
+
+    operator fun get(key: K): V?
+
+    fun remove(key: K): V?
+
+    fun clear()
+}
+
+class PerpetualCache<K, V> : Cache<K, V> {
+
+    private val cache = HashMap<K, V>()
+
+    override val size: Int
+        get() = cache.size
+
+    override fun set(key: K, value: V) {
+        cache[key] = value
+    }
+
+    override fun remove(key: K) = cache.remove(key)
+
+    override fun get(key: K) = cache[key]
+
+    override fun clear() = cache.clear()
+}
+
