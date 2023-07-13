66f6475520fa236b563f2f4f8783f9d077623461
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-07-13 16:00:20 +0200

    add LocalContactDetailsProvider

--- collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/contactdetails/provider/LocalContactDetailsProvider.kt
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/contactdetails/provider/LocalContactDetailsProvider.kt
@@ -2,14 +2,17 @@ package com.kaleyra.collaboration_suite_core_ui.contactdetails.provider
 
 import com.kaleyra.collaboration_suite_core_ui.contactdetails.model.ContactDetails
 import com.kaleyra.collaboration_suite_core_ui.model.UsersDescription
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withTimeout
 
-internal class LocalContactDetailsProvider(val usersDescription: UsersDescription) :
-    ContactDetailsProvider {
+internal class LocalContactDetailsProvider(
+    val usersDescription: UsersDescription,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+) : ContactDetailsProvider {
 
     override suspend fun fetchContactsDetails(
         vararg userIds: String,
@@ -18,7 +21,7 @@ internal class LocalContactDetailsProvider(val usersDescription: UsersDescriptio
         runCatching {
             withTimeout(timeout) {
                 val contactsDetails = userIds.map { userId ->
-                    async(Dispatchers.IO) {
+                    async(ioDispatcher) {
                         val name = async { usersDescription.name(listOf(userId)) }
                         val image = async { usersDescription.image(listOf(userId)) }
                         ContactDetails(userId = userId, name = name.await(), image = image.await())
---
+++ collaboration-suite-core-ui/src/test/java/com/kaleyra/collaboration_suite_core_ui/contactdetailsprovider/LocalContactDetailsProviderTest.kt
@@ -0,0 +1,120 @@
+package com.kaleyra.collaboration_suite_core_ui.contactdetailsprovider
+
+import android.net.Uri
+import com.kaleyra.collaboration_suite_core_ui.contactdetails.model.ContactDetails
+import com.kaleyra.collaboration_suite_core_ui.contactdetails.provider.LocalContactDetailsProvider
+import com.kaleyra.collaboration_suite_core_ui.model.DefaultUsersDescription
+import com.kaleyra.collaboration_suite_core_ui.model.UsersDescription
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class LocalContactDetailsProviderTest {
+
+    private val testDispatcher = StandardTestDispatcher()
+
+    private val uriUser1 = mockk<Uri>()
+
+    private val uriUser2 = mockk<Uri>()
+
+    @Test
+    fun `test contacts details empty user ids`() = runTest(testDispatcher) {
+        val provider = LocalContactDetailsProvider(
+            usersDescription = getUsersDescription(),
+            ioDispatcher = testDispatcher
+        )
+        val result = provider.fetchContactsDetails()
+        assertEquals(setOf<ContactDetails>(), result)
+    }
+
+    @Test
+    fun `test contacts details immediate fetch`() = runTest(testDispatcher) {
+        val provider = LocalContactDetailsProvider(
+            usersDescription = getUsersDescription(fetchDelay = 0L),
+            ioDispatcher = testDispatcher
+        )
+        val result = provider.fetchContactsDetails("userId1", "userId2")
+        val expected = setOf(
+            ContactDetails("userId1", "username1", uriUser1),
+            ContactDetails("userId2", "username2", uriUser2)
+        )
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test contacts details delayed fetch`() = runTest(testDispatcher) {
+        val provider = LocalContactDetailsProvider(
+            usersDescription = getUsersDescription(fetchDelay = 1500L),
+            ioDispatcher = testDispatcher
+        )
+        val result = provider.fetchContactsDetails("userId1", "userId2")
+        val expected = setOf(
+            ContactDetails("userId1", "username1", uriUser1),
+            ContactDetails("userId2", "username2", uriUser2)
+        )
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test contacts details fetch timed out`() = runTest(testDispatcher) {
+        val timeout = 1000L
+        val provider = LocalContactDetailsProvider(
+            usersDescription = getUsersDescription(fetchDelay = timeout),
+            ioDispatcher = testDispatcher
+        )
+        val result = provider.fetchContactsDetails("userId1", "userId2", timeout = timeout)
+        assertEquals(setOf<ContactDetails>(), result)
+    }
+
+    @Test
+    fun `test contacts details fetch timeout limit`() = runTest(testDispatcher) {
+        val timeout = 1000L
+        val provider = LocalContactDetailsProvider(
+            usersDescription = getUsersDescription(fetchDelay = timeout - 1),
+            ioDispatcher = testDispatcher
+        )
+        val result = provider.fetchContactsDetails("userId1", "userId2", timeout = timeout)
+        val expected = setOf(
+            ContactDetails("userId1", "username1", uriUser1),
+            ContactDetails("userId2", "username2", uriUser2)
+        )
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test contacts details fetch exception occurrence`() = runTest(testDispatcher) {
+        val provider = LocalContactDetailsProvider(
+            usersDescription = DefaultUsersDescription(name = { throw Exception() }),
+            ioDispatcher = testDispatcher
+        )
+        val result = provider.fetchContactsDetails("userId1", "userId2")
+        assertEquals(setOf<ContactDetails>(), result)
+    }
+
+    private fun getUsersDescription(fetchDelay: Long = 0L): UsersDescription {
+        return DefaultUsersDescription(
+            name = { userIds ->
+                delay(fetchDelay)
+                when {
+                    userIds.find { it == "userId1" } != null -> "username1"
+                    userIds.find { it == "userId2" } != null -> "username2"
+                    else -> "null"
+                }
+            },
+            image = { userIds ->
+                delay(fetchDelay)
+                when {
+                    userIds.find { it == "userId1" } != null -> uriUser1
+                    userIds.find { it == "userId2" } != null -> uriUser2
+                    else -> Uri.EMPTY
+                }
+            }
+        )
+    }
+
+}
