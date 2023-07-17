ace6a79b1369877920bd2dbfcba7a750fa1792ab
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-07-17 16:11:43 +0200

    improve flow handling in ContactDetailsManager

--- collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/contactdetails/ContactDetailsManager.kt
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/contactdetails/ContactDetailsManager.kt
@@ -3,8 +3,16 @@ package com.kaleyra.collaboration_suite_core_ui.contactdetails
 import android.net.Uri
 import com.kaleyra.collaboration_suite.Contact
 import com.kaleyra.collaboration_suite_core_ui.contactdetails.provider.CollaborationContactDetailsProvider
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.plus
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import java.util.concurrent.ConcurrentHashMap
@@ -16,25 +24,25 @@ object ContactDetailsManager {
 
     const val FETCH_TIMEOUT = 2500L
 
-    val Contact.combinedDisplayName: StateFlow<String?>
-        get() = MutableStateFlow(contactNames[userId])
-
-    val Contact.combinedDisplayImage: StateFlow<Uri?>
-        get() = MutableStateFlow(contactImages[userId])
-
     private val collaborationContactDetailsProvider by lazy { CollaborationContactDetailsProvider() }
 
-    private val contactNames = ConcurrentHashMap<String, String?>()
+    private val contactNames = MutableStateFlow(HashMap<String, String?>())
 
-    private val contactImages = ConcurrentHashMap<String, Uri?>()
+    private val contactImages = MutableStateFlow(HashMap<String, Uri?>())
 
     private val mutex = Mutex()
 
+    val Contact.combinedDisplayName: Flow<String?>
+        get() = contactNames.map { it[userId] }
+
+    val Contact.combinedDisplayImage: Flow<Uri?>
+        get() = contactImages.map { it[userId] }
+
     suspend fun refreshContactDetails(vararg userIds: String, timeout: Long = FETCH_TIMEOUT) = mutex.withLock {
         val fetchedContactDetails = collaborationContactDetailsProvider.fetchContactsDetails(userIds = userIds, timeout = timeout)
-        fetchedContactDetails.forEach {
-            contactNames[it.userId] = it.name
-            contactImages[it.userId] = it.image
+        fetchedContactDetails.forEach { contactDetails ->
+            contactNames.update { it.apply { this[contactDetails.userId] = contactDetails.name } }
+            contactImages.update { it.apply { this[contactDetails.userId] = contactDetails.image } }
         }
     }
 
