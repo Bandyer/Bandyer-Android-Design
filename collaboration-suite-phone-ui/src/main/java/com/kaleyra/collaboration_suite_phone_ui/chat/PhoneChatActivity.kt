32ded7b2b1a3445ff3e09fa9c7d73e1edea9df51
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-08-29 15:23:39 +0200

    fix status bar icon color on forced day night theme on the chat

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/chat/PhoneChatActivity.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/chat/PhoneChatActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.lightColors
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.kaleyra.collaboration_suite_core_ui.ChatActivity
 import com.kaleyra.collaboration_suite_core_ui.Theme
 import com.kaleyra.collaboration_suite_core_ui.requestConfiguration
 import com.kaleyra.collaboration_suite_core_ui.theme.KaleyraTheme
 import com.kaleyra.collaboration_suite_phone_ui.chat.utility.collectAsStateWithLifecycle
 import com.kaleyra.collaboration_suite_phone_ui.chat.viewmodel.PhoneChatViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 internal class PhoneChatActivity : ChatActivity() {
 
@@ -23,7 +30,25 @@ internal class PhoneChatActivity : ChatActivity() {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            val theme = viewModel.theme.collectAsStateWithLifecycle(Theme()).value
+            val systemUiController = rememberSystemUiController()
+            val theme by viewModel.theme.collectAsStateWithLifecycle(Theme())
+            val isSystemDarkTheme = isSystemInDarkTheme()
+            val isDarkTheme by remember {
+                derivedStateOf {
+                    when (theme.defaultStyle) {
+                        Theme.DefaultStyle.Day -> false
+                        Theme.DefaultStyle.Night -> true
+                        else -> isSystemDarkTheme
+                    }
+                }
+            }
+
+            LaunchedEffect(Unit) {
+                snapshotFlow { isDarkTheme }
+                    .onEach { systemUiController.statusBarDarkContentEnabled = !it }
+                    .launchIn(this)
+            }
+
             KaleyraTheme(
                 lightColors = theme.day.colors,
                 darkColors = theme.night.colors,
@@ -31,10 +56,11 @@ internal class PhoneChatActivity : ChatActivity() {
                 isDarkTheme = when (theme.defaultStyle) {
                     Theme.DefaultStyle.Day -> false
                     Theme.DefaultStyle.Night -> true
-                    else -> isSystemInDarkTheme()
-                },
-                content = { ChatScreen(onBackPressed = this::finishAndRemoveTask, viewModel = viewModel) }
-            )
+                    else -> isDarkTheme
+                }
+            ) {
+                ChatScreen(onBackPressed = this::finishAndRemoveTask, viewModel = viewModel)
+            }
         }
     }
 }
