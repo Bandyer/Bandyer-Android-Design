b4d4f4fa2ddb3459acddd83947050735c6c0913e
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-08-29 12:52:22 +0200

    remove deps in theme

--- collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/theme/Theme.kt
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/theme/Theme.kt
@@ -5,8 +5,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
