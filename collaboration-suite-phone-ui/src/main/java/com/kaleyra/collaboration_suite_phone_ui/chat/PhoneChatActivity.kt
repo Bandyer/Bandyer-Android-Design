8d86b66971c0896670818c932d4cca64b6d17bcd
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-08-29 12:08:18 +0200

    refactor PhoneChatActivity to use KaleyraTheme

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/chat/PhoneChatActivity.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/chat/PhoneChatActivity.kt
@@ -1,17 +1,16 @@
-@file:OptIn(ExperimentalComposeUiApi::class)
-
 package com.kaleyra.collaboration_suite_phone_ui.chat
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.lightColors
 import androidx.core.view.WindowCompat
-import com.google.android.material.composethemeadapter.MdcTheme
 import com.kaleyra.collaboration_suite_core_ui.ChatActivity
+import com.kaleyra.collaboration_suite_core_ui.Theme
 import com.kaleyra.collaboration_suite_core_ui.requestConfiguration
+import com.kaleyra.collaboration_suite_core_ui.theme.KaleyraTheme
+import com.kaleyra.collaboration_suite_phone_ui.chat.utility.collectAsStateWithLifecycle
 import com.kaleyra.collaboration_suite_phone_ui.chat.viewmodel.PhoneChatViewModel
 
 internal class PhoneChatActivity : ChatActivity() {
@@ -24,9 +23,18 @@ internal class PhoneChatActivity : ChatActivity() {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            MdcTheme(setDefaultFontFamily = true) {
-                ChatScreen(onBackPressed = this::finishAndRemoveTask, viewModel = viewModel)
-            }
+            val theme = viewModel.theme.collectAsStateWithLifecycle(Theme()).value
+            KaleyraTheme(
+                lightColors = theme.day.colors,
+                darkColors = theme.night.colors,
+                fontFamily = theme.fontFamily,
+                isDarkTheme = when (theme.defaultStyle) {
+                    Theme.DefaultStyle.Day -> false
+                    Theme.DefaultStyle.Night -> true
+                    else -> isSystemInDarkTheme()
+                },
+                content = { ChatScreen(onBackPressed = this::finishAndRemoveTask, viewModel = viewModel) }
+            )
         }
     }
 }
