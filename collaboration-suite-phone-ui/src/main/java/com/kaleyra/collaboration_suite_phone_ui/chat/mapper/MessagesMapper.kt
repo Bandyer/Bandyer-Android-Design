1282fede14e37073624a9fd52d8f178b21b10d5e
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-09-15 18:37:06 +0200

    refactor MessagesMapper

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/chat/mapper/MessagesMapper.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/chat/mapper/MessagesMapper.kt
@@ -1,6 +1,7 @@
 package com.kaleyra.collaboration_suite_phone_ui.chat.mapper
 
 import android.net.Uri
+import com.kaleyra.collaboration_suite.conversation.ChatParticipants
 import com.kaleyra.collaboration_suite.conversation.Message
 import com.kaleyra.collaboration_suite.conversation.Messages
 import com.kaleyra.collaboration_suite_core_ui.MessagesUI
@@ -9,35 +10,73 @@ import com.kaleyra.collaboration_suite_core_ui.contactdetails.ContactDetailsMana
 import com.kaleyra.collaboration_suite_core_ui.utils.TimestampUtils
 import com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.ConversationElement
 import com.kaleyra.collaboration_suite_phone_ui.common.avatar.model.ImmutableUri
+import com.kaleyra.collaboration_suite_phone_ui.common.immutablecollections.ImmutableMap
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.transform
-import java.util.Date
+
+typealias ParticipantDetails = Pair<String, ImmutableUri>
 
 object MessagesMapper {
 
-    fun Message.toUiMessage(): Flow<com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message> {
+    fun Flow<ChatParticipants>.toChatParticipantUserDetails(): Flow<ImmutableMap<String, ParticipantDetails>> =
+        flatMapLatest { chatParticipants ->
+                val participantsList = chatParticipants.list
+                val users = mutableMapOf<String, ParticipantDetails>()
+                participantsList
+                    .map { participant ->
+                        combine(
+                            participant.combinedDisplayName,
+                            participant.combinedDisplayImage.map { ImmutableUri(it ?: Uri.EMPTY) }
+                        ) { name, image -> Triple(participant.userId, name ?: "", image) }
+                    }
+                    .merge()
+                    .transform { (userId, name, image) ->
+                        users[userId] = ParticipantDetails(name, image)
+                        val values = users.values.toList()
+                        if (values.size == participantsList.size) {
+                            emit(ImmutableMap(users))
+                        }
+                    }
+            }
+            .distinctUntilChanged()
+
+    fun Flow<MessagesUI>.toMyMessagesSendStates(): Flow<ImmutableMap<String, com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message.State>> {
+        return this.flatMapLatest { messagesUI ->
+            val states = mutableMapOf<String, com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message.State>()
+            val messages = messagesUI.my
+
+            messages
+                .map { message -> message.state.map { Pair(message.id, it) } }
+                .merge()
+                .transform { (messageId, messageState) ->
+                    states[messageId] = messageState.mapToUiState()
+                    if (messages.size == states.keys.size) {
+                        emit(ImmutableMap(states))
+                    }
+                }
+        }
+    }
+
+    fun Message.toUiMessage(): com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message {
         val text = (content as? Message.Content.Text)?.message ?: ""
         val time = TimestampUtils.parseTime(creationDate.time)
 
         return if (this is com.kaleyra.collaboration_suite.conversation.OtherMessage) {
-            combine(creator.combinedDisplayName, creator.combinedDisplayImage.map { ImmutableUri(it ?: Uri.EMPTY) }) { name, image ->
-                com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message.OtherMessage(id, text, time, name ?: "",  image)
-            }
+            com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message.OtherMessage(id, text, time)
         } else {
-            flowOf(com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message.MyMessage(id, text, time, state.map { state -> mapToUiState(state) }))
+            com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message.MyMessage(id, text, time)
         }
     }
 
-    private fun mapToUiState(state: Message.State): com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message.State =
-        when (state) {
+    private fun Message.State.mapToUiState(): com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message.State =
+        when (this) {
             is Message.State.Sending -> com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message.State.Sending
             is Message.State.Sent -> com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message.State.Sent
             else -> com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message.State.Read
@@ -47,38 +86,23 @@ object MessagesMapper {
         firstUnreadMessageId: String?,
         shouldShowUnreadHeader: Flow<Boolean>
     ): Flow<List<ConversationElement>> {
-        return flatMapLatest { messagesUI ->
-            val map = mutableMapOf<String, Pair<com.kaleyra.collaboration_suite_phone_ui.chat.conversation.model.Message, Date>>()
-            val messagesList = messagesUI.list
+        return combine(map { it.list }, shouldShowUnreadHeader) { messages, unreadHeader ->
+            val items = mutableListOf<ConversationElement>()
+            messages.forEachIndexed { index, message ->
+                val previousMessage = messages.getOrNull(index + 1)
 
-            messagesList
-                .map { message -> message.toUiMessage().map { Pair(it, message.creationDate) } }
-                .merge()
-                .transform { pair ->
-                    val message = pair.first
-                    map[message.id] = pair
-                    val values = map.values.toList()
-                    if (values.size == messagesList.size) {
-                        emit(values)
-                    }
-                }.combine(shouldShowUnreadHeader) { pairs, unreadHeader ->
-                    val items = mutableListOf<ConversationElement>()
-                    var previousCreationDate: Date? = null
-                    pairs.forEach { (message, creationDate) ->
-                        items.add(ConversationElement.Message(message))
+                items.add(ConversationElement.Message(message.toUiMessage()))
 
-                        if (unreadHeader && message.id == firstUnreadMessageId) {
-                            items.add(ConversationElement.UnreadMessages)
-                        }
-
-                        if (previousCreationDate == null || !TimestampUtils.isSameDay(creationDate.time, previousCreationDate!!.time)) {
-                            items.add(ConversationElement.Day(creationDate.time))
-                        }
-                        previousCreationDate = creationDate
-                    }
-                    items
+                if (unreadHeader && message.id == firstUnreadMessageId) {
+                    items.add(ConversationElement.UnreadMessages)
                 }
-        }.distinctUntilChanged()
+
+                if (previousMessage == null || !TimestampUtils.isSameDay(message.creationDate.time, previousMessage.creationDate.time)) {
+                    items.add(ConversationElement.Day(message.creationDate.time))
+                }
+            }
+            items
+        }
     }
 
     suspend fun findFirstUnreadMessageId(
