c2f5900a629db811c14e6c55ae1c42c458c223eb
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-09-20 15:33:22 +0200

    add immutable map

--- collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/common/immutablecollections/ImmutableMap.kt
+++ collaboration-suite-phone-ui/src/main/java/com/kaleyra/collaboration_suite_phone_ui/common/immutablecollections/ImmutableMap.kt
@@ -2,6 +2,8 @@ package com.kaleyra.collaboration_suite_phone_ui.common.immutablecollections
 
 import androidx.compose.runtime.Immutable
 @Immutable
-data class ImmutableMap<K, out V>(val value: Map<K, V>) {
+data class ImmutableMap<K, out V>(val value: Map<K, V> = HashMap()) {
+    operator fun get(key: K): V? = value[key]
+
     fun count() = value.count()
 }
