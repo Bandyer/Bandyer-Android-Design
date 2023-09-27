4acadb3bf9d67cf411ab66f0b151007997353f80
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-09-27 13:11:31 +0200

    refactor ConversationStateMapper test

--- collaboration-suite-phone-ui/src/test/java/com/kaleyra/collaboration_suite_phone_ui/mapper/chat/ConversationStateMapperTest.kt
+++ collaboration-suite-phone-ui/src/test/java/com/kaleyra/collaboration_suite_phone_ui/mapper/chat/ConversationStateMapperTest.kt
@@ -17,7 +17,6 @@ import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
-import java.util.Date
 
 class ConversationStateMapperTest {
 
@@ -34,77 +33,56 @@ class ConversationStateMapperTest {
         Mocks.otherParticipantState.value = ChatParticipant.State.Invited
         Mocks.otherParticipantEvents.value = ChatParticipant.Event.Typing.Idle
     }
-    
+
     @Test
-    fun conversationConnecting_getChatState_networkConnecting() = runTest {
+    fun conversationConnected_toConnectionState_connected() = runTest {
+        Mocks.conversationState.value = Conversation.State.Connected
+        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState().first(), ConnectionState.Connected)
+    }
+
+    @Test
+    fun conversationConnecting_toConnectionState_connecting() = runTest {
         Mocks.conversationState.value = Conversation.State.Connecting
-        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState(flowOf(Mocks.chatParticipantsMock)).first(), ConnectionState.NetworkState.Connecting)
+        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState().first(), ConnectionState.Connecting)
     }
 
     @Test
-    fun conversationDisconnecting_getChatState_none() = runTest {
+    fun conversationDisconnecting_toConnectionState_unknown() = runTest {
         Mocks.conversationState.value = Conversation.State.Disconnecting
-        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState(flowOf(Mocks.chatParticipantsMock)).first(), ConnectionState.Undefined)
+        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState().first(), ConnectionState.Unknown)
     }
 
     @Test
-    fun conversationDisconnected_getChatState_none() = runTest {
+    fun conversationDisconnected_toConnectionState_none() = runTest {
         Mocks.conversationState.value = Conversation.State.Disconnected
-        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState(flowOf(Mocks.chatParticipantsMock)).first(), ConnectionState.Undefined)
+        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState().first(), ConnectionState.Unknown)
     }
 
     @Test
-    fun conversationStateUserInactive_getChatState_none() = runTest {
+    fun conversationStateUserInactive_toConnectionState_none() = runTest {
         Mocks.conversationState.value = Conversation.State.Disconnected.UserInactive
-        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState(flowOf(Mocks.chatParticipantsMock)).first(), ConnectionState.Undefined)
+        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState().first(), ConnectionState.Unknown)
     }
 
     @Test
-    fun conversationStateUnsupportedVersion_getChatState_none() = runTest {
+    fun conversationStateUnsupportedVersion_toConnectionState_none() = runTest {
         Mocks.conversationState.value = Conversation.State.Disconnected.UnsupportedVersion
-        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState(flowOf(Mocks.chatParticipantsMock)).first(), ConnectionState.Undefined)
+        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState().first(), ConnectionState.Unknown)
     }
 
     @Test
-    fun conversationStateUnknown_getChatState_none() = runTest {
+    fun conversationStateUnknown_toConnectionState_none() = runTest {
         Mocks.conversationState.value = Conversation.State.Disconnected.Unknown
-        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState(flowOf(Mocks.chatParticipantsMock)).first(), ConnectionState.Undefined)
+        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState().first(), ConnectionState.Unknown)
     }
 
     @Test
-    fun conversationReconnecting_getChatState_networkOffline() = runTest {
-        with(flowOf(Mocks.conversationMock).toConnectionState(flowOf(Mocks.chatParticipantsMock))) {
+    fun conversationReconnecting_toConnectionState_offline() = runTest {
+        with(flowOf(Mocks.conversationMock).toConnectionState()) {
             first()
             conversationState.value = Conversation.State.Connecting
-            assertEquals(first(), ConnectionState.NetworkState.Offline)
+            assertEquals(first(), ConnectionState.Offline)
         }
     }
 
-    @Test
-    fun participantOnline_getChatState_userOnline() = runTest {
-        Mocks.otherParticipantState.value = ChatParticipant.State.Joined.Online
-        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState(flowOf(Mocks.chatParticipantsMock)).first(), ConnectionState.UserState.Online)
-    }
-
-    @Test
-    fun participantOfflineAt_getChatState_userOfflineTimestamp() = runTest {
-        val nowMillis = Mocks.now.toEpochMilli()
-        Mocks.otherParticipantState.value = ChatParticipant.State.Joined.Offline(
-            ChatParticipant.State.Joined.Offline.LastLogin.At(
-                Date(nowMillis)
-            ))
-        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState(flowOf(Mocks.chatParticipantsMock)).first(), ConnectionState.UserState.Offline(nowMillis))
-    }
-
-    @Test
-    fun participantOffline_getChatState_userOffline() = runTest {
-        Mocks.otherParticipantState.value = ChatParticipant.State.Joined.Offline(ChatParticipant.State.Joined.Offline.LastLogin.Never)
-        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState(flowOf(Mocks.chatParticipantsMock)).first(), ConnectionState.UserState.Offline(null))
-    }
-
-    @Test
-    fun participantTyping_getChatState_userTyping() = runTest {
-        Mocks.otherParticipantEvents.value = ChatParticipant.Event.Typing.Started
-        Assert.assertEquals(flowOf(Mocks.conversationMock).toConnectionState(flowOf(Mocks.chatParticipantsMock)).first(), ConnectionState.UserState.Typing)
-    }
 }
