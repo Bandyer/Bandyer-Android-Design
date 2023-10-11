b3154da6a4b88d701d46f1a04465c10e66fb91b6
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-10-11 12:15:34 +0200

    fix deprecation of registerReceiver

--- collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/termsandconditions/broadcastreceiver/TermsAndConditionBroadcastReceiver.kt
+++ collaboration-suite-core-ui/src/main/java/com/kaleyra/collaboration_suite_core_ui/termsandconditions/broadcastreceiver/TermsAndConditionBroadcastReceiver.kt
@@ -1,9 +1,12 @@
 package com.kaleyra.collaboration_suite_core_ui.termsandconditions.broadcastreceiver
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.RECEIVER_NOT_EXPORTED
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Build
 
 abstract class TermsAndConditionBroadcastReceiver : BroadcastReceiver() {
 
@@ -13,12 +16,15 @@ abstract class TermsAndConditionBroadcastReceiver : BroadcastReceiver() {
         const val ACTION_CANCEL = "com.kaleyra.collaboration_suite_core_ui.termsandconditions.broadcastreceiver.ACTION_CANCEL"
     }
 
+    @SuppressLint("UnspecifiedRegisterReceiverFlag")
     fun registerForTermAndConditionAction(context: Context) {
-        context.registerReceiver(this, IntentFilter().apply {
+        val filter = IntentFilter().apply {
             addAction(ACTION_ACCEPT)
             addAction(ACTION_DECLINE)
             addAction(ACTION_DECLINE)
-        })
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) context.registerReceiver(this, filter, RECEIVER_NOT_EXPORTED)
+        else context.registerReceiver(this, filter)
     }
 
     override fun onReceive(context: Context, intent: Intent) {
