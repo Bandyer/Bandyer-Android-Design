ee30afbe035b9e5f20d1c96ec4a573680a81d5a3
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-10-20 10:46:37 +0200

    add CallUITest

--- collaboration-suite-core-ui/src/androidTest/java/com/kaleyra/collaboration_suite_core_ui/ActivityExtensionsTest.kt
+++ collaboration-suite-core-ui/src/androidTest/java/com/kaleyra/collaboration_suite_core_ui/ActivityExtensionsTest.kt
@@ -1,21 +1,11 @@
 package com.kaleyra.collaboration_suite_core_ui
 
-import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
-import androidx.test.platform.app.InstrumentationRegistry
 import com.kaleyra.collaboration_suite_core_ui.utils.extensions.ActivityExtensions.moveToFront
-import com.kaleyra.collaboration_suite_core_ui.utils.extensions.ContextExtensions
-import com.kaleyra.collaboration_suite_core_ui.utils.extensions.ContextExtensions.doesFileExists
-import com.kaleyra.collaboration_suite_core_ui.utils.extensions.ContextExtensions.tryToOpenFile
-import com.kaleyra.collaboration_suite_core_ui.utils.extensions.UriExtensions
-import com.kaleyra.collaboration_suite_core_ui.utils.extensions.UriExtensions.getMimeType
-import io.mockk.every
-import io.mockk.mockkObject
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
