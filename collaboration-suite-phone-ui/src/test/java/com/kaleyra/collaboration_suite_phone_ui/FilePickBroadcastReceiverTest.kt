0f2755309646369b81d189f79d0592a0bfa298c7
Stefano Brusadelli <stefano.brusadelli@kaleyra.com>
2023-10-27 12:43:58 +0200

    fix previous loaded file kept in cache on the next call

--- collaboration-suite-phone-ui/src/test/java/com/kaleyra/collaboration_suite_phone_ui/FilePickBroadcastReceiverTest.kt
+++ collaboration-suite-phone-ui/src/test/java/com/kaleyra/collaboration_suite_phone_ui/FilePickBroadcastReceiverTest.kt
@@ -7,6 +7,8 @@ import com.kaleyra.collaboration_suite_phone_ui.call.fileshare.filepick.FilePick
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -24,9 +26,24 @@ class FilePickBroadcastReceiverTest {
     @Test
     fun testOnReceive() = runTest {
         val uriMock = mockk<Uri>()
+        var result: Uri? = null
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+            result = FilePickBroadcastReceiver.fileUri.first()
+        }
         receiver.onReceive(contextMock, Intent(FilePickBroadcastReceiver.ACTION_FILE_PICK_EVENT).apply {
           putExtra("uri", uriMock)
         })
-        assertEquals(uriMock, FilePickBroadcastReceiver.fileUri.first())
+        assertEquals(uriMock, result)
     }
+
+    @Test
+    fun `test file uri flow does not have a replay value`() = runTest {
+        val uriMock = mockk<Uri>()
+        receiver.onReceive(contextMock, Intent(FilePickBroadcastReceiver.ACTION_FILE_PICK_EVENT).apply {
+            putExtra("uri", uriMock)
+        })
+        val uri = FilePickBroadcastReceiver.fileUri.first()
+        assertEquals(null, uri)
+    }
+
 }
